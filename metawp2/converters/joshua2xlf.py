3e559abb294a51101a41eb3ca924825fba802e9e
lupo01 <lupo01>
2011-01-28 12:32:38 +0000

    Comments, coding guidelines, added T_NUM as a sys.argv param.

--- metawp2/converters/joshua2xlf.py
+++ metawp2/converters/joshua2xlf.py
@@ -3,7 +3,7 @@ This program prints the best version of each sentence to a single file.
 """
 import re
 import os
-import time #for debugging purposes
+#import time
 import sys
 
 class _Node:
@@ -34,13 +34,16 @@ def replace_brackets(string):
 def get_str(listVar):
     string = ''
     for item in listVar:
-        string = string + item
+        string += item
     return string
 
 
-def get_1best (xlf):
+# This method has as an input all translated sentences in nbest file. In this
+# file the sentences are in descending order according to the translation word
+# penalty. The method gets always the first (best) sentence and
+# returns these as a list. 
+def get_1best(xlf):
     bestSnts = []
-    d={}
     ids = set()
     for snt in xlf:
         # Number of sentence.
@@ -49,32 +52,38 @@ def get_1best (xlf):
         if snt_no not in ids:
             ids.add(snt_no)
             bestSnts.append( (snt, snt_no) )
-            
-            
     return bestSnts
 
-def annotate_OOV (token):
-    if token.endswith("_OOV"):
-        return "<annotation type=\"OOV\" value=\"1\" />"
-    else :
-        return "<annotation type=\"OOV\" value=\"0\" />"
 
-def remove_OOV (string):
+# This method returns an annotation for a token. If the token is OOV, it sets
+# the value on 1, otherwise 0. 
+def annotate_OOV(token):
+    if token.endswith('_OOV'):
+        return '<annotation type=\"OOV\" value=\"1\" />'
+    else :
+        return '<annotation type=\"OOV\" value=\"0\" />'
+
+
+# Removes a string '_OOV' from the end of a token, if the token has it.
+def remove_OOV(string):
     return re.sub(r'([^ ]*)_OOV', r'\1', string)
 
-def get_tokens_xml (string, s_phrase_id):
+
+# This method encapsulates each token to the appropriate tags.
+def get_tokens_xml(string, s_phrase_id):
     tokens = string.split()
-    output = "\n\t\t\t<tokens>"
+    output = '\n\t\t\t<tokens>'
     i = 1
     for token in tokens:
-        s_token_id = "%s_k%s"% (s_phrase_id, str(i))
-        output += "\n\t\t\t\t<token id=\"%s\">" %s_token_id
-        output += "\n\t\t\t\t\t<string>%s</string>" %remove_OOV(token)
-        output += "\n\t\t\t\t\t%s" %annotate_OOV(token)
-        output += "\n\t\t\t\t</token>" 
+        s_token_id = '%s_k%s'% (s_phrase_id, str(i))
+        output += '\n\t\t\t\t<token id=\"%s\">' % s_token_id
+        output += '\n\t\t\t\t\t<string>%s</string>' % remove_OOV(token)
+        output += '\n\t\t\t\t\t%s' % annotate_OOV(token)
+        output += '\n\t\t\t\t</token>' 
         i += 1
-    output += "\n\t\t\t</tokens>"
+    output += '\n\t\t\t</tokens>'
     return output
+
 
 # -----------INPUT-----------
 # file to be converted
@@ -86,6 +95,9 @@ FILENAME_INPUT = sys.argv[2]
 # languages input and output
 INPUT_LANG = sys.argv[3]
 OUTPUT_LANG = sys.argv[4]
+
+# t number
+T_NUM = sys.argv[5]
 # ---------INPUT END---------
 
 f = open(FILENAME, 'r')
@@ -96,8 +108,7 @@ g = open(FILENAME_INPUT, 'r')
 inputSnts = g.read().split('\n')
 g.close()
 
-
-
+# Makes from XLF a list of best translated sentences.
 xlf = get_1best(xlf)
 
 
@@ -143,7 +154,7 @@ line_no = 0
 for (line, snt_no) in xlf:
     #---------------PARSE SENTENCES---------------
     # Returns a part beginning with '(ROOT{... ...)' and ending with '-#end#-'.
-    s = line.partition(' ||| ')[2].partition(' ||| ')[0] + '-#end#-'
+    s = '%s-#end#-' % (line.partition(' ||| ')[2].partition(' ||| ')[0])
     # Replaces textual brackets with '<open>' and '<close>'.
     # It prevents confusing textual brackets and tree brackets.
     s = s.replace('( ', '<open>').replace(' )', '<close>')
@@ -169,8 +180,8 @@ for (line, snt_no) in xlf:
         if re.findall('[(][[].[]]', s[i:i + 4]):
             # sNodeString - Saves each node in textual form (e. g. '[X]{3-7}').
             # It is later used in the whole sentence composition.
-            node.sNodeString = node.sNodeString + '<<>>' + re.search \
-                               ('[[].[]][{]\d+-\d+[}]', s[i:]).group(0)
+            node.sNodeString += '<<>>%s' % (re.search('[[].[]][{]\d+-\d+[}]', 
+                                                      s[i:]).group(0))
             # Saves actual node to temp variable.
             oldnode = node
             # Creates new node.
@@ -179,10 +190,10 @@ for (line, snt_no) in xlf:
             oldnode.add_child(node)
             # Creates a connection between new node and his father.
             node.parent = oldnode
-            num = num + 1
+            num += 1
             # Adds a node index.
             node_no.append(num)
-            iPhraseID = iPhraseID + 1
+            iPhraseID += 1
             node.iPhraseID = iPhraseID
 
         # If a root node starts or if a next node starts.
@@ -200,11 +211,10 @@ for (line, snt_no) in xlf:
             sText = s[i:].partition('}')[2].partition(' ([')[0]
             if not sText.count(')') and sText:
                 # sString - Saves the gained string.
-                node.sString = node.sString + replace_brackets(sText)
+                node.sString += replace_brackets(sText)
                 # sNodeString - Saves the gained string.
                 # It is later used in the whole sentence composition.
-                node.sNodeString = node.sNodeString + '<<>>' + \
-                                   replace_brackets(sText)
+                node.sNodeString += '<<>>%s' % (replace_brackets(sText))
 
         # If a node ends.
         if s[i] == ')':
@@ -213,16 +223,15 @@ for (line, snt_no) in xlf:
             sText = replace_brackets(s[:i].rpartition('}')[2].split(')')[-1])
             if sText:
                 # sString - Saves the gained string.
-                node.sString = node.sString + sText
+                node.sString += sText
                 # sNodeString - Saves the gained string.
                 # It is later used in the whole sentence composition.
-                node.sNodeString = node.sNodeString + '<<>>' + sText
+                node.sNodeString += '<<>>%s' % (sText)
             # sNodeString is splitted. The list may contain string(s)
             # or node(s) in textual form (e. g. [S]{0-3}).
             node.sNodeString = node.sNodeString.strip('<<>>').split('<<>>')
 
-            key = '[' + node.sClass + ']{' + str(node.iFrom) + '-' + \
-                  str(node.iTo) + '}' 
+            key = '[%s]{%s-%s}' % (node.sClass, str(node.iFrom), str(node.iTo)) 
             nodeStrings[key] = [key, node.sNodeString]
             # If node.sClass is 'ROOT', keeps ROOT node as an actual node
             # in memory (important for printing part).
@@ -248,7 +257,7 @@ for (line, snt_no) in xlf:
         for item in re.findall('[[].[]][{]\d+-\d+[}]', snt):
             snt = snt.replace(item, get_str(nodeStrings[item][1]))
             break
-        a = a + 1
+        a += 1
     if a == 1000: print '>1000 operations in creating sentence!'
     tarSnt = snt.strip(' ')
     #----------END COMPOSITION OF THE WHOLE TARGET LANGUAGE SENTENCE----------
@@ -256,26 +265,26 @@ for (line, snt_no) in xlf:
 
     #-------------CREATING AN OUTPUT FILE--------------
     sXlf = ''
-    sXlf = sXlf + '\n<alt-trans tool-id="t1" add:rank="1">'
-    sXlf = sXlf + '\n\t<source>' + inputSnts[line_no].strip() + '</source>'
-    sXlf = sXlf + '\n\t<target>' + remove_OOV(tarSnt) + '</target>'
+    sXlf += '\n<alt-trans tool-id="t%s" add:rank="1">' % (T_NUM)
+    sXlf += '\n\t<source>%s</source>' % (inputSnts[line_no].strip())
+    sXlf += '\n\t<target>%s</target>' % (remove_OOV(tarSnt))
 
     # total, lm, pt0, pt1, pt2
     scores = line.rpartition('|||')[0].rpartition('|||')[2].strip().split(' ')
     # word penalty
     if len(scores) == 5:
         scores.append(line.rpartition('|||')[2].strip())
-        sXlf = sXlf + '\n\t<add:scores>'
-        sXlf = sXlf + '\n\t\t<score type="total" value="' + scores[5] + '" />'
-        sXlf = sXlf + '\n\t\t<score type="lm" value="' + scores[0] + '" />'
-        sXlf = sXlf + '\n\t\t<score type="pt0" value="' + scores[1] + '" />'
-        sXlf = sXlf + '\n\t\t<score type="pt1" value="' + scores[2] + '" />'
-        sXlf = sXlf + '\n\t\t<score type="pt2" value="' + scores[3] + '" />'
-        sXlf = sXlf + '\n\t\t<score type="wordpenalty" value="' + scores[4] + '" />'
-        sXlf = sXlf + '\n\t</add:scores>'
+        sXlf += '\n\t<add:scores>'
+        sXlf += '\n\t\t<score type="total" value="%s" />' % (scores[5])
+        sXlf += '\n\t\t<score type="lm" value="%s" />' % (scores[0])
+        sXlf += '\n\t\t<score type="pt0" value="%s" />' % (scores[1])
+        sXlf += '\n\t\t<score type="pt1" value="%s" />' % (scores[2])
+        sXlf += '\n\t\t<score type="pt2" value="%s" />' % (scores[3])
+        sXlf += '\n\t\t<score type="wordpenalty" value="%s" />' % (scores[4])
+        sXlf += '\n\t</add:scores>'
     
-        sXlf = sXlf + '\n\t<add:derivation type="hiero_decoding" id="s' + \
-               snt_no + '_t1_r1_d1">'
+        sXlf += '\n\t<add:derivation type="hiero_decoding" id="s%s_t1_r1_d1">'\
+                % (snt_no)
         chList = [node]
         while chList:
             i = 1
@@ -283,57 +292,51 @@ for (line, snt_no) in xlf:
             # between chList[0] and chList[2].
             for child in chList[0].get_children():
                 chList.insert(i, child)
-                i = i + 1
+                i += 1
     
             # Creates a string with a list of children.
             sChildren = ''
             if len(chList[0].get_children()):
                 sChildren = ' children="'
                 for child_no in chList[0].get_children():
-                    sChildren = sChildren + 's' + snt_no + '_p' + \
-                                str(child_no.iPhraseID) + ','
-                sChildren = sChildren.strip(',') +'"'
+                    sChildren += 's%s_p%s,' % (snt_no, str(child_no.iPhraseID))
+                sChildren = '%s"' % (sChildren.strip(','))
     
             # Creates a phrase with node parameters
             # and with a node string, if exists.
-            sphrase_id = 's' + snt_no + '_t1_r1_d1_p' + str(chList[0].iPhraseID)  
-            sXlf = sXlf + "\n\t\t<phrase id=\"%s\" %s>" %(sphrase_id, sChildren)
+            sPhrase_id = 's%s_t%s_r1_d1_p%s' % (snt_no, T_NUM,
+                                                str(chList[0].iPhraseID))
+            sXlf += '\n\t\t<phrase id=\"%s\" %s>' % (sPhrase_id, sChildren)
             if chList[0].sString:
-                sXlf += get_tokens_xml( chList[0].sString.strip(), sphrase_id )
-                #sXlf = sXlf + '\n\t\t\t<string>' + replace_OOV ( chList[0].sString.strip() ) + \
-                #       '</string>'
-            sXlf = sXlf + '\n\t\t\t<annotation type="class" value="' + \
-                   chList[0].sClass + '" />'
-            sXlf = sXlf + '\n\t\t\t<alignment from="' + chList[0].iFrom + \
-                   '" to="' + chList[0].iTo + '" />'
-            sXlf = sXlf + '\n\t\t</phrase>'
+                sXlf += get_tokens_xml(chList[0].sString.strip(), sPhrase_id)
+            sXlf += '\n\t\t\t<annotation type="class" value="%s" />' % \
+                    (chList[0].sClass)
+            sXlf += '\n\t\t\t<alignment from="%s" to="%s" />' % \
+                    (chList[0].iFrom, chList[0].iTo)
+            sXlf += '\n\t\t</phrase>'
             
             # The processed (printed) node is removed from the list.
             chList.pop(0)
     
-        sXlf = sXlf + '\n\t</add:derivation>'
-        sXlf = sXlf + '\n</alt-trans>'
+        sXlf += '\n\t</add:derivation>'
+        sXlf += '\n</alt-trans>'
         # Removes '\n' in the beginning of the string.
         xmlFiles.append((sXlf.strip(),snt_no))
         #-------------END CREATING AN OUTPUT FILE--------------
         # Counts iterations in main for loop.
-    line_no = line_no + 1
+    line_no += 1
 
 # Creates a directory for output files.
-DIR_NAME = ("t1-%s-%s" % ( INPUT_LANG, OUTPUT_LANG ))
-#DIR_NAME = FILENAME.rpartition('.')[0] + time.strftime('_%y%m%d_%H%M%S') + \
-#           '_output'
-
-os.mkdir( DIR_NAME )
+DIR_NAME = ('t%s-%s-%s' % (T_NUM, INPUT_LANG, OUTPUT_LANG))
+#DIR_NAME = '%s_output' % (time.strftime('_%y%m%d_%H%M%S'))
+os.mkdir(DIR_NAME)
 
 i = 1
 # Prints output string to .xml files.
-for (outputFile,sntNumber) in xmlFiles:
-    
-    filename = ("%s//t1-%s-%s-%.4d.xml" % ( DIR_NAME, INPUT_LANG, OUTPUT_LANG, long(sntNumber) ) )
-    #h = open(DIR_NAME + '//t2-' + INPUT_LANG + '-' + OUTPUT_LANG + \
-    #         '-t1-s' + sntNumbers[i] + '.xml', 'w')
-    h = open(filename,'w')
+for (outputFile, sntNumber) in xmlFiles:
+    filename = ('%s//t%s-%s-%s-%.4d.xml' % (DIR_NAME, T_NUM, INPUT_LANG, \
+                                            OUTPUT_LANG, long(sntNumber)))
+    h = open(filename, 'w')
     h.write(outputFile)
     h.close()
-    i = i + 1
+    i += 1
