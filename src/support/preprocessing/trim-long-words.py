8af203b0e4394cbc1ec9c61c51a3e256c7acc5fa
elav01 <elav01>
2011-03-08 13:09:16 +0000

    Stderr message for trimming long words
    Former-commit-id: 23dcca88d58704c31fa3601f733af563bdb3b7d1

--- src/support/preprocessing/trim-long-words.py
+++ src/support/preprocessing/trim-long-words.py
@@ -1,8 +1,12 @@
 import re
 import sys
 
+counter = 0
+
 for line in sys.stdin:
     for word in line.split(' '):
         if len(word) > 100:
             line = re.sub(word, word[0:100], line)
-    sys.stdout.write(line) 
+            counter +=1
+    sys.stdout.write(line)
+sys.stderr.write("%d words trimmed" % counter) 
