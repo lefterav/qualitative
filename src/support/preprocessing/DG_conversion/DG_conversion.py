b3226fe7ffb22a5ea672509e8b55c5d3ee8f610a
lightnin <lightnin>
2011-04-04 12:08:01 +0000

    Updated DG_conversion to 9.2/10.0 pylint score.  Still needs some re-factoring, I have added some comments inside the file...

--- src/support/preprocessing/DG_conversion/DG_conversion.py
+++ src/support/preprocessing/DG_conversion/DG_conversion.py
@@ -1,9 +1,14 @@
+"""
+Description: Converts data in EuroScript's format in plain text and vice versa
+Author: Eleftherios Avramidis
+"""
 import getopt
 import re
 import sys
 
 
 class _Input:
+    """Stores information about the input."""
     SOURCE_FILENAME = None # source filename
     TARGET_SENTENCES_FILENAME = None # filename of target
     # sentences list
@@ -14,92 +19,105 @@ class _Input:
     TARGET_LANG = '' # target language
 
 
-# Function prints a list of all required and possible parameters.
-def help():
-    print "\nList of parameters:"
-    print "-i [Input filename in DEB format]"
-    print "-t [Output filename for sentence/line extraction  (function 1)]"
-    print "-j [Input filename containing translated sentences in sentence/line format]"
-    print "-o [Output filename re-wrapping translation outcome into the original format (function 2)]"
-    print "-s [Name/id of system]"
-    print "-l [Target language code e.g. en]\n"
-    
+def usage():
+    """Prints a list of all required and possible parameters."""
+    print "\nList of parameters:" \
+    "-i [Input filename in DEB format]" \
+    "-t [Output filename for sentence/line extraction  (function 1)]" \
+    "-j [Input filename containing translated sentences in " \
+      "sentence/line format]" \
+    "-o [Output filename re-wrapping translation outcome into the original " \
+      " format (function 2)]" \
+    "-s [Name/id of system]" \
+    "-l [Target language code e.g. en]\n"
 
-# Function checks, if the user gave all required arguments.
-def check_args(Input):
+
+def check_args(_input):
+    """Checks user provided arguments for completeness."""
     stop = False
-    if not Input.SOURCE_FILENAME:
+    if not _input.SOURCE_FILENAME:
         stop = True
         print "ERROR: Missing parameter -i [Input filename]"
-#    if not Input.SOURCE_SENTENCES_FILENAME:
+#    if not _input.SOURCE_SENTENCES_FILENAME:
 #        stop = True
 #        print "ERROR: Missing parameter -t [Required name of output " \
 #          "file with source sentences list]"
-#    if not Input.TARGET_SENTENCES_FILENAME:
+#    if not _input.TARGET_SENTENCES_FILENAME:
 #        stop = True    
 #        print "ERROR: Missing parameter -j [Name of input file " \
 #          "with target sentences list]"
-#    if not Input.SYSTEM:
+#    if not _input.SYSTEM:
 #        stop = True
 #        print "ERROR: Missing parameter -s [Kind of system format]"
-#    if not Input.TARGET_LANG:
+#    if not _input.TARGET_LANG:
 #        stop = True
 #        print "ERROR: Missing parameter -l [Target language]"
     if stop:
         sys.exit("Program terminated.")
 
 
-# Function reads the command line arguments, saves them to the Input class
-# variables and checks, if the user gave all required arguments.
-def read_commandline_args(Input):
+def read_commandline_args(_input):
+    """Reads arguments, creates Input objects and checks them."""
     try:
         args = getopt.getopt(sys.argv[1:], "i:j:t:o:s:l:")[0]
-    except getopt.GetoptError:
-        help()
-        sys.exit("Program terminated.")
-        
-    for opt, arg in args:
-        if opt == '-i': Input.SOURCE_FILENAME = arg
-        if opt == '-j': Input.TARGET_SENTENCES_FILENAME = arg
-        if opt == '-t': Input.SOURCE_SENTENCES_FILENAME = arg
-        if opt == '-o': Input.OUTPUT_FILENAME = arg
-        if opt == '-s': Input.SYSTEM = arg
-        if opt == '-l': Input.TARGET_LANG = arg
-            
-    check_args(Input)
     
-    return Input
+    except getopt.GetoptError:
+        usage()
+        sys.exit("Program terminated.")
+    
+    for opt, arg in args:
+        if opt == '-i':
+            _input.SOURCE_FILENAME = arg
+        
+        if opt == '-j':
+            _input.TARGET_SENTENCES_FILENAME = arg
+        
+        if opt == '-t':
+            _input.SOURCE_SENTENCES_FILENAME = arg
+        
+        if opt == '-o':
+            _input.OUTPUT_FILENAME = arg
+        
+        if opt == '-s':
+            _input.SYSTEM = arg
+        
+        if opt == '-l':
+            _input.TARGET_LANG = arg
+    
+    check_args(_input)
+    return _input
 
 
-# This function reads the file and returns its content.
 def read_source_file(filename):
+    """Reads the file and returns its content."""
     f = open(filename, 'r')
     content = f.read()
     f.close()
     return content
 
 
-# This function saves source sentences line by line as a list to the file.
 def save_source_sentences(filename, text):
+    """Saves source sentences line by line as a list to the file."""
     f = open(filename, 'w')
     for sentence in text.split('<source>')[1:]:
         f.write('%s\n' % (sentence.split('</source>')[0]))
     f.close()
 
 
-# This function reads the target sentences from the file.
 def read_target_sentences(filename):
+    """Reads the target sentences from the file."""
     f = open(filename, 'r')
     trg_sentences = f.read().strip().split('\n')
     f.close()
     return trg_sentences
 
 
-# This function compares count of source and target sentences.
 def compare_snt_counts(text, trg_sentences):
+    """Compares count of source and target sentences."""
     if text.count('<source>') > len(trg_sentences):
         print 'ERROR: Less target sentences than source sentences!'
         sys.exit("Program terminated.")
+    
     elif text.count('<source>') < len(trg_sentences):
         print 'WARNING: Less source sentences than target sentences!'
 
@@ -107,12 +125,17 @@ def compare_snt_counts(text, trg_sentences):
 # This function replaces the source sentences with the target sentences
 # and adds sysid parameter to the beginning tag.
 def replace_src_with_trg(text, system, target_lang, trg_sentences):
+    """Replaces source with target sentences and adds sysid to the tags."""
     i = 0
     for sentence in text.split('<source>')[1:]:
+        ###
+        ### Use str.format(...) instead of str % (...) !
+        ###
         replaced = re.sub('[^<]*</source>', '%s</target>' % \
                   trg_sentences[i], sentence)
         text = text.replace(sentence, replaced)
         i += 1
+    
     text = text.replace('<source>', '<target system="%s">' % (system))
     text = text.partition(' ')[0] + ' sysid="%s" ' % \
            (system) + text.partition(' ')[2]
@@ -120,8 +143,8 @@ def replace_src_with_trg(text, system, target_lang, trg_sentences):
     return text
 
 
-# This function creates file with target sentences and modifications.
 def create_output_file(filename, output_filename, text):
+    """Creates an output file with target sentences and modifications."""
 #    output_filename = '%s_output.%s' % (filename.rpartition('.')[0], \
 #                        filename.rpartition('.')[2])
     g = open(output_filename, 'w')
@@ -131,32 +154,44 @@ def create_output_file(filename, output_filename, text):
 
 
 
+###
+### Create a "main()" method which does that...
+### Encapsulate thise method in an 'if __name__ == "__main__":' block.
+###
+
 # Reads the command line input arguments. Explains what is the correct syntax
 # in case of wrong input arguments and stops the program run.
-Input = _Input()
-Input = read_commandline_args(Input)
+INPUT = _Input()
+INPUT = read_commandline_args(INPUT)
 
-if (Input.SOURCE_FILENAME and Input.SOURCE_SENTENCES_FILENAME != ''):
+###
+### Why don't you create the _Input instance INSIDE the read_commandline_args
+### method instead???
+###
+
+if (INPUT.SOURCE_FILENAME and INPUT.SOURCE_SENTENCES_FILENAME != ''):
     # Reads the source file.
-    text = read_source_file(Input.SOURCE_FILENAME)
+    TEXT = read_source_file(INPUT.SOURCE_FILENAME)
     
     # Saves source sentences line by line as a list to the file.
-    save_source_sentences(Input.SOURCE_SENTENCES_FILENAME, text)
+    save_source_sentences(INPUT.SOURCE_SENTENCES_FILENAME, TEXT)
     print 
+
 else:
     # Reads the source file.
-    text = read_source_file(Input.SOURCE_FILENAME)
+    TEXT = read_source_file(INPUT.SOURCE_FILENAME)
     
     # Reads the target sentences from the file.
-    trg_sentences = read_target_sentences(Input.TARGET_SENTENCES_FILENAME)
+    TRG_SENTENCES = read_target_sentences(INPUT.TARGET_SENTENCES_FILENAME)
     
     # Compares count of source and target sentences.
-    compare_snt_counts(text, trg_sentences)
+    compare_snt_counts(TEXT, TRG_SENTENCES)
     
     # Replaces the source sentences with the target sentences and adds
     # sysid parameter to the beginning tag.
-    text = replace_src_with_trg(text, Input.SYSTEM, Input.TARGET_LANG, trg_sentences)
+    TEXT = replace_src_with_trg(TEXT, INPUT.SYSTEM, INPUT.TARGET_LANG,
+      TRG_SENTENCES)
     
     # Creates an output file with target sentences and above made modifications.
-    create_output_file(Input.SOURCE_FILENAME, Input.OUTPUT_FILENAME, text)
+    create_output_file(INPUT.SOURCE_FILENAME, INPUT.OUTPUT_FILENAME, TEXT)
 
