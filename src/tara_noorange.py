7eab8f1f7a17d2efc2cc66edbad259223c988bcc
elav01 <elav01>
2011-04-13 16:18:03 +0000

    Functions for splitting test/train data
    Former-commit-id: 22cd047b73a9406beb3197424166b6bd36844238

--- src/tara_noorange.py
+++ src/tara_noorange.py
@@ -16,6 +16,7 @@ from os import getenv
 import os
 import pickle
 import orange, orngLR
+from sentence.dataset import DataSet
 
 from io.input.orangereader import OrangeData
 from io.output.xmlwriter import XmlWriter
@@ -43,43 +44,116 @@ class Experiment:
                             
                             }
     
-    def split_corpus(self):
-        filename = os.getenv("HOME") + "/taraxu_data/wmt08-humaneval-data/wmt08_human_binary.jcml"
+    def split_corpus(self, filename, filename_train, filename_test, proportion=0.1):
+        #filename = os.getenv("HOME") + "/taraxu_data/wmt08-humaneval-data/wmt08_human_binary.jcml"
         class_name = "rank"
-        self.desired_attributes = []
+        
+        #self.desired_attributes = []
         
         #Load data from external file
         pdr = XmlReader(filename) 
         dataset =  pdr.get_dataset()
         
         
-        desired_attributes=['langsrc', 'langtgt', 'testset']
+        self.desired_attributes = {'tgt-1_system' : 'd', 
+                                   'tgt-2_system' : 'd',
+                                   'tgt-3_system' : 'd',
+                                   'tgt-4_system' : 'd',
+                                   'tgt-5_system' : 'd',
+                                   'segment_id' : 'd', 
+                                   'id' : 'd',
+                                   'document_id' : 'd',
+                                   'judge_id': 'd'}
+        
         
         #convert data in orange format
         orangedata = OrangeData( dataset, class_name, self.desired_attributes )
         
-        orig_dataset = orangedata.get_dataset()
+        #orig_dataset = orangedata.get_dataset()
         
         i=0
         
         #split data the orange way (stratified)
-        [training_part, test_part] = orangedata.split_data(0.1)
+        [training_part, test_part] = orangedata.split_data(proportion)
         training_data = OrangeData(training_part, class_name)
         test_data = OrangeData(test_part, class_name)
         
         print "TESTSET------"
         orig_test_data = test_data.get_dataset()    
         xmlwriter = XmlWriter(orig_test_data)
-        xmlwriter.write_to_file(os.getenv("HOME") + "/workspace/TaraXUscripts/data/test08.xml")
+        xmlwriter.write_to_file(filename_test)
         orig_test_data = None
     
         print "TRAINSET------"
         orig_train_data = training_data.get_dataset()    
         xmlwriter = XmlWriter(dataset)
-        xmlwriter.write_to_file(os.getenv("HOME") + "/workspace/TaraXUscripts/data/train08.xml")
+        xmlwriter.write_to_file(filename_train)
     
+    """
+    Splits a judgments set into two sets, one testset and one training set. Allows for extra criteria 
+    for the test set, such as no-repetitive judgments and clear ranking (no ties)
+    @param  filename: pointing to the xml file containing the original full set to be read from
+    @param filename_train: xml file to be created with the training data
+    @param filename_test: xml file to be created with the test data
+    @param ties_threshold: test sentences will be dropped, if they have nore than n ties when ranked
+    """
+    def get_test_sentences(self, filename, filename_train, filename_test, ties_threshold = 1, max_sentences = 100):
+        reader = XmlReader(filename)
+        parallelsentences = reader.get_parallelsentences()
         
-    
+        prev_id = None
+        repeats = 1
+        single_ids = []
+        prev_ps = None
+        
+        #get unique judgments
+        for parallelsentence in parallelsentences:
+            if prev_id == parallelsentence.get_attribute("id"):
+                repeats += 1
+            elif prev_ps:
+                repeats = 1
+                #choose the sentence as test sentence, only if it appears once and 
+                #doesn't have to many ranking ties
+                if repeats == 1 and self.count_ties(prev_ps) < ties_threshold:
+                    single_ids.append(prev_id)
+                
+            prev_id = parallelsentence.get_attribute("id")
+            prev_ps = parallelsentence
+       
+        os.sys.stderr.write("%d sentences chosen\n" % len(single_ids))
+        
+        single_ids = single_ids[0:max_sentences-1]
+        testset = []
+        trainset = []
+        
+        for parallelsentence in parallelsentences:
+            if parallelsentence.get_attribute("id") in single_ids:
+                testset.append(parallelsentence)
+            else:
+                trainset.append(parallelsentence)
+        
+        
+        tmp_dataset = DataSet(trainset)
+        xmlwriter = XmlWriter(tmp_dataset)
+        xmlwriter.write_to_file(filename_train)
+        
+        tmp_dataset = DataSet(testset)
+        xmlwriter = XmlWriter(tmp_dataset)
+        xmlwriter.write_to_file(filename_test)
+            
+        
+    def count_ties(self, parallelsentence):
+        ranks = []
+        ties = 0
+        for translation in parallelsentence.get_translations():
+            ranks.append (translation.get_attribute("rank"))
+            
+        for rank_value in set(ranks):
+            ranks_count = ranks.count(rank_value)
+            if (ranks_count > 1):
+                ties += ranks_count-1
+        return ties
+                
     
     def add_external_features(self, given_filename="evaluations_all.jcml"):
         
@@ -208,15 +282,18 @@ if __name__ == '__main__':
     #split_corpus()
     #add_external_features("train08.xml")
     
-    myexperiment = Experiment()
+    #myexperiment = Experiment()
+
+    exp = Experiment()
+    exp.get_test_sentences("/home/elav01/taraxu_data/wmt10-humaneval-data/wmt10.jcml", "/home/elav01/taraxu_data/wmt10-humaneval-data/wmt10-train.jcml" , "/home/elav01/taraxu_data/wmt10-humaneval-data/wmt10-test.jcml")
     
     #myexperiment.add_external_features("test08.xml")
     
-    train_filename = dir + "featured_train08.xml"
-    classifiers = myexperiment.train_classifiers('/home/elav01/workspace/TaraXUscripts/src/tmpa04du_.tab')
-    #classifiers = myexperiment.train_classifiers('/home/elav01/workspace/TaraXUscripts/src/tmpa04du_.tab')
-    test_filename = dir + "featured_test08.xml"
-    myexperiment.test_classifiers(classifiers, test_filename)
+#    train_filename = dir + "featured_train08.xml"
+#    classifiers = myexperiment.train_classifiers('/home/elav01/workspace/TaraXUscripts/src/tmpa04du_.tab')
+#    #classifiers = myexperiment.train_classifiers('/home/elav01/workspace/TaraXUscripts/src/tmpa04du_.tab')
+#    test_filename = dir + "featured_test08.xml"
+#    myexperiment.test_classifiers(classifiers, test_filename)
     
         
     
