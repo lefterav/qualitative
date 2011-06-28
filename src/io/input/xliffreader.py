5bba9a4f5705debff8f216eb5256c8842a1bad92
lupo01 <lupo01>
2011-06-28 15:58:52 +0000

    To get_attributes() added token_count attribute and global weights attributes. Attribute names extended by tool id.
    Former-commit-id: 4c30618fc0b6d0458207d9474ac3005c4dc539fc

--- src/io/input/xliffreader.py
+++ src/io/input/xliffreader.py
@@ -6,6 +6,7 @@ Created on Jun 27, 2011
 @author: jogin
 '''
 
+
 from xml.dom.minidom import parse
 from sentence.parallelsentence import ParallelSentence
 from sentence.sentence import SimpleSentence
@@ -24,7 +25,8 @@ class XliffReader(object):
         Constructor. Creates an XML object that handles META-NET XLIFF data
         @param input_filename: the name of XML file 
         @type input_filename: string
-        @param load: by turning this option to false, the instance will be initialized without loading everything into memory
+        @param load: by turning this option to false, the instance will be 
+                     initialized without loading everything into memory
         @type load: boolean 
         """
         self.input_filename = input_filename
@@ -35,38 +37,47 @@ class XliffReader(object):
     
     def load(self):
         """
-        Loads the data of the file into memory. It is useful if the Classes has been asked not to load the filename upon initialization
+        Loads the data of the file into memory. It is useful if the Classes has
+        been asked not to load the filename upon initialization
         """
         self.xmlObject = parse(self.input_filename)
     
     
     def get_dataset(self):
         """
-        Returs the contents of the XML file into an object structure, which is represented by the DataSet object
-        Note that this will cause all the data of the XML file to be loaded into system memory at once. 
-        For big data sets this may not be optimal, so consider sentence-by-sentence reading with SAX (saxjcml.py)
+        Returs the contents of the XML file into an object structure, which is 
+        represented by the DataSet object
+        Note that this will cause all the data of the XML file to be loaded 
+        into system memory at once. 
+        For big data sets this may not be optimal, so consider 
+        sentence-by-sentence reading with SAX (saxjcml.py)
         @rtype: sentence.dataset.DataSet
         @return: A data set containing all the data of the XML file
         """
         return DataSet(self.get_parallelsentences(), self.get_attributes())
-    
-    
-    def get_attributes(self):
-        """
-        @return a list of the names of the attributes contained in the XML file
-        """
-        judgedCorpus = self.xmlObject.getElementsByTagName(self.TAG_DOC)
-        sentenceList = judgedCorpus[0].getElementsByTagName(self.TAG_SENT)
-        attributesKeySet = set()
-        
-        for xmlEntry in sentenceList:
-            for attributeKey in xmlEntry.attributes.keys():
-                attributesKeySet.add(attributeKey)            
-        return list(attributesKeySet)
-    
+
     
     def length(self):
         return len(self.xmlObject.getElementsByTagName('trans-unit'))
+    
+    
+    def get_weights(self, tool_id):
+        """
+        Finds the global weights for particular tool ID in file. Used by
+        function get_parallelsentences().
+        @tool_id: tool id
+        @type string
+        @return: global weights
+        @type: list
+        """
+        weights = []
+        tools = self.xmlObject.getElementsByTagName('tool')
+        for tool in tools:
+            if tool.getAttribute('tool-id') == tool_id:
+                for elem in tool.getElementsByTagName('metanet:weight'):
+                    weights.append(('%s-%s-%s' % ('global', elem.getAttribute('type'), tool_id), \
+                                    elem.getAttribute('value')))
+        return weights
         
         
     def get_parallelsentences(self):
@@ -97,14 +108,19 @@ class XliffReader(object):
                         tgt = SimpleSentence(unescape(transunit_tgt.childNodes[0].nodeValue))
 
                 # alt-trans_tool_id parsing
-                tgt.add_attribute('system', altTrans.getAttribute('tool-id'))
+                tool_id = altTrans.getAttribute('tool-id')
+                tgt.add_attribute('tool_id', tool_id)
+                
+                # add global weights for particular tool id
+                for weight in self.get_weights(tool_id):
+                    tgt.add_attribute(weight[0], weight[1])
                 
                 # alt-trans_score parsing 
                 alttrans_scores = altTrans.getElementsByTagName("metanet:scores")
                 for alttrans_score in alttrans_scores:
                     if alttrans_score in altTrans.childNodes:
                         for elem in alttrans_score.getElementsByTagName("metanet:score"):
-                            tgt.add_attribute(elem.getAttribute('type'), \
+                            tgt.add_attribute('%s-%s' % (tool_id, elem.getAttribute('type')), \
                                               elem.getAttribute('value'))
                 
                 # alt-trans_annotation parsing
@@ -113,7 +129,7 @@ class XliffReader(object):
                     if alttrans_annotation in altTrans.childNodes:
                         for elem in alttrans_annotation.getElementsByTagName("metanet:annotation"):
                             if elem in alttrans_annotation.childNodes:
-                                tgt.add_attribute(elem.getAttribute('type'), \
+                                tgt.add_attribute('%s-%s' % (tool_id, elem.getAttribute('type')), \
                                                   elem.getAttribute('value'))
                             
                 # alt-trans_OOV_words
@@ -123,7 +139,19 @@ class XliffReader(object):
                     if alttrans_annotation.getAttribute('type') == 'oov' \
                      or alttrans_annotation.getAttribute('type') == 'OOV':
                         OOV_count += int(alttrans_annotation.getAttribute('value'))
-                tgt.add_attribute('OOV_count', str(OOV_count))
+                tgt.add_attribute('%s-%s' % (tool_id, 'OOV_count'), str(OOV_count))
+                
+                # alt-trans token_count parsing
+                token_count = {}
+                for token in altTrans.getElementsByTagName('metanet:token'):
+                    d = token.getAttribute('id').partition('_d')[2].partition('_')[0]
+                    if d not in token_count:
+                        token_count[d] = 1
+                    else:
+                        token_count[d] = int(token_count[d]) + 1
+                for d_count in token_count:
+                    tgt.add_attribute('%s-%s%s-%s' % (tool_id, 'd', d_count, \
+                                          'token-count'), token_count[d_count])
                 
                 # add a target with new attributes to the list
                 tgt_list.append(tgt)
@@ -133,7 +161,6 @@ class XliffReader(object):
             for transunit_ref in transUnit.childNodes:
                 if transunit_ref.nodeName == 'target':
                     ref = SimpleSentence(unescape(transunit_ref.childNodes[0].nodeValue))
-            
             # create an object of parallel sentence
             ps = ParallelSentence(src, tgt_list, ref)
                 
@@ -141,8 +168,10 @@ class XliffReader(object):
         
         return ps
     
+
     def unload(self):
         self.xmlObject.unlink()
+    
     
     def __read_attributes__(self, xmlEntry):
         """
