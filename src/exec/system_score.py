7a6cd0e4e7664bdd40726193250e8f5be570a66e
elav01 <elav01>
2011-07-02 15:21:53 +0000

    Sax system scoring

--- src/exec/system_score.py
+++ src/exec/system_score.py
@@ -3,14 +3,25 @@
 @author: lefterav
 '''
 import sys
+from sentence.rankhandler import RankHandler
+from xml.sax import make_parser
 
 
 if __name__ == '__main__':
-    from autoranking import AutoRankingExperiment
-    exp = AutoRankingExperiment()
-    scoresprint = exp.print_system_score(sys.argv[1], sys.argv[2])
-    f = open(sys.argv[3], 'w')
-    for entry in scoresprint:
-        f.write(entry)
-    f.close()
+#    from autoranking import AutoRankingExperiment
+#    exp = AutoRankingExperiment()
+#    scoresprint = exp.print_system_score(sys.argv[1], sys.argv[2])
+#    f = open(sys.argv[3], 'w')
+#    for entry in scoresprint:
+#        f.write(entry)
+#    f.close()
+    from io.saxscoring import SaxSystemScoring
+    saxreader = SaxSystemScoring("rank", sys.argv[2])
+    fileinput = open(sys.argv[1])
+    
+    myparser = make_parser()
+    myparser.setContentHandler(saxreader)
+    myparser.parse(fileinput)
+    fileinput.close
+
         
