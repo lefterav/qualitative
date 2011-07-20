3d10ed15b86e66e8c878a856e958f45e4779a6f4
lupo01 <lupo01>
2011-07-20 08:49:42 +0000

    Former-commit-id: d767f1c9ba96bf59706bb82b2e0334d65d7ea0cb

---
+++ src/io/input/rankreader.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on Jun 27, 2011
+
+@author: jogin
+'''
+
+
+from xml.dom.minidom import parse
+from sentence.parallelsentence import ParallelSentence
+from sentence.sentence import SimpleSentence
+from xml.sax.saxutils import unescape
+ 
+
+class RankReader(object):
+    """
+    classdocs
+    """
+
+    def __init__(self, input_filename, load = True):
+        """
+        Constructor. Creates an XML object that handles ranking file data
+        @param input_filename: the name of XML file
+        @type input_filename: string
+        @param load: by turning this option to false, the instance will be 
+                     initialized without loading everything into memory
+        @type load: boolean 
+        """
+        self.input_filename = input_filename
+        self.loaded = load
+        if load:
+            self.load()
+    
+    def load(self):
+        """
+        Loads the data of the file into memory. It is useful if the Classes has
+        been asked not to load the filename upon initialization
+        """
+        self.xmlObject = parse(self.input_filename)
+
+    def unload(self):
+        self.xmlObject.unlink()
+
+    def get_rankings(self):
+        """
+        This function parses a ranking xml file and returns a list of parallel
+        sentence objects.
+        @return ps_list: list of tuples in format (ranking-item_id, ParallelSentence)
+        @type ps_list: list of tuples
+        """
+        r_items = self.xmlObject.getElementsByTagName('ranking-item')
+        ps_list = []
+        for r_item in r_items:
+            stc_id = r_item.getAttribute('sentence_id')
+            src = ''
+            tgt_list = []
+            for rank_child in r_item.childNodes:
+                if rank_child.nodeName == 'source':
+                    src = SimpleSentence(unescape(rank_child.childNodes[0].nodeValue))
+                elif rank_child.nodeName != '#text':                    
+                    tgt = SimpleSentence(unescape(rank_child.childNodes[0].nodeValue))
+                    tgt.add_attribute('system', rank_child.getAttribute('name'))
+                    tgt.add_attribute('rank', rank_child.getAttribute('rank'))
+                    tgt_list.append(tgt)
+            
+            ps = ParallelSentence(src, tgt_list)
+            ps.add_attributes({'sentence-id': stc_id})
+            ps_list.append(ps)
+        return ps_list
+        
