641449aff9f4c0767f1d9830b06d1281b1bc8fd6
elav01 <elav01>
2011-08-04 12:18:41 +0000

    Minor improvements on parallel sentence handling
    Former-commit-id: 09991127fcfc169df97fcede60165c531650b5ae

---
+++ src/exec/rank_correlation_analysis.py
@@ -0,0 +1,15 @@
+'''
+Created on Aug 4, 2011
+
+@author: Eleftherios Avramidis
+'''
+
+
+"""
+    Reads two versions of the same dataset, one with estimated ranking and one with gold rankings. 
+    It then calculates the ranking correlation coefficients. 
+"""
+
+
+if __name__ == '__main__':
+    pass
--- src/sentence/pairwiseparallelsentenceset.py
+++ src/sentence/pairwiseparallelsentenceset.py
@@ -7,6 +7,10 @@ Created on Jul 12, 2011
 '''
 
 class PairwiseParallelSentenceSet():
+    """
+    A set of pairwise parallel sentences, in order to facilitate pairwise comparisons etc. 
+    Works as a wrapper over a dictionary, where pairs are indexed based on the system names of the 2 target translations.
+    """
     
     def __init__(self, pps_list):
         """
@@ -18,14 +22,25 @@ class PairwiseParallelSentenceSet():
         for pps in pps_list:
             self.pps_dict[pps.get_system_names()] = pps
     
-    def get_pairwise_parallelsentence(self, system_names):
+    def get_pairwise_parallelsentence(self, system_names, order = True):
         """
-        @param system_names: names of translation systems
+        Provides the pairwise parallel sentence, whose target sentences provide output by the two given systems
+        @param system_names: pair of translation system names
         @type system_names: tuple of strings
-        @return:  
+        @param order: whether the order of the systems in the tuple is important, or not
+        @type order: boolean
+        @return: the pairwise parallel sentence that contains the outputs of the two given systems
         @return type: PairwiseParallelSentence object
         """
+            
         try:
             return self.pps_dict[system_names]
         except:
-            print "At least one of system names is missing in xml file."
+            if not order:
+                try:
+                    system_names_reversed = (system_names[1], system_names[0])
+                    return self.pps_dict[system_names_reversed]
+                except:
+                    print "At least one of system names is missing."
+            else:
+                print "At least one of system names is missing."
