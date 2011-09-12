6e59d1a02c3521b514ecd13f910fd22c30d8231a
elav01 <elav01>
2011-09-12 17:10:14 +0000

    Former-commit-id: 4e99f3b97b4e57829ea0bcc4dba8f0a9175f8f40

--- src/exec/iwslt11.py
+++ src/exec/iwslt11.py
@@ -11,7 +11,7 @@ import sys
 import re
 
 if __name__ == '__main__':
-    step = sys.argv[0]
+    step = int(sys.argv[0])
     mydir = "/share/taraxu/selection-mechanism/iwslt11"
     targetdir = "/share/taraxu/vilar/iwslt11/ibm1/hyps"
     extension = ".hyp"
@@ -20,27 +20,27 @@ if __name__ == '__main__':
     pattern_name = "([^/]*)_IWSLT11"
     langpair = "en-fr"
     
-    sourcefile = "/share/taraxu/vilar/iwslt11/data/preproc/devsets/IWSLT11.TALK.tst2010.en-fr.en.fc"
-    sourcefilename_nopath = re.findall("([^/]*)$")[0]
-    jcmlfilename = "%s/%s.jcml" % (mydir, sourcefilename_nopath)
+    sourcefilename = "/share/taraxu/vilar/iwslt11/data/preproc/devsets/IWSLT11.TALK.tst2010.en-fr.en.fc"
+    sourcefilenamename_nopath = re.findall("([^/]*)$", sourcefilename)[0]
+    jcmlfilename = "%s/%s.jcml" % (mydir, sourcefilenamename_nopath)
     
     exp = Experiment()
     
     if step == 0:
         
-        targetfiles = [filename for filename in os.listdir (targetdir) if filename.endswith(extension)]
-        dataset = LineReader(sourcefile, targetfiles, langpair, testset, pattern_name).get_dataset()
+        targetfiles = ["%s,%s" % (targetdir, filename) for filename in os.listdir (targetdir) if filename.endswith(extension)]
+        dataset = LineReader(sourcefilename, targetfiles, langpair, testset, pattern_name).get_dataset()
         print "writing"
         XmlWriter(dataset).write_to_file(jcmlfilename)
     
     if step == 1:
         print "English parser features"
-        bpfile = sourcefile.replace("jcml", "bp.en.jcml")
+        bpfile = sourcefilename.replace("jcml", "bp.en.jcml")
         exp.add_b_features_batch(jcmlfilename, bpfile, "http://blade-1.dfki.uni-sb.de:8682", "en")
 
     if step == 2:
         print "French parser features"
-        bpfile = sourcefile.replace("jcml", "bp.fr.jcml")
+        bpfile = sourcefilename.replace("jcml", "bp.fr.jcml")
         exp.add_b_features_batch(jcmlfilename, bpfile, "http://blade-1.dfki.uni-sb.de:8683", "fr")
     
 
