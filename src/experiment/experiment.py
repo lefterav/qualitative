38a3cb927defc1f8823dddc3d8911f2dabe245f5
elav01 <elav01>
2011-09-13 14:21:26 +0000

    Devising general-use experiment script
    Former-commit-id: d0f042f9ccfec81b2a6e94e0c2bf4a408dad50ee

--- src/experiment/experiment.py
+++ src/experiment/experiment.py
@@ -11,22 +11,48 @@ class Experiment(object):
     input = []
     output = []
     phases = []
+    completed = False
 
-
-    def __init__(self, params):
+    def __init__(self):
         '''
         Constructor
         '''
         pass
 
     def run(self):  
+        output = self.input
+        for i in range (len(self.phases)):
+#        for phase in self.phases:
+            self.phases[i].input = output
+            self.phases[i].run()
+            output = self.phases[i].output
+        
+        self.completed = True
+        
+
+    def __get_executable_phases__(self):
+        """
+        Return a list of the tasks that have all their requirements met
+        """
+        
+        executable_phases = []
+        
         for phase in self.phases:
-            nextinput = phase.input
-            for task in phase.tasks:
-                if False not in [req.completed for req in task.required]:
-                    task.input = nextinput
-                    task.run()
-                    task.completed = True
-                    nextinput = task.output
+            allcompleted = True
+            for requirement in phase.required:
+                if not requirement.completed:
+                    allcompleted = False
+            if allcompleted:
+                executable_phases.append(phase)
+        
+        return executable_phases
+
+
+#            for task in phase.tass:
+#                if False not in [req.completed for req in task.required]:
+#                    task.input = nextinput
+#                    task.run()
+#                    task.completed = True
+#                    nextinput = task.output
                 
         
