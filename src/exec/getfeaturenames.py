c33e347cfa23e8c8c6b66256f21ccd807ed1437c
elav01 <elav01>
2011-09-21 22:23:32 +0000

    fixed issue with inexistent attribute names
    Former-commit-id: cdf96223de40780b8c27b5b7211669132923d5bf

--- src/exec/getfeaturenames.py
+++ src/exec/getfeaturenames.py
@@ -8,13 +8,15 @@ a = argv[1]
 attlist = a.split('" ')
 for att in attlist:
     att_name = att.split("=")[0]
-    if att_name not in ["rank"] and not att_name.endswith("berkeley_tree"):
-        allfeatures.append(att_name)
-        if att_name.endswith("ratio"):
-            ratiofeatures.append(att_name)
-        else:
-            noratiofeatures.append(att_name)
-            
+    if att_name not in ["rank"] and not att_name.endswith("berkeley-tree"):
+        for i in range(1,3):
+            att_name = "tgt-%d_%s" % (i, att_name)
+            allfeatures.append(att_name)
+            if att_name.endswith("ratio"):
+                ratiofeatures.append(att_name)
+            else:
+                noratiofeatures.append(att_name)
+                
 print "all features:\n", ",".join(allfeatures)
 print "single features:\n", ",".join(noratiofeatures)
 print "ratio features:\n", ",".join(ratiofeatures)
