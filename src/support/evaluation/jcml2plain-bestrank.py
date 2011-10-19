d122f9861462292966fd796663963f22d4fd07e1
elav01 <elav01>
2011-10-19 15:55:01 +0000

    extract best ranked sentence of JCML files, to simple txt format
    Former-commit-id: e7ea3d5f68a6b20428b366bdb391e4b4143cda40

--- src/support/evaluation/jcml2plain-bestrank.py
+++ src/support/evaluation/jcml2plain-bestrank.py
@@ -3,14 +3,14 @@ Created on 19 Oct 2011
 
 @author: Eleftherios Avramidis
 '''
-
+import sys
 
 if __name__ == '__main__':
     from io.input.jcmlreader import JcmlReader
     #filename_in = "/home/elav01/taraxu_data/ml4hmt/5/classified.jcml"
     #filename_out = "/home/elav01/taraxu_data/ml4hmt/5/bestselected.txt"
-    filename_in = argv[1]
-    filename_out = argv[2]
+    filename_in = sys.argv[1]
+    filename_out = sys.argv[2]
     
     file_out = open(filename_out, 'w')
     rank_attribute = "orig_rank"
