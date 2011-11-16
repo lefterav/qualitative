a33eebc04900ccbb26c7529aab9460aa50073e3b
elav01 <elav01>
2011-11-16 14:32:20 +0000

    Updates for the calculation of the inter-annotator agreement

--- src/support/evaluation/interannotator.py
+++ src/support/evaluation/interannotator.py
@@ -157,23 +157,23 @@ def print_total_pis(systems, ranking_file_sets):
 
 systems = [  "lucy", "moses", "trados", "google"  ]
 
-#ranking_files_deen = [  "/home/elav01/taraxu_data/r1/results/19-1-WMT08-de-en-ranking.xml", "/home/elav01/taraxu_data/r1/results/6-1-WMT08-de-en-ranking.xml"]
-#ranking_files_ende = ["/home/elav01/taraxu_data/r1/results/10-1-WMT08-en-de-ranking.xml", "/home/elav01/taraxu_data/r1/results/15-1-WMT08-en-de-ranking.xml", "/home/elav01/taraxu_data/r1/results/25-1-WMT08-en-de-ranking.xml"]
-ranking_files_ende = ["/home/elav01/taraxu_data/ml4hmt-submissions/rankings/1-ML4HMT-DCU-Task-es-en-ranking.xml","/home/elav01/taraxu_data/ml4hmt-submissions/rankings/2-ML4HMT-DFKI-Task-es-en-ranking.xml","/home/elav01/taraxu_data/ml4hmt-submissions/rankings/3-ML4HMT-BM-Task-es-en-ranking.xml"]
+ranking_files_deen = [  "/home/elav01/taraxu_data/r1/results/19-1-WMT08-de-en-ranking.xml", "/home/elav01/taraxu_data/r1/results/6-1-WMT08-de-en-ranking.xml"]
+ranking_files_ende = ["/home/elav01/taraxu_data/r1/results/10-1-WMT08-en-de-ranking.xml", "/home/elav01/taraxu_data/r1/results/15-1-WMT08-en-de-ranking.xml", "/home/elav01/taraxu_data/r1/results/25-1-WMT08-en-de-ranking.xml"]
+#ranking_files_ende = ["/home/elav01/taraxu_data/ml4hmt-submissions/rankings/1-ML4HMT-DCU-Task-es-en-ranking.xml","/home/elav01/taraxu_data/ml4hmt-submissions/rankings/2-ML4HMT-DFKI-Task-es-en-ranking.xml","/home/elav01/taraxu_data/ml4hmt-submissions/rankings/3-ML4HMT-BM-Task-es-en-ranking.xml"]
  
 #ranking_files = ["/home/elav01/taraxu_data/r1/results/10-1-WMT08-en-de-ranking.xml", "/home/elav01/taraxu_data/r1/results/15-1-WMT08-en-de-ranking.xml"]
 #ranking_files = ["/home/elav01/taraxu_data/r1/results/10-1-WMT08-en-de-ranking.xml", "/home/elav01/taraxu_data/r1/results/25-1-WMT08-en-de-ranking.xml"]
 #ranking_files = ["/home/elav01/taraxu_data/r1/results/15-1-WMT08-en-de-ranking.xml", "/home/elav01/taraxu_data/r1/results/25-1-WMT08-en-de-ranking.xml"]
 
 print "Overall correlations scores"
-#print print_total_pis(systems, [("de-en", ranking_files_deen), ("en-de", ranking_files_ende)])
-print print_total_pis(systems, [("es-en", ranking_files_ende)])
+print print_total_pis(systems, [("de-en", ranking_files_deen), ("en-de", ranking_files_ende)])
+#print print_total_pis(systems, [("es-en", ranking_files_ende)])
 
 print "English to German\n"
 print print_pairwise_pis(systems, ranking_files_ende)
 
-#print "German to English\n"
-#print print_pairwise_pis(systems, ranking_files_deen)
+print "German to English\n"
+print print_pairwise_pis(systems, ranking_files_deen)
 
 
 
