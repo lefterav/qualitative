c1767f3235d9170083e3f061893ba770f2188cf2
elav01 <elav01>
2011-11-30 15:57:19 +0000

    First draft using Ruffus
    Former-commit-id: 7f16dbdf8eac5061401f5886927772b7fb3d2332

--- src/experiment/autoranking.py
+++ src/experiment/autoranking.py
@@ -4,36 +4,56 @@ Created on 20 Oct 2011
 @author: elav01
 '''
 
+from ruffus import *
 
-class ExpDataReader(AccumulativeTask):
+@files(None, 'deleteme.preprocessed.data')
+def experiment_data_reader(input_file, output_file):
+    f = open(output_file, "w")
+    f.write("test")
+    f.close()
+
     
-    def execute(self):
-        
-
-
-class Autoranking:
     
-    def define(self):
-        executable = []
-        training_data_reader = ExpDataReader(inputfilenames = self.training_filenames)
-        #executable.append(training_data_reader) #dataset object and jcmlfile
+@files('deleteme.preprocessed.data', 'deleteme.pairwise.data')
+def experiment_data_pairwiser(input_file, output_file):
+    print "pairwise"  
+    f = open(input_file, "r")
+    print f.read(input_file)
+    f.close()
+    
+
+pipeline_run([experiment_data_pairwiser])
+
+
+#class ExpDataReader(AccumulativeTask):
+    
+#    def execute(self):
         
-        training_data_pairwiser = ExpDataPairwiser(input = training_data_reader) #pairwise dataset object and jcmlfile
-        training_data_converter = ExpDataOrangeConverter(input = training_data_pairwiser) #orange object and tab file
-        classifier_trainer = ExpClassifierTrainer(input = training_data_converter) #classifier object and pickle
+
+
+#class Autoranking:
+    
+#    def define(self):
+#        executable = []
+#        training_data_reader = ExpDataReader(inputfilenames = self.training_filenames)
+#        executable.append(training_data_reader) #dataset object and jcmlfile
+#        
+#        training_data_pairwiser = ExpDataPairwiser(input = training_data_reader) #pairwise dataset object and jcmlfile
+#        training_data_converter = ExpDataOrangeConverter(input = training_data_pairwiser) #orange object and tab file
+#        classifier_trainer = ExpClassifierTrainer(input = training_data_converter) #classifier object and pickle
+#        
+#        test_data_reader = ExpDataReader(inputfilenames = self.testing_filenames)        
         
-        test_data_reader = ExpDataReader(inputfilenames = self.testing_filenames)        
+#        test_data_pairwiser = ExpDataPairwiser(input = test_data_reader) #pairwise dataset object and jcmlfile
         
-        test_data_pairwiser = ExpDataPairwiser(input = test_data_reader) #pairwise dataset object and jcmlfile
+#        test_data_converter = ExpDataOrangeConverter(input = test_data_pairwiser) #orange object and tab file
         
-        test_data_converter = ExpDataOrangeConverter(input = test_data_pairwiser) #orange object and tab file
-        
-        classifier_tester = ExpClassifierTester(classifier = classifier_trainer, input = test_data_converter)
-        results_analyzer = ResultsAnalyzer(results = classifier_tester, input = test_data_reader)
+#        classifier_tester = ExpClassifierTester(classifier = classifier_trainer, input = test_data_converter)
+#        results_analyzer = ResultsAnalyzer(results = classifier_tester, input = test_data_reader)
         
         
 
 
 
-if __name__ == '__main__':
-    pass
+#if __name__ == '__main__':
+#    pass
