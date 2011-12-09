1a28009c51487ae617e33d1e082a88b36329a7c3
elav01 <elav01>
2011-12-09 18:32:24 +0000

    First Ruffus pipeline steps

--- src/experiment/autoranking/autoranking.py
+++ src/experiment/autoranking/autoranking.py
@@ -5,24 +5,50 @@ Created on 20 Oct 2011
 '''
 
 from ruffus import *
+import shutil
+import os
+from io.input.jcmlreader import JcmlReader
+from sentence.rankhandler import RankHandler
 
-@files(None, 'deleteme.preprocessed.data')
-def experiment_data_reader(input_file, output_file):
-    f = open(output_file, "w")
-    f.write("test")
-    f.close()
+from experiment.autoranking.bootstrap import cfg
 
-    
-    
-@files('deleteme.preprocessed.data', 'deleteme.pairwise.data')
-def experiment_data_pairwiser(input_file, output_file):
-    print "pairwise"  
-    f = open(input_file, "r")
-    print f.read(input_file)
-    f.close()
+#TODO: Use stringIO to pass objects loaded into memory???
+
+path = cfg.get('general','path')
+try:
+    os.mkdir(path)
+except OSError:
+    pass
+os.chdir(path)
+
+dataset = [None]
+
+def _retrieve_sentences(obj, output_file):
+    if obj == [None]:
+        return JcmlReader(output_file).get_parallelsentences()
+    else:
+        return obj
+
+
+@files(None, 'data.input.jcml', cfg.get('training', 'filename'), dataset)
+def data_fetch(input_file, output_file, external_file, dataset):
+    '''
+    Fetch file and place it comfortably in the working directory
+    then load the data into memory
+    '''
+    shutil.copy(external_file, output_file)
+    dataset[0] = _retrieve_sentences(dataset, output_file)
+
+#@active_if(True)
+@files(data_fetch, 'data.pairwise.jcml', dataset)
+def data_pairwise(input_file, output_file, dataset, cfg.get('training', )):
+    print input_file
+    print "received dataset with value" , dataset
+    RankHandler(self.class_name).get_pairwise_from_multiclass_set(parallelsentences, allow_ties, self.exponential)
     
 
-pipeline_run([experiment_data_pairwiser])
+
+pipeline_run([data_pairwise], [data_fetch], multiprocess = 1)
 
 
 #class ExpDataReader(AccumulativeTask):
