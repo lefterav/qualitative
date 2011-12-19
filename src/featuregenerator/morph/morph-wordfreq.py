86d9f127a12ae1a6502846486153a9d8e5b2301b
elav01 <elav01>
2011-12-19 14:06:53 +0000

    Morphessor preprocessing script with illegal character substitution, encoding conversion and limit for test purposes

--- src/featuregenerator/morph/morph-wordfreq.py
+++ src/featuregenerator/morph/morph-wordfreq.py
@@ -6,14 +6,39 @@ Created on 19 Dec 2011
 import nltk
 import sys
 from operator import itemgetter
+import codecs
 
 fd = nltk.FreqDist()
 
 filename = sys.argv[1]
-for line in open(filename, 'r'):
-    for word in nltk.word_tokenize(line):
+filename_out = sys.argv[2]
+try:
+    encoding_in = sys.argv[3]
+except:
+    encoding_in = "utf-8"
+try:
+    encoding_out = sys.argv[4]
+except:
+    encoding_out = "utf-8"
+try:
+    limit = int(sys.argv[5])
+except:
+    limit  = -1
+
+for line in codecs.open(filename, 'r', encoding_in):
+    for word in line.split():
+            word = word.replace("*", "<STAR>")
+            word = word.replace("/", "<SLASH>")
+	    word = word.replace("#", "<HASH>")
+	    word = word.replace(" ", "_")
+	    word = word.replace("+", "<PLUS>")
+            word = unicode(word)
             fd.inc(word)
+    if limit > -1:  
+        if fd.N() > limit:
+            break
+        
 
-
-for wordtuple in sorted(fd.items(), key=itemgetter(0)):
-    print "%s %s" % wordtuple
+outfile = codecs.open(filename_out, 'w', encoding_out)
+for (word, freq) in sorted(fd.items(), key=itemgetter(0)):
+    outfile.write( "%s %s\n" % (freq, word) )
