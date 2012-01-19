62419bd3483c2c2abc8e1eb2c2ecf7d7ee4454da
lupo01 <lupo01>
2012-01-19 22:04:58 +0000

    Changed comments, added launching commands.

--- src/featuregenerator/parser/berkeley/berkeleyparsersocket.py
+++ src/featuregenerator/parser/berkeley/berkeleyparsersocket.py
@@ -13,16 +13,16 @@ class BerkeleyParserSocket():
     """
     This class enables running java methods for BParser.java from Python.
     """
-    def __init__(self, berkeley_parser_loc, py4j_loc, java_server_loc):
+    def __init__(self, berkeley_parser_jar, py4j_jar, java_server_loc):
         """
-        @param berkeley_parser_loc: Location of BerkeleyParser.jar
-        @type berkeley_parser_loc: string
-        @param py4j_loc: Location of py4j.jar
-        @type py4j_loc: string
+        @param berkeley_parser_jar: Location of BerkeleyParser.jar
+        @type berkeley_parser_jar: string
+        @param py4j_jar: Location of py4j.jar
+        @type py4j_jar: string
         @param java_server_loc: Location of JavaServer.class
         @type java_server_loc: string
         """
-        cmd = "java -cp %s:%s:%s JavaServer" % (berkeley_parser_loc, py4j_loc, java_server_loc)
+        cmd = "java -cp %s:%s:%s JavaServer" % (berkeley_parser_jar, py4j_jar, java_server_loc)
         
         # run Java server
         Popen(cmd, shell=True, close_fds=True)
@@ -54,3 +54,14 @@ class BerkeleyParserSocket():
         Java server is terminated from here.
         """
         self.gateway.shutdown()
+'''
+lib_1 = '/media/DATA/Arbeit/DFKI/BerkeleyServer/lib/BerkeleyParser.jar' # berkeley_parser_loc
+lib_2 = '/media/DATA/Arbeit/DFKI/TaraXUscripts/support/berkeley-server/lib/py4j0.7.jar' # py4j_loc
+lib_3 = '/media/DATA/Arbeit/DFKI/BerkeleyServer/bin/' # java_server_loc
+
+bps = BerkeleyParserSocket(lib_1, lib_2, lib_3)
+bps.parse()
+# if the server wasn't closed, it has to be done manually in cmdline ('kill <PID>')
+# with local address 25333 specified in JavaServer.java 
+bps.shutdown_server()
+'''
