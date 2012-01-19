0ea50b6a7445da6f85a61207568c48ca51fb6250
elav01 <elav01>
2012-01-19 16:50:01 +0000

    More steps for the annotation pipeline. Still not tested thoroughly

--- src/experiment/autoranking/annotate.py
+++ src/experiment/autoranking/annotate.py
@@ -27,6 +27,7 @@ from sentence.dataset import DataSet
 from sentence.rankhandler import RankHandler
 from featuregenerator.diff_generator import DiffGenerator
 from sentence.scoring import Scoring
+
 from io import saxjcml
 
 
@@ -38,6 +39,14 @@ from xml import sax
 import re
 
 from featuregenerator.parser.berkeley.berkeleyclient import BerkeleyFeatureGenerator 
+from featuregenerator.lm.srilm.srilm_ngram import SRILMngramGenerator
+from featuregenerator.parser.berkeley.parsermatches import ParserMatches
+from featuregenerator.lengthfeaturegenerator import LengthFeatureGenerator
+from featuregenerator.ratio_generator import RatioGenerator
+from featuregenerator.ibm1featuregenerator import Ibm1FeatureGenerator
+from featuregenerator.levenshtein.levenshtein_generator import LevenshteinGenerator
+from featuregenerator.bleu.bleugenerator import BleuGenerator
+from featuregenerator.attribute_rank import AttributeRankGenerator
 
 
 path = cfg.get('general','path')
@@ -77,13 +86,13 @@ for parser_name in [section for section in cfg.sections() if section.startswith(
 #TODO: handle the case where parser is absent for one language
 
             
-#@posttask(touch_file("features_berkeley_source.done"))
+@posttask(touch_file("features_berkeley_source.done"))
 @active_if(parser_source)
 @transform(data_fetch, suffix(".orig.jcml"), ".parsed.%s.f.jcml" % source_language, source_language, cfg.get(parser_source, "url"), cfg.getboolean(parser_source, "tokenize"))
 def features_berkeley_source(input_file, output_file, source_language, parser_url, parser_tokenize):
     features_berkeley(input_file, output_file, source_language, parser_url, parser_tokenize)
 
-#@posttask(touch_file("features_berkeley_target.done"))
+@posttask(touch_file("features_berkeley_target.done"))
 @active_if(parser_target)
 @transform(data_fetch, suffix(".orig.jcml"), ".parsed.%s.f.jcml" % target_language, target_language, cfg.get(parser_target, "url"), cfg.getboolean(parser_target, "tokenize"))
 def features_berkeley_target(input_file, output_file, target_language, parser_url, parser_tokenize):
@@ -93,14 +102,46 @@ def features_berkeley(input_file, output_file, language, parser_url, parser_toke
     parser = BerkeleyFeatureGenerator(parser_url, language, parser_tokenize)
     saxjcml.run_features_generator(input_file, output_file, [parser])
 
-
-#def features_lm():
-#    pass
-
-def features_lm_source():
+#unimplemented
+def features_berkeley_socket(input_file, output_file, language, parser_url, parser_tokenize):
     pass
 
-def features_lm_target():
+
+source_language = cfg.get("general", "source_language")
+target_language = cfg.get("general", "target_language")
+lm_source = False
+lm_target = False
+for lm_name in [section for section in cfg.sections() if section.startswith("lmserver:")]:
+    if cfg.get(lm_name, "language") == source_language:
+        lm_source = lm_name
+    elif cfg.get(lm_name, "language") == target_language:
+        lm_target = lm_name
+
+
+@transform(data_fetch, suffix(".orig.jcml"), ".lm.%s.f.jcmk" % source_language, source_language, cfg.get(lm_source, "url"), cfg.getboolean(lm_source, "tokenize"), cfg.getboolean(lm_source, "tokenize"), cfg.getboolean(lm_source, "lowercase")) 
+def features_lm_source(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase):
+    features_lm(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase)
+    #saxjcml.run_features_generator(input_file, output_file, [srilm_ngram])
+
+@transform(data_fetch, suffix(".orig.jcml"), ".lm.%s.f.jcmk" % target_language, target_language, cfg.get(lm_target, "url"), cfg.getboolean(lm_target, "tokenize"), cfg.getboolean(lm_target, "tokenize"), cfg.getboolean(lm_target, "lowercase")) 
+def features_lm_target(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase):
+    features_lm(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase)
+    
+def features_lm(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase):
+    features_lm_batch(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase)
+
+def features_lm_batch(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase):
+    srilm_ngram = SRILMngramGenerator(lm_url, language, lm_lowercase, lm_tokenize) 
+    processed_parallelsentences = srilm_ngram.add_features_batch(JcmlReader(input_file).get_parallelsentences())
+    Parallelsentence2Jcml(processed_parallelsentences).write_to_file(output_file)
+
+
+def features_ibm(input_file, output_file, ibm1lexicon):
+    ibmfeaturegenerator = Ibm1FeatureGenerator(ibm1lexicon)
+    saxjcml.run_features_generator(input_file, output_file, [ibmfeaturegenerator])
+    
+#unimplemented
+def features_lm_single(input_file, output_file, language, lm_url, lm_tokenize, lm_lowercase):
     pass
 
 @collate([features_berkeley_source, features_berkeley_target], regex(r"([^.]+)\.(.+)\.f.jcml"),  r"\1.all.f.jcml")
@@ -112,10 +153,31 @@ def features_gather(singledataset_annotations, gathered_singledataset_annotation
         appended_dataset = JcmlReader(appended_file).get_dataset()
         original_dataset.merge_dataset_symmetrical(appended_dataset)
     Parallelsentence2Jcml(original_dataset.get_parallelsentences()).write_to_file(gathered_singledataset_annotations)
+
+    
+@transform(features_gather, suffix(".all.f.jcml"), ".all.analyzed.f.jcml", cfg.get("general", "source_language"), cfg.get("general", "target_language"))    
+def analyze_external_features(input_file, output_file, source_language, target_language):
+    langpair = (source_language, target_language)
+    analyzers = [LengthFeatureGenerator(),
+                 ParserMatches(langpair),
+                 RatioGenerator()]
+    saxjcml.run_features_generator(input_file, output_file, analyzers)
     
 
-def analyze_external_features():
-    pass
+@active_if(cfg.getboolean("annotation", "reference_features"))
+@transform(data_fetch, suffix(".orig.jcml"), ".ref.f.jcml", cfg.get("annotation", "moreisbetter").split(","), cfg.get("annotation", "lessisbetter").split(",")) 
+def reference_features(input_file, output_file, moreisbetter_atts, lessisbetter_atts):
+    analyzers = [LevenshteinGenerator(),
+                 BleuGenerator(),
+                 RatioGenerator()
+                 ]
+    
+    for attribute in moreisbetter_atts:
+        analyzers.append(AttributeRankGenerator(attribute), None, True)
+    for attribute in lessisbetter_atts:
+        analyzers.append(AttributeRankGenerator(attribute))
+        
+    saxjcml.run_features_generator(input_file, output_file, analyzers)
 
 def create_ranks():
     pass
