28bb684cfe68a0cb11089a176b3d810029c40f38
lupo01 <lupo01>
2012-01-27 14:44:39 +0000

    Parameter sysid moved from set tag to seg tag

--- src/support/preprocessing/TaraXUformats/txt2xml.py
+++ src/support/preprocessing/TaraXUformats/txt2xml.py
@@ -7,9 +7,10 @@ Created on Jan 24, 2012
 The script converts line-by-line sentences from txt file to xml format.
 
 Example for command line:
---lang english
---srcFilename /media/DATA/Arbeit/DFKI/120123_txt2xml/nc11/source.txt
---tgtFilename /media/DATA/Arbeit/DFKI/120123_txt2xml/nc11/source2.xml
+--srcLang english
+--tgtLang german
+--srcFilename /TaraXUscripts/support/preprocessing/TaraXUformats/source.txt
+--tgtFilename /TaraXUscripts/support/preprocessing/TaraXUformats/source2.xml
 --setid iwslt11_tst2011
 --setType source
 --sysid system3
@@ -26,26 +27,35 @@ parser.add_option("-x", '--tgtFilename', dest='tgtFilename', \
                                                     help="target xml filename")
 parser.add_option("-i", '--setid', dest='setid', help="id of the sentence set")
 parser.add_option("-t", '--setType', dest='setType', \
-                              help="type of sentence set (source/translation)")
-parser.add_option("-l", '--lang', dest='lang', \
-                                      help="language of source file sentences")
-parser.add_option("-l", '--sysid', dest='sysid', \
+                                                   help="type of sentence set")
+parser.add_option("-l", '--srcLang', dest='srcLang', \
+               help="source language of translation set (Option not required)")
+parser.add_option("-g", '--tgtLang', dest='tgtLang', \
+               help="target language of translation set (Option not required)")
+parser.add_option("-d", '--sysid', dest='sysid', \
                                                 help="translation system name")
 
 options, args  = parser.parse_args()
-if not options.srcFilename: sys.exit('Option --srcFilename is missing!')
-if not options.tgtFilename: sys.exit('Option --tgtFilename is missing!')
-if not options.setid: sys.exit('Option --setid is missing!')
-if not options.setType: sys.exit('Option --setType is missing!')
-if not options.lang: sys.exit('Option --lang is missing.')
-if not options.sysid: sys.exit('Option --sysid is missing.')
+if not options.srcFilename: sys.exit('ERROR: Option --srcFilename is missing!')
+if not options.tgtFilename: sys.exit('ERROR: Option --tgtFilename is missing!')
+if not options.setid: sys.exit('ERROR: Option --setid is missing!')
+if not options.setType: sys.exit('ERROR: Option --setType is missing!')
+if not options.sysid: sys.exit('ERROR: Option --sysid is missing!')
 
 srcFilename = options.srcFilename
 tgtFilename = options.tgtFilename
 setid = options.setid
 setType = options.setType
-lang = options.lang
 sysid = options.sysid
+
+srcLang = ''
+if not options.srcLang: print 'WARNING: Option --srcLang is missing.'
+else: srcLang = options.srcLang
+
+tgtLang = ''
+if not options.tgtLang: print 'WARNING: Option --tgtLang is missing.'
+else: tgtLang = options.tgtLang
+
 
 f = open(srcFilename)
 content = f.read().strip()
@@ -54,10 +64,10 @@ f.close()
 sentences = content.split('\n')
 
 output = '<?xml version="1.0" encoding="UTF-8"?>\n'
-output += '<set setid="%s" type="%s" lang="%s" sysid="%s">\n' \
-                                                % (setid, setType, lang, sysid)
+output += '<set setid="%s" type="%s" srcLang="%s" tgtLang="%s">\n' \
+                                    % (setid, setType, srcLang, tgtLang)
 for i in range(len(sentences)):
-    output += '\t<seg docid="%s" id="%s">\n' % (setid, i+1)
+    output += '\t<seg docid="1" sysid="%s" id="%s">\n' % (sysid, i+1)
     output += '\t\t%s\n' % sentences[i]
     output += '\t</seg>\n'
 output += '</set>\n'
