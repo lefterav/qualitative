546546783b038c689e313f7770cf8cb60388a4bb
lupo01 <lupo01>
2012-01-31 12:22:54 +0000

    Major update: xml file needs now following parameters: linksFile, srcFile, refFile, srcLang, tgtLang, setid, combxml and optional sys1File, sys1Name, ..., sys5File, sys5Name.

--- src/support/preprocessing/TaraXUformats/txt2xml.py
+++ src/support/preprocessing/TaraXUformats/txt2xml.py
@@ -1,79 +1,178 @@
 '''
-Created on Jan 24, 2012
+Created on Jan 30, 2012
 
 @author: jogin
 
 
-The script converts line-by-line sentences from txt file to xml format.
+The script converts sgm xml format to another xml format.
 
 Example for command line:
---srcLang english
---tgtLang german
---srcFilename /TaraXUscripts/support/preprocessing/TaraXUformats/source.txt
---tgtFilename /TaraXUscripts/support/preprocessing/TaraXUformats/source2.xml
---setid iwslt11_tst2011
---setType source
---sysid system3
+--linksFile r2-testset.en-de.links.noEmpty
+--srcFile r2-testset.en-de.en.noEmpty
+--refFile r2-testset.en-de.de.noEmpty
+--srcLang en
+--tgtLang de
+--setid my-set-id
+--combxml combined.en-de.xml
+--sys1File moses.en-de.txt
+--sys1Name moses
+
+There can be up to 5 sys parameters (maximum is sys5File, sys5Name).
 '''
 
 import sys
 from optparse import OptionParser
 
+# command line arguments definition
 parser = OptionParser()
+parser.add_option("-l", '--linksFile', dest='linksFile', \
+                help="links txt file with line-by-line sentence id and doc id")
+parser.add_option("-s", '--srcFile', dest='srcFile', \
+                            help="source txt file with line-by-line sentences")
+parser.add_option("-r", '--refFile', dest='refFile', \
+                         help="reference txt file with line-by-line sentences")
+parser.add_option("-g", '--srcLang', dest='srcLang', help="source language")
+parser.add_option("-t", '--tgtLang', dest='tgtLang', help="target language")
+parser.add_option("-i", '--setid', dest='setid', help="id of the whole set")
+parser.add_option("-x", '--combxml', dest='combxml', \
+                                       help="name of output combined xml file")
 
-parser.add_option("-s", '--srcFilename', dest='srcFilename', \
-                        help="source txt filename with line-by-line sentences")
-parser.add_option("-x", '--tgtFilename', dest='tgtFilename', \
-                                                    help="target xml filename")
-parser.add_option("-i", '--setid', dest='setid', help="id of the sentence set")
-parser.add_option("-t", '--setType', dest='setType', \
-                                                   help="type of sentence set")
-parser.add_option("-l", '--srcLang', dest='srcLang', \
-               help="source language of translation set (Option not required)")
-parser.add_option("-g", '--tgtLang', dest='tgtLang', \
-               help="target language of translation set (Option not required)")
-parser.add_option("-d", '--sysid', dest='sysid', \
-                                                help="translation system name")
+# optional command line arguments
+parser.add_option("-1", '--sys1File', dest='sys1File', \
+                   help="1.system output txt file with line-by-line sentences")
+parser.add_option("-a", '--sys1Name', dest='sys1Name', help="1.system name")
+parser.add_option("-2", '--sys2File', dest='sys2File', \
+                   help="2.system output txt file with line-by-line sentences")
+parser.add_option("-b", '--sys2Name', dest='sys2Name', help="2.system name")
+parser.add_option("-3", '--sys3File', dest='sys3File', \
+                   help="3.system output txt file with line-by-line sentences")
+parser.add_option("-c", '--sys3Name', dest='sys3Name', help="3.system name")
+parser.add_option("-4", '--sys4File', dest='sys4File', \
+                   help="4.system output txt file with line-by-line sentences")
+parser.add_option("-d", '--sys4Name', dest='sys4Name', help="4.system name")
+parser.add_option("-5", '--sys5File', dest='sys5File', \
+                   help="5.system output txt file with line-by-line sentences")
+parser.add_option("-e", '--sys5Name', dest='sys5Name', help="5.system name")
 
+# check of command line arguments
 options, args  = parser.parse_args()
-if not options.srcFilename: sys.exit('ERROR: Option --srcFilename is missing!')
-if not options.tgtFilename: sys.exit('ERROR: Option --tgtFilename is missing!')
+if not options.linksFile: sys.exit('ERROR: Option --linksFile is missing!')
+if not options.srcFile: sys.exit('ERROR: Option --srcFile is missing!')
+if not options.refFile: sys.exit('ERROR: Option --refFile is missing!')
+if not options.srcLang: sys.exit('ERROR: Option --srcLang is missing!')
+if not options.tgtLang: sys.exit('ERROR: Option --tgtLang is missing!')
 if not options.setid: sys.exit('ERROR: Option --setid is missing!')
-if not options.setType: sys.exit('ERROR: Option --setType is missing!')
-if not options.sysid: sys.exit('ERROR: Option --sysid is missing!')
+if not options.combxml: sys.exit('ERROR: Option --combxml is missing!')
 
-srcFilename = options.srcFilename
-tgtFilename = options.tgtFilename
+# variables with command line arguments
+linksFile = options.linksFile
+srcFile = options.srcFile
+refFile = options.refFile
+srcLang = options.srcLang
+tgtLang = options.tgtLang
 setid = options.setid
-setType = options.setType
-sysid = options.sysid
+combxml = options.combxml
 
-srcLang = ''
-if not options.srcLang: print 'WARNING: Option --srcLang is missing.'
-else: srcLang = options.srcLang
-
-tgtLang = ''
-if not options.tgtLang: print 'WARNING: Option --tgtLang is missing.'
-else: tgtLang = options.tgtLang
-
-
-f = open(srcFilename)
-content = f.read().strip()
+# read input files
+f = open(linksFile)
+contentLinks = f.read().strip().split('\n')
 f.close()
 
-sentences = content.split('\n')
+f = open(srcFile)
+contentSrc = f.read().strip().split('\n')
+f.close()
 
+f = open(refFile)
+contentRef = f.read().strip().split('\n')
+f.close()
+
+# read input system files if exist
+contentSys1 = []
+sys1Name = ''
+contentSys2 = []
+sys2Name = ''
+contentSys3 = []
+sys3Name = ''
+contentSys4 = []
+sys4Name = ''
+contentSys5 = []
+sys5Name = ''
+
+if options.sys1File and options.sys1Name:
+    f = open(options.sys1File)
+    contentSys1 = f.read().strip().split('\n')
+    f.close()
+    sys1Name = options.sys1Name
+    
+if options.sys2File and options.sys2Name:
+    f = open(options.sys2File)
+    contentSys2 = f.read().strip().split('\n')
+    f.close()
+    sys2Name = options.sys2Name
+
+if options.sys3File and options.sys3Name:
+    f = open(options.sys3File)
+    contentSys3 = f.read().strip().split('\n')
+    f.close()
+    sys3Name = options.sys3Name
+
+if options.sys4File and options.sys4Name:
+    f = open(options.sys4File)
+    contentSys4 = f.read().strip().split('\n')
+    f.close()
+    sys4Name = options.sys4Name
+
+if options.sys5File and options.sys5Name:
+    f = open(options.sys5File)
+    contentSys5 = f.read().strip().split('\n')
+    f.close()
+    sys5Name = options.sys5Name
+
+# check if number of lines in input files is equal
+if len(contentLinks) != len(contentSrc):
+    sys.exit('ERROR: Number of lines in linksFile and srcFile not equal!')
+if len(contentLinks) != len(contentRef):
+    sys.exit('ERROR: Number of lines in linksFile and refFile not equal!')
+if contentSys1 and len(contentLinks) != len(contentSys1):
+    sys.exit('ERROR: Number of lines in linksFile and sys1File not equal!')
+if contentSys2 and len(contentLinks) != len(contentSys2):
+    sys.exit('ERROR: Number of lines in linksFile and sys2File not equal!')
+if contentSys3 and len(contentLinks) != len(contentSys3):
+    sys.exit('ERROR: Number of lines in linksFile and sys3File not equal!')
+if contentSys4 and len(contentLinks) != len(contentSys4):
+    sys.exit('ERROR: Number of lines in linksFile and sys4File not equal!')
+if contentSys5 and len(contentLinks) != len(contentSys5):
+    sys.exit('ERROR: Number of lines in linksFile and sys5File not equal!')
+
+# create combined xml file
 output = '<?xml version="1.0" encoding="UTF-8"?>\n'
-output += '<set setid="%s" type="%s" srcLang="%s" tgtLang="%s">\n' \
-                                    % (setid, setType, srcLang, tgtLang)
-for i in range(len(sentences)):
-    output += '\t<seg docid="1" sysid="%s" id="%s">\n' % (sysid, i+1)
-    output += '\t\t%s\n' % sentences[i]
+output += '<set setid="%s" source-language="%s" target-language="%s">\n' \
+                                                    % (setid, srcLang, tgtLang)
+
+for i in range(len(contentLinks)):
+    segid = contentLinks[i].split('\t')[1]
+    docid = contentLinks[i].split('\t')[2]
+    source = contentSrc[i]
+    reference = contentRef[i]
+    
+    output += '\t<seg id="%s" docid="%s">\n' % (segid, docid)
+    output += '\t\t<source>%s</source>\n' % (source)
+    output += '\t\t<reference>%s</reference>\n' % (reference)
+    if contentSys1: output +='\t\t<translation system="%s">%s</translation>\n'\
+                                                   % (sys1Name, contentSys1[i])
+    if contentSys2: output +='\t\t<translation system="%s">%s</translation>\n'\
+                                                   % (sys2Name, contentSys2[i])
+    if contentSys3: output +='\t\t<translation system="%s">%s</translation>\n'\
+                                                   % (sys3Name, contentSys3[i])
+    if contentSys4: output +='\t\t<translation system="%s">%s</translation>\n'\
+                                                   % (sys4Name, contentSys4[i])
+    if contentSys5: output +='\t\t<translation system="%s">%s</translation>\n'\
+                                                   % (sys5Name, contentSys5[i])
     output += '\t</seg>\n'
 output += '</set>\n'
 
-f = open(tgtFilename, 'w')
+f = open(combxml, 'w')
 f.write(output)
 f.close()
 
-print '%s was generated' % tgtFilename
+print '%s was generated' % combxml
