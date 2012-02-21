a688965a8404967a37a94664b48cb531f3904ad0
elav01 <elav01>
2012-02-21 16:18:21 +0000

    Minor step for extracting "clean" test set

--- src/experiment/autoranking/extract_testset.py
+++ src/experiment/autoranking/extract_testset.py
@@ -5,12 +5,13 @@ Created on 7 Feb 2012
 '''
 
 from io.input.jcmlreader import JcmlReader
-from sentence.pairwisedataset import FilteredPairwiseDataset
+from sentence.pairwisedataset import FilteredPairwiseDataset , AnalyticPairwiseDataset
 from io.sax.saxps2jcml import Parallelsentence2Jcml
 
 def get_clean_testset(input_file, output_file):
     plain_dataset = JcmlReader(input_file)
-    filtered_dataset = FilteredPairwiseDataset(plain_dataset)
+    analytic_dataset = AnalyticPairwiseDataset(plain_dataset) 
+    filtered_dataset = FilteredPairwiseDataset(analytic_dataset)
     
 
 if __name__ == '__main__':
