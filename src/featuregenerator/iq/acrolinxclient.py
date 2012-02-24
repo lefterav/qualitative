e8844c3db6e170ffb7753c04d8c49a02f1dacbbe
elav01 <elav01>
2012-02-24 22:49:46 +0000

    SoapClient for IQ: Function that searches soapProperties in responses by key Possibility to update license on a local file Retrieval of the XML report from the remote server XML reading still missing Minor restructuring

--- src/featuregenerator/iq/acrolinxclient.py
+++ src/featuregenerator/iq/acrolinxclient.py
@@ -1,9 +1,4 @@
 '''
-Created on Feb 24, 2012
-
-@author: jogin
-'''
-'''
 Created on Feb 13, 2012
 
 @author: jogin
@@ -19,12 +14,16 @@ Created on Feb 13, 2012
 
 
 from suds.client import Client
+import base64
+import re
+import urllib
 
+host = "msv-3231.sb.dfki.de:8031"
 # create suds client
-url = "http://msv-3231.sb.dfki.de:8031/acrolinx/services/core-no-mtom?wsdl"
+url = "http://{0}/acrolinx/services/core-no-mtom?wsdl".format(host)
 soap_client = Client(url)
 print soap_client
-import base64
+
 
 #print soap_client
 #ping = soap_client.service.ping()
@@ -34,20 +33,65 @@ import base64
 
 
 license_data_filename = "license.dat"
-USER_ID = '1359'   #if license doesn't work, delete license.dat and change user id
+USER_ID = '1359'   #if license doesn't work, delete license.dat and change user id OR remove access id
+
+def _get_property(response, key):
+    """
+    Performs a search into the response of the server, 
+    and returns the value of a SOAPproperty given its key
+    @param response: 
+    """
+    for soap_property in response:
+        if soap_property['key'] == key:
+            return soap_property['value']
+            break
+    return None
 
 def _update_license(response):
     """
-    Function to call every time we get a response that may change the license. It extracts the license string from the 
-    response and updates the text file. Then it returns the license string.
+    Function to call every time we get a response that may change the license. 
+    It extracts the license string from the response and updates the text file. 
+    Then it returns the license string. 
+    This should be called after registerUser or checkDocument
     @return: the license string
     @rtype: str
     """
-    licenseDataStr = response[0]['value']
+    licenseDataStr = _get_property(response, "license.data")
     license_data_file = open(license_data_filename, 'w')
     license_data_file.write(licenseDataStr)
     license_data_file.close()
     return licenseDataStr
+
+
+def _read_report_xml(report_xml):
+    """
+    Processes the xml report by Acrolinx IQ and returns a dict of the corrections suggested
+    @param report_xml: the content of the report
+    @type report_xml: str
+    @return: a dict containing the correction suggested
+    @rtype: {str: str}
+    @todo: write the function that reads the XML
+    """
+    pass
+
+
+def _attributes2soapproperties(attributes = {}):
+    """
+    Converts a normal python dict to a list of SoapProperty instances
+    @param attributes: a dict containing soap properties
+    @type attributes: {str, str}
+    @return: a list of SoapProperty instances that can be sent to Soap
+    @rtype: [SoapProperty, ...]
+    @todo: replace the handwritten soapProperties with 
+    """
+    soap_properties = []
+    for key, value in attributes.iteritems():
+        soap_property = soap_client.factory.create('soapProperty')
+        soap_property['key'] = key
+        soap_property['value'] = value
+        soap_properties.append(soap_property)
+    return soap_properties   
+    
 
 
 #register only once
@@ -100,19 +144,20 @@ try:
     
     ### TEXT SETTINGS
     
-    text = 'Das ist meine Erste versuch.'
+    text = 'Dear clients, we would like to informm you that during the latest commerccial update we recieved marvelous products, which wwe can offers in really good prices. Please keeps in touch for further notice. This break every possibility'
     # encode text to base64
     text64 = base64.standard_b64encode(text)
     
+    #@todo: replace the following declarations by a dict to be fed to function _attributes2soapproperties
     # create soapProperty object with text_lang
     textLang = soap_client.factory.create('soapProperty')
     textLang['key'] = 'text_lang'
-    textLang['value'] = 'de'
+    textLang['value'] = 'en'
     
     # create soapProperty object with textType
     textType = soap_client.factory.create('soapProperty')
     textType['key'] = 'text_type'
-    textType['value'] = 'MT-preediting-DE-EN'
+    textType['value'] = 'MT-preediting-DE-EN-T1'
         
     # create soapProperty object with check_spelling
     checkSpelling = soap_client.factory.create('soapProperty')
@@ -132,17 +177,30 @@ try:
     # create soapProperty object with check_terms
     checkTerms = soap_client.factory.create('soapProperty')
     checkTerms['key'] = 'check_terms'
-    checkTerms['value'] = 'MT-postediting-DE-EN.modules.terms'
+    checkTerms['value'] = 'MT-preediting-DE-EN-T1.modules.terms'
     ###check_terms: comma-separated list of term sets
     
-    print soap_client.service.getLanguageOptions('de')
+#    print soap_client.service.getLanguageOptions('en')
     #aaa
-    SOAPProperty = [licenseData, userId, sessionId, \
-                    textLang, textType, checkSpelling, checkGrammar, checkStyle]
-    print SOAPProperty
+    soap_properties = [licenseData, userId, sessionId, \
+                    textLang, textType, checkSpelling, checkGrammar, checkStyle, checkTerms]
+    print soap_properties
     
-    resp = soap_client.service.checkDocumentMtom(SOAPProperty, text64, sessionIdStr, checkId)
-    print resp
+    resp = soap_client.service.checkDocumentMtom(soap_properties, text64, "utf-8", checkId)
+    licenseDataStr = _update_license(resp)
+    
+    #extract document score from the response
+    document_score = _get_property(resp, "document_score")
+    #get url of the report xml
+    report_url = _get_property(resp, "report_url")
+    #fix the host part of the url
+    report_url = re.sub("http://[^/]*/", "http://{0}/".format(host), report_url)
+    print "retrieving report from ", report_url
+    report_xml = urllib.urlopen(report_url).read()
+    
+    #@todo: write the function that reads that
+    attributes = _read_report_xml(report_xml)
+    
     
 finally:
     soap_client.service.releaseClientSession(sessionIdStr)
