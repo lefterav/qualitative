aec9ed728c020ecdec7c93018e730346ff01784f
elav01 <elav01>
2012-03-07 14:22:39 +0000

    Script to check experiment results
    Former-commit-id: f0b082baaa38018b9082694f617bc7b0a1f03cb6

--- src/experiment/qualityestimation/experiment_check.py
+++ src/experiment/qualityestimation/experiment_check.py
@@ -11,5 +11,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Display progress of variable')
     
     mysuite = QualityEstimationSuite()
-    mysuite.get_values_fix_params("quality_estimation_development", 0, "RootMeanSqrErr")
-    mysuite.get_values_fix_params("quality_estimation_development", 0, "MeanAvgErr")
+    a = mysuite.get_values_fix_params("./19/quality_estimation_development", 0, "RootMeanSqrErr")
+    b = mysuite.get_values_fix_params("./19/quality_estimation_development", 0, "MeanAvgErr")
+    for entry in zip(a[0], b[0] , ["%s-%s" % (i['classifier'], i['att']) for i in a[1]]):
+        print entry
