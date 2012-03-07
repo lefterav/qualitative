7bb2b3fd48b2868cd83a567b00cf260fc89e885a
elav01 <elav01>
2012-03-07 17:16:16 +0000

    bug with a comma
    Former-commit-id: de56e3ce5c4821e0c1ecf1e079999a0d00cfa659

--- src/experiment/qualityestimation/experiment_check.py
+++ src/experiment/qualityestimation/experiment_check.py
@@ -6,12 +6,15 @@ Created on 07 Mar 2012
 
 from suite import QualityEstimationSuite
 import argparse 
+import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Display progress of variable')
+
+    directory = sys.argv[1]
     
     mysuite = QualityEstimationSuite()
-    a = mysuite.get_values_fix_params("./19/quality_estimation_development", 0, "RootMeanSqrErr")
-    b = mysuite.get_values_fix_params("./19/quality_estimation_development", 0, "MeanAvgErr")
+    a = mysuite.get_values_fix_params("./%s" % directory , 0, "RootMeanSqrErr")
+    b = mysuite.get_values_fix_params("./%s" % directory, 0, "MeanAvgErr")
     for entry in zip(a[0], b[0] , ["%s-%s" % (i['classifier'], i['att']) for i in a[1]]):
         print entry
