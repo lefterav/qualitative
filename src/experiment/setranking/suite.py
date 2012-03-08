c2ac8b2bba8024daee0f1ad53df48087617bd4ed
elav01 <elav01>
2012-03-08 03:36:46 +0000

    support for soft set ranking

--- src/experiment/setranking/suite.py
+++ src/experiment/setranking/suite.py
@@ -52,6 +52,9 @@ class QualityEstimationSuite(PyExperimentSuite):
         self.discretization = False
         if params.has_key("discretization"):
             self.discretization = params["discretization"]
+        self.testset_ratio = 0.7
+        if params.has_key("testset_ratio"):
+            self.testset_ratio = params["testset_ratio"]
         self.hidden_attributes = params["hidden_attributes"].split(",")
         self.discrete_attributes = params["discrete_attributes"].split(",")
         self.filter_score_diff = params["filter_score_diff"]
@@ -68,7 +71,7 @@ class QualityEstimationSuite(PyExperimentSuite):
             print "loading big set"
             shutil.copy(params["training_set"], "trainset.jcml")
         if n == 1:
-            self._get_testset(params["test_set"], params["mode"])
+            self._get_testset(params["test_set"], params["mode"], self.testset_ratio)
         
         if n == 2:
             print "coupling training set"
@@ -117,6 +120,8 @@ class QualityEstimationSuite(PyExperimentSuite):
             orangeData = Table("testset.tab")
             classified_set_vector = self.myclassifier.classify_orange_table(orangeData)
             self.classified_values_vector = [str(v[0]) for v in classified_set_vector]
+            self.classified_probs_vector = [(v[1]["-1"], v[1]["1"]) for v in classified_set_vector]
+#            print [str(v[1]["-1"]) for v in classified_set_vector]
 #            print classified_set_vector
         
         if n == 8:
@@ -129,21 +134,31 @@ class QualityEstimationSuite(PyExperimentSuite):
 
             print "reconstructing test set"
             att_vector = [{"rank_predicted": v} for v in self.classified_values_vector]
+            att_prob_neg = [{"prob_-1": v[0]} for v in self.classified_probs_vector]
+            att_prob_pos = [{"prob_1": v[1]} for v in self.classified_probs_vector]
             print att_vector
             print "adding guessed rank"
             self.simple_testset.add_attribute_vector(att_vector, "ps")
-            self.reconstructed_testset = self.simple_testset.get_single_set_with_hard_ranks("rank_predicted")
+            self.simple_testset.add_attribute_vector(att_prob_neg, "ps")
+            self.simple_testset.add_attribute_vector(att_prob_pos, "ps")
+            self.reconstructed_hard_testset = self.simple_testset.get_single_set_with_hard_ranks("rank_predicted")
+            self.reconstructed_soft_testset = self.simple_testset.get_single_set_with_soft_ranks("prob_-1", "prob_1", "rank_soft_predicted")
             self.simple_testset = None
         if n == 9:
-            original_score_vector = [float(ps.get_attribute("tgt-1_score")) for ps in self.reconstructed_testset.get_parallelsentences()]
+            original_score_vector = [float(ps.get_attribute("tgt-1_score")) for ps in self.reconstructed_hard_testset.get_parallelsentences()]
             #original_score_set = set(original_score_vector)
             original_score_sorted = sorted(original_score_vector, reverse=True)
             original_rank_vector = [{"rank_original": str(original_score_sorted.index(v)+1)} for v in original_score_vector]
-            self.reconstructed_testset.add_attribute_vector(original_rank_vector, "ps")
+            self.reconstructed_hard_testset.add_attribute_vector(original_rank_vector, "ps")
+            if self.reconstructed_soft_testset:
+                self.reconstructed_soft_testset.add_attribute_vector(original_rank_vector, "ps")
             
         if n == 10:
+            ret = {}
             from support.evaluation.wmt12.wmt_scoring import WmtScoring
-            ret = WmtScoring(self.reconstructed_testset).process("", "rank_original", "", "rank_predicted")
+            ret["hard"] = WmtScoring(self.reconstructed_hard_testset).process("", "rank_original", "", "rank_predicted")
+            if self.reconstructed_soft_testset:
+                ret["soft"] = WmtScoring(self.reconstructed_soft_testset).process("", "rank_original", "", "rank_soft_predicted")
             print ret
             print "finished"
         return ret
@@ -154,34 +169,44 @@ class QualityEstimationSuite(PyExperimentSuite):
             pickle.dump(self.myclassifier.classifier, objectfile)
             objectfile.close()
         if n == 7:
-            classified_vector_file = open("classified.txt", 'w')
+            classified_vector_file = open("classified.hard.txt", 'w')
             for value in self.classified_values_vector:
                 classified_vector_file.write("{0}\n".format(value))
             classified_vector_file.close()
+            classified_prob_file = open("classified.soft.txt", 'w')
+            for value1, value2 in self.classified_probs_vector:
+                classified_prob_file.write("{0}\t{1}\n".format(value1, value2))
+            classified_prob_file.close()
         if n == 8:
 #            Parallelsentence2Jcml(self.simple_testset).write_to_file("testset.classified.jcml")
-            Parallelsentence2Jcml(self.reconstructed_testset).write_to_file("testset.reconstructed.jcml")
+            Parallelsentence2Jcml(self.reconstructed_hard_testset).write_to_file("testset.reconstructed.hard.jcml")
+            Parallelsentence2Jcml(self.reconstructed_soft_testset).write_to_file("testset.reconstructed.soft.jcml")
         if n == 9:
-            Parallelsentence2Jcml(self.reconstructed_testset).write_to_file("testset.reconstructed_w_orig.jcml")
-
+            Parallelsentence2Jcml(self.reconstructed_hard_testset).write_to_file("testset.reconstructed.org.hard.jcml")
+            Parallelsentence2Jcml(self.reconstructed_soft_testset).write_to_file("testset.reconstructed.org.soft.jcml")
+    
     def restore_state(self,params, rep, n):
-        if n in [5, 6]:
+        if n in [6, 7]:
             objectfile = open("classifier.pickle", 'r')
-            self.myclassifier = pickle.load(objectfile)
+            self.myclassifier = OrangeClassifier(pickle.load(objectfile))
             objectfile.close()
         if n == 8:
-            classified_vector_file = open("classified.txt", 'r') 
+            classified_vector_file = open("classified.hard.txt", 'r') 
             self.classified_values_vector = classified_vector_file.readlines()
             classified_vector_file.close()
+            classified_prob_file = open("classified.soft.txt", 'r') 
+            self.classified_probs_vector = classified_prob_file.readlines()
+            classified_prob_file.close()
         if n == 9:
 #            self.simple_testset = JcmlReader("testset.classified.jcml").get_dataset
-            self.reconstructed_testset = JcmlReader("testset.reconstructed.jcml").get_dataset()
+            self.reconstructed_hard_testset = JcmlReader("testset.reconstructed.hard.jcml").get_dataset()
+            self.reconstructed_soft_testset = JcmlReader("testset.reconstructed.soft.jcml").get_dataset()
         if n == 10:
-            self.reconstructed_testset = JcmlReader("testset.reconstructed_w_orig.jcml").get_dataset()
-        
+            self.reconstructed_hard_testset = JcmlReader("testset.reconstructed.org.hard.jcml").get_dataset()
+            self.reconstructed_soft_testset = JcmlReader("testset.reconstructed.org.soft.jcml").get_dataset()        
     ##############################
                 
-    def _get_testset(self, test_filename, mode = ""):
+    def _get_testset(self, test_filename, mode = "", ratio=0.7):
         if not test_filename == "":
             print "arbitrarily split given set to training and test sets 90% + 10%"
             simple_trainset = JcmlReader("trainset.jcml").get_dataset()
@@ -189,7 +214,7 @@ class QualityEstimationSuite(PyExperimentSuite):
             if mode == "development":
                 simple_trainset, a = simple_trainset.split(0.03)
             
-            simple_trainset, simple_testset = simple_trainset.split(0.70)
+            simple_trainset, simple_testset = simple_trainset.split(ratio)
             Parallelsentence2Jcml(simple_trainset).write_to_file("trainset.jcml")
             Parallelsentence2Jcml(simple_testset).write_to_file("testset.jcml")
         else:
