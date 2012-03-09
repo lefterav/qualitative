6ad8a90ab2f03e8b365c1e54d8cbc56cd194da5c
elav01 <elav01>
2012-03-09 18:16:54 +0000

    final version if  check script

--- src/experiment/qualityestimation/check.py
+++ src/experiment/qualityestimation/check.py
@@ -14,18 +14,27 @@ if __name__ == '__main__':
     directory = sys.argv[1]
     
     mysuite = QualityEstimationSuite()
-    ta = mysuite.get_values_fix_params("./%s" % directory , 0, "RootMeanSqrErr")
-    tb = mysuite.get_values_fix_params("./%s" % directory, 0, "MeanAvgErr")
-    tc = mysuite.get_values_fix_params("./%s" % directory, 0 , "LargeErrPerc")
-    td = mysuite.get_values_fix_params("./%s" % directory, 0 , "SmallErrPerc")
-    te = mysuite.get_values_fix_params("./%s" % directory, 0 , "Interval")
-    tf = mysuite.get_values_fix_params("./%s" % directory, 0 , "DeltaAvg")
-    tg = mysuite.get_values_fix_params("./%s" % directory, 0 , "Spearman-Corr")
-    tacc = mysuite.get_values_fix_params("./%s" % directory, 0 , "CA")
-    tauc = mysuite.get_values_fix_params("./%s" % directory, 0 , "AUC")
+    ta = mysuite.get_values_fix_params("./%s" % directory , 1, "RootMeanSqrErr")
+    tb = mysuite.get_values_fix_params("./%s" % directory, 1, "MeanAvgErr")
+    tc = mysuite.get_values_fix_params("./%s" % directory, 1 , "LargeErrPerc")
+    td = mysuite.get_values_fix_params("./%s" % directory, 1 , "SmallErrPerc")
+    te = mysuite.get_values_fix_params("./%s" % directory, 1 , "interval1")
+    te2 = mysuite.get_values_fix_params("./%s" % directory, 1 , "interval2")
+    tf = mysuite.get_values_fix_params("./%s" % directory, 1 , "DeltaAvg")
+    tg = mysuite.get_values_fix_params("./%s" % directory, 1 , "Spearman-Corr")
+    tacc = mysuite.get_values_fix_params("./%s" % directory, 1 , "CA")
+    tauc = mysuite.get_values_fix_params("./%s" % directory, 1 , "AUC")
     
-    for a, b, c, ad, e, f, g, acc, auc, d, in zip(ta[0], tb[0], tc[0], td[0], te[0], tf[0], tg[0], tacc[0], tauc[0], ta[1]):
-        output = [str(a), str(b), str(c), str(ad), str(e), str(f), str(g), str(acc), str(auc)]
+    for a, b, c, ad, e, e2, f, g, acc, auc, d, in zip(ta[0], tb[0], tc[0], td[0], te[0], te2[0], tf[0], tg[0], tacc[0], tauc[0], ta[1]):
+        try:
+            current_acc = "%.1f" % (acc[0]*100.0)
+        except:
+            current_acc = "None"
+        try:
+            current_auc =  "%.1f" % (auc[0]*100.0)
+        except:
+            current_auc = "None"
+        output = [str(a), str(b), str(c), str(ad), str(e), str(e2), str(f), str(g), current_acc, current_auc]
         output.append(d['classifier'])
         output.append(d['att'])
         if d.has_key('filter_score_diff'):
@@ -33,10 +42,6 @@ if __name__ == '__main__':
         if d.has_key('discretization'):
             output.append(str(d['discretization']))
         
-        if d.has_key('CA'):
-            output.append(str(d['CA']))
-        if d.has_key('AUC'):
-            output.append(str(d['AUC']))
         print "\t".join(output)
             
         
