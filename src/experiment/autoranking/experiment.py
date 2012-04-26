6cbd7d1e60187ab1d9cc877e0681d3d4df2edbbd
elav01 <elav01>
2012-04-26 13:51:18 +0000

    Modifications for autoranking pipeline

--- src/experiment/autoranking/experiment.py
+++ src/experiment/autoranking/experiment.py
@@ -7,14 +7,11 @@ Created on 23 Feb 2012
 import os
 import shutil
 from ruffus import pipeline_run
+import annotate
 
 class Experiment:
     pass
 
 if __name__ == '__main__':
-    
-    import annotate
-    
-#cfg.readfp(StringIO.StringIO(CONFIG_TEMPLATE)
     pipeline_run([annotate.analyze_external_features])
     
