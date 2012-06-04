70384ff1a49401b42152b6301907101977803ad2
elav01 <elav01>
2012-06-04 19:54:51 +0000

    Diplay process id

--- src/experiment/autoranking/bootstrap.py
+++ src/experiment/autoranking/bootstrap.py
@@ -267,7 +267,8 @@ class ExperimentConfigParser(ConfigParser):
         self.write(new_configfile)
         new_configfile.close()
         self.path = path
-        sys.stderr.write("working in path {}\n".format(path))
+        sys.stderr.write("Working in path {}\n".format(path))
+        sys.stderr.write("System process pid: {}\m".format(os.getpid()))
         return path
     
     def _get_new_step_id(self, existing_files):
