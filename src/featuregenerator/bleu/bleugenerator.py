136a96f466f8efe9be5736f3e3aab92b574095f9
elav01 <elav01>
2012-06-15 16:26:25 +0000

    Cross bleu Generator
    Former-commit-id: 1e0a1954b9cfe2e6b897fcca321190f0897f07c5

--- src/featuregenerator/bleu/bleugenerator.py
+++ src/featuregenerator/bleu/bleugenerator.py
@@ -13,15 +13,19 @@ import os
 import subprocess
 import sys
 import codecs
+import bleu
+
+
+  
 
 class BleuGenerator(FeatureGenerator):
     '''
-    classdocs
+    Provides BLEU score against the reference
     '''
     
     def get_features_tgt(self, target, parallelsentence):
         """
-        Calculates Levenshtein distance for the given target sentence, against the reference sentence
+        Calculates BLEU score for the given target sentence, against the reference sentence
         @param simplesentence: The target sentence to be scored
         @type simplesentence: sentence.sentence.SimpleSentence
         @rtype: dict
@@ -30,40 +34,63 @@ class BleuGenerator(FeatureGenerator):
         target_untokenized = target.get_string()
         ref_untokenized = parallelsentence.get_reference().get_string()
 
-        bleu_value = self.bleu(target_untokenized, ref_untokenized)
+        bleu_value = bleu.score_sentence(target_untokenized, ref_untokenized)
         return {'bleu': str(bleu_value)}
 
 
-    def bleu(self, translation, reference):
-        
-        translation = " ".join(PunktWordTokenizer().tokenize(translation))
-        tfilename = mktemp(dir=u'/tmp/', suffix=u'.tgt.txt')
-        tfile = codecs.open(tfilename, 'w', 'utf-8')
-        tfile.write(translation)
-        tfile.close()
-        
-        reference = " ".join(PunktWordTokenizer().tokenize(reference))
-        rfilename = mktemp(dir=u'/tmp/', suffix=u'.ref.txt')
-        rfile = codecs.open(rfilename, 'w', 'utf-8')
-        rfile.write(reference)
-        rfile.close()
-        
-        ofilename = mktemp(dir=u'/tmp/', suffix=u'.out.txt')
-        ofile = codecs.open(ofilename, 'w', 'utf-8')
-        
-        path = os.path.dirname(__file__)
-        bleupath = os.path.join(path, "bleu")
-        print bleupath
-        subprocess.call([bleupath, "-s" , "-p", "-S", "-r", rfilename, tfilename], stdout = ofile)
-        ofile.close()
-        ofile = codecs.open(ofilename, 'r', 'utf-8')
-        output = ofile.readline()
-        output = float(output)
-        return output
 
+class CrossBleuGenerator(FeatureGenerator):
+    '''
+    Provides cross-BLEU score of the current target sentence against the others
+    '''
+    
+    def get_features_tgt(self, translation, parallelsentence):
+        current_system_name = translation.get_attribute("system")
+        alltranslations = dict([(t.get_attribute("system"), t.get_string()) for t in parallelsentence.get_translations()])
+        del(alltranslations[current_system_name])
+        references = alltranslations.values()
+        bleu_value = bleu.score_sentence(translation, references)
+        return {'bleu': str(bleu_value)}
+        
+            
+            
         
         
         
+        
+    
+
+
+#    def bleu(self, translation, reference):
+#        
+#        translation = " ".join(PunktWordTokenizer().tokenize(translation))
+#        tfilename = mktemp(dir=u'/tmp/', suffix=u'.tgt.txt')
+#        tfile = codecs.open(tfilename, 'w', 'utf-8')
+#        tfile.write(translation)
+#        tfile.close()
+#        
+#        reference = " ".join(PunktWordTokenizer().tokenize(reference))
+#        rfilename = mktemp(dir=u'/tmp/', suffix=u'.ref.txt')
+#        rfile = codecs.open(rfilename, 'w', 'utf-8')
+#        rfile.write(reference)
+#        rfile.close()
+#        
+#        ofilename = mktemp(dir=u'/tmp/', suffix=u'.out.txt')
+#        ofile = codecs.open(ofilename, 'w', 'utf-8')
+#        
+#        path = os.path.dirname(__file__)
+#        bleupath = os.path.join(path, "bleu")
+#        print bleupath
+#        subprocess.call([bleupath, "-s" , "-p", "-S", "-r", rfilename, tfilename], stdout = ofile)
+#        ofile.close()
+#        ofile = codecs.open(ofilename, 'r', 'utf-8')
+#        output = ofile.readline()
+#        output = float(output)
+#        return output
+#
+#        
+        
+
 
     
         
