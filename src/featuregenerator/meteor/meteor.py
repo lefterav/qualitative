cab0bdd077be8c9ac29b579210f44c2a5636e9aa
elav01 <elav01>
2012-06-15 16:26:10 +0000

    Untested class for scoring with Meteor
    Former-commit-id: d9da9e8354c0b816eea6e7d96b967283b34952f5

--- src/featuregenerator/meteor/meteor.py
+++ src/featuregenerator/meteor/meteor.py
@@ -9,7 +9,7 @@ from py4j.java_gateway import GatewayClient
 from py4j.java_gateway import java_import
 from featuregenerator.languagefeaturegenerator import LanguageFeatureGenerator
 
-class LanguageToolSocketFeatureGenerator(LanguageFeatureGenerator):
+class MeteorGenerator(LanguageFeatureGenerator):
     '''
     classdocs
     '''
@@ -22,3 +22,41 @@ class LanguageToolSocketFeatureGenerator(LanguageFeatureGenerator):
         '''
         self.lang = lang
     
+    
+        meteor_view = gateway.new_jvm_view()
+        java_import(meteor_view, 'edu.cmu.meteor.scorer.*')
+        java_import(meteor_view, 'edu.cmu.meteor.util.*')
+        
+#        import edu.cmu.meteor.scorer.MeteorScorer; 
+#        import edu.cmu.meteor.scorer.MeteorConfiguration;
+#        import edu.cmu.meteor.scorer.MeteorStats;
+#        import edu.cmu.meteor.util.Constants;
+#        import edu.cmu.meteor.util.Normalizer;
+#        import edu.cmu.meteor.util.SGMData;
+
+        
+        config = meteor_view.MeteorConfiguration();
+        config.setLanguage(lang);
+        self.scorer = meteor_view.MeteorScorer(config)
+        
+        
+    
+    def score(self, target, references):
+        stats = self.scorer.getMeteorStats(target, references);
+        
+        return {'meteor_precision' : stats.precision, 
+                'meteor_recall' : stats.recall, 
+                'meteor_fragPenalty' : stats.fragPenalty,  
+                'meteor_score' : stats.score}
+    
+
+
+class CrossMeteorGenerator(MeteorGenerator):
+    
+    def get_features_tgt(self, translation, parallelsentence):
+        current_system_name = translation.get_attribute("system")
+        alltranslations = dict([(t.get_attribute("system"), t.get_string()) for t in parallelsentence.get_translations()])
+        del(alltranslations[current_system_name])
+        references = alltranslations.values()
+        stats = self.score(translation, references)
+        return stats
