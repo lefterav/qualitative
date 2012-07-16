373e40d0bd64875042d1cc999a6daa3750355a36
elav01 <elav01>
2012-07-16 15:39:12 +0000

    Fixed several issues with repetition, subfolders etc.. Verbose output on logging file

--- src/experiment/autorankingsearch/suite.py
+++ src/experiment/autorankingsearch/suite.py
@@ -46,7 +46,7 @@ import pickle
 import os
 
 from expsuite import PyExperimentSuite
-
+from time import gmtime, strftime
 
 
 class AutorankingSuite(PyExperimentSuite):
@@ -65,38 +65,10 @@ class AutorankingSuite(PyExperimentSuite):
         
         self.meta_attributes = params["meta_attributes"].split(",")
         
-        
-#        source_attributes = params["{}_source".format(params["att"])].split(",")
-#        target_attributes = params["{}_target".format(params["att"])].split(",")
-#        general_attributes = params["{}_general".format(params["att"])].split(",")
-#        
-#        params["source_attributes"] = source_attributes
-#        params["target_attributes"] = target_attributes
-#        params["general_attributes"] = general_attributes
-#        
-#        self.active_attributes = []
-#        if general_attributes != [""]:
-#            self.active_attributes.extend(general_attributes) #TODOL check whether ps prefix is needed
-#        if source_attributes != [""]:
-#            self.active_attributes.extend(["src_{}".format(att) for att in source_attributes])
-#        if target_attributes != [""]:
-#            self.active_attributes.extend(["tgt-1_{}".format(att) for att in target_attributes])
-#            self.active_attributes.extend(["tgt-2_{}".format(att) for att in target_attributes])
-#        
-#        if self.active_attributes == [""]:
-#            self.active_attributes = []
-#        self.discretization = False
-#        if params.has_key("discretization"):
-#            self.discretization = params["discretization"]
-#
-#        self.hidden_attributes = params["hidden_attributes"].split(",")
-#        self.discrete_attributes = params["discrete_attributes"].split(",")
  
         self.class_name = params["class_name"]
         self.class_type = params["class_type"]
         
-#        self.training_sets = params["training_sets"].format(**params).split(',')
-#        self.testset = params["test_set"].format(**params)
         self.ties = params["ties"]
         
         self.trainset_orange_filename = params["training_tab"].format(**params)
@@ -111,96 +83,80 @@ class AutorankingSuite(PyExperimentSuite):
         ret = {}
         
         if n == 80:
-#            print "train classifier"
-            print ".",
+            logging.info("===== TRAINING CLASSIFIER =====")
             input_file = self.trainset_orange_filename
             
             trainset = Table(input_file)
-            class_var = trainset.domain.class_var.name
-            self.attributes.add(class_var) 
-            newdomain = Domain(list(self.attributes), trainset.domain)
+            new_attributes = []
+            new_attributes.extend(self.attributes)
+            new_attributes.append(self.class_name)
+            newdomain = Domain(new_attributes, trainset.domain)
             newtrainset = Table(newdomain, trainset)
-
+            logging.debug("Original domain reduced from %d features to %d features including class. \nFinal features = %s ", len(trainset.domain), len(newtrainset.domain), newtrainset.domain)
+            
+            
+            cltimestart = time.time()
             mylearner = self.learner(**self.classifier_params)
             trained_classifier = mylearner(newtrainset)
             self.classifier = OrangeClassifier(trained_classifier)
             self.classifier.print_content()
-            
-        
+            cltimeend = time.time()
+            ret["training_time"] = cltimeend - cltimestart
         
         if n == 85:
-            print "evaluate classifier with cross-fold validation"
+            logging.info("evaluate classifier with cross-fold validation")
             orangeData = Table(self.trainset_orange_filename)
             learner = self.learner(**self.classifier_params)
             cv = evaluation.testing.cross_validation([learner], orangeData, 10)
             ret["CA"] = evaluation.scoring.CA(cv)[0]
             ret["AUC"] = evaluation.scoring.AUC(cv)[0]
-#            
-#        if n == 90:
-#            print "test_classifier"
-#            
-#            input_file = self.testset_orange_filename
-##            output_file = "classified.tab"
-#            
-#            print "performing classification"
-#            orangedata = Table(input_file)
-#            
-#            
-#           
-#                    
-#            classified_set_vector = self.classifier.classify_orange_table(orangedata)
-#            self.classified_values_vector = [str(v[0]) for v in classified_set_vector]
-#            self.classified_probs_vector = [(v[1]["-1"], v[1]["1"]) for v in classified_set_vector]
-#            
-#            
-##            print [str(v[1]["-1"]) for v in classified_set_vector]
-##            print classified_set_vector
-#        
-##        if n == 95:
-##            print "accuracy over test set"
-##            orangedata = Table(self.testset_orange_filename)
-##            cv = evaluation.testing.default_evaluation([self.learner(**self.classifier_params)], orangedata)
-##            ret["CA_test"] = evaluation.scoring.CA(cv)
-##            ret["AUC_test"] = evaluation.scoring.AUC(cv)
-#        
-#        
-#        if n == 100:
-#            print "EVALUATION"
-#            print "reloading coupled test set"
-#            self.simple_testset = JcmlReader(self.pairwise_test_filename).get_dataset()
-#            
-#            print "reconstructing test set"
-#            att_vector = [{"rank_predicted": v} for v in self.classified_values_vector]
-#            att_prob_neg = [{"prob_-1": v[0]} for v in self.classified_probs_vector]
-#            att_prob_pos = [{"prob_1": v[1]} for v in self.classified_probs_vector]
-#            print att_vector
-#            
-#            print "adding guessed rank"
-#            self.simple_testset.add_attribute_vector(att_vector, "ps")
-#            self.simple_testset.add_attribute_vector(att_prob_neg, "ps")
-#            self.simple_testset.add_attribute_vector(att_prob_pos, "ps")
-#            
-#            self.simple_testset = AnalyticPairwiseDataset(self.simple_testset) #this 
-#            self.simple_testset = CompactPairwiseDataset(self.simple_testset) #and this should have no effect
-#            
-#            self.reconstructed_hard_testset = self.simple_testset.get_single_set_with_hard_ranks("rank_predicted", "rank_hard")
-#            self.reconstructed_soft_testset = self.simple_testset.get_single_set_with_soft_ranks("prob_-1", "prob_1", "rank_soft_predicted", "rank_soft")
-#            self.simple_testset = None
-#            
-#        
-#        if n == 120:
-#            print "Scoring correlation"
-#            scoringset = Scoring(self.reconstructed_hard_testset)
-#            ret["kendalltau-hard"], ret["kendalltau-hard-pi"]  = scoringset.get_kendall_tau("rank_hard", self.class_name)
-#            ret["kendalltau_b-hard"], ret["kendalltau_b-hard-pi"]  = scoringset.get_kendall_tau_b("rank_hard", self.class_name)
-#            ret["b1-acc-hard-1"], ret["b1-acc-hard-any"] = scoringset.selectbest_accuracy("rank_hard", self.class_name)            
-#            
-#            scoringset = Scoring(self.reconstructed_soft_testset)
-#            ret["kendalltau-soft"], ret["kendalltau-soft-pi"] = scoringset.get_kendall_tau("rank_soft", self.class_name)
-#            ret["kendalltau_b-soft"], ret["kendalltau_b-soft-pi"] = scoringset.get_kendall_tau_b("rank_soft", self.class_name)
-#            ret["b1-acc-soft-1"], ret["b1-acc-soft-any"] = scoringset.selectbest_accuracy("rank_soft", self.class_name)        
-#            
-#            
+
+        if self.critical_score not in ["CA", "AUC"]:
+            if n == 90:
+                logging.info("==== TESTING CLASSIFIER ====")
+                
+                orangedata = Table(self.testset_orange_filename)                         
+                classified_set_vector = self.classifier.classify_orange_table(orangedata)
+                
+                self.classified_values_vector = [str(v[0]) for v in classified_set_vector]
+                self.classified_probs_vector = [(v[1]["-1"], v[1]["1"]) for v in classified_set_vector]
+                
+            if n == 100:
+                logging.info("==== EVALUATION TESTSET ====")
+                logging.info("== reloading coupled test set ==")
+                self.simple_testset = JcmlReader(self.pairwise_test_filename).get_dataset()
+                
+                logging.info("reconstructing test set")
+                att_vector = [{"rank_predicted": v} for v in self.classified_values_vector]
+                att_prob_neg = [{"prob_-1": v[0]} for v in self.classified_probs_vector]
+                att_prob_pos = [{"prob_1": v[1]} for v in self.classified_probs_vector]
+                
+                logging.info("adding guessed rank")
+                self.simple_testset.add_attribute_vector(att_vector, "ps")
+                self.simple_testset.add_attribute_vector(att_prob_neg, "ps")
+                self.simple_testset.add_attribute_vector(att_prob_pos, "ps")
+                
+                self.simple_testset = AnalyticPairwiseDataset(self.simple_testset) #this 
+                self.simple_testset = CompactPairwiseDataset(self.simple_testset) #and this should have no effect
+                
+                self.reconstructed_hard_testset = self.simple_testset.get_single_set_with_hard_ranks("rank_predicted", "rank_hard")
+                self.reconstructed_soft_testset = self.simple_testset.get_single_set_with_soft_ranks("prob_-1", "prob_1", "rank_soft_predicted", "rank_soft")
+                self.simple_testset = None
+                
+            
+            if n == 120:
+                logging.info("== Scoring correlation ==")
+                scoringset = Scoring(self.reconstructed_hard_testset)
+                ret["kendalltau-hard"], ret["kendalltau-hard-pi"]  = scoringset.get_kendall_tau("rank_hard", self.class_name)
+                ret["kendalltau_b-hard"], ret["kendalltau_b-hard-pi"]  = scoringset.get_kendall_tau_b("rank_hard", self.class_name)
+                ret["b1-acc-hard-1"], ret["b1-acc-hard-any"] = scoringset.selectbest_accuracy("rank_hard", self.class_name)            
+                
+                scoringset = Scoring(self.reconstructed_soft_testset)
+                ret["kendalltau-soft"], ret["kendalltau-soft-pi"] = scoringset.get_kendall_tau("rank_soft", self.class_name)
+                ret["kendalltau_b-soft"], ret["kendalltau_b-soft-pi"] = scoringset.get_kendall_tau_b("rank_soft", self.class_name)
+                ret["b1-acc-soft-1"], ret["b1-acc-soft-any"] = scoringset.selectbest_accuracy("rank_soft", self.class_name)        
+                
+            
         return ret
 #        
 #    def save_state(self, params, rep, n):
@@ -293,21 +249,9 @@ class AutorankingSuite(PyExperimentSuite):
             shutil.copy(test_filename, "testset.jcml")
 
 
-def modify_params():
-    #read parameters
-    #define initial sets
-    #create a new config file
-    #pass it in sys.argv
-    pass
-
-def check_results():
-    pass
-    #get relevant variables
-    #augment set that was most succesful
-    
-
 import argparse, ConfigParser
 from operator import itemgetter
+import shutil
 
 def load_data():
     parser = argparse.ArgumentParser()
@@ -325,6 +269,10 @@ def load_data():
     trainset_orange_filename = eval(cparser.get(experiment, "training_tab"))
     critical_score = eval(cparser.get(experiment, "critical_score"))
     path = cparser.get("DEFAULT", "path")
+    exppath = os.path.join(path, experiment)
+    shutil.rmtree(exppath, True)
+    os.mkdir(exppath)
+    
     orangedata = Table(trainset_orange_filename)
     return orangedata, cparser, experiment, critical_score, path
 
@@ -342,7 +290,7 @@ def get_new_config(initial_config, path, featuresets, experiment, repetition):
         attsets.append('"{}"'.format(attset_id))
     attsets = "[{}]".format(",".join(attsets))
     initial_config.set(experiment, "att", attsets)
-    filename = os.path.join(path, "experiment_{}.cfg".format(repetition))
+    filename = os.path.join(path, experiment, "experiment_{}.cfg".format(repetition))
     f = open(filename, "w")
     initial_config.write(f)
     f.close()
@@ -403,31 +351,53 @@ def get_new_featuresets(featureset_scores, previous_value):
     best_featureset, best_value = featureset_scores[0]
     converged = False
     new_featuresets = []
-    print "best_featureset =",best_featureset
     if best_value < previous_value:
         converged = True
-        print "didn't improve value, stopping"
+        logging.info("None of the alternatives improved score, stopping")
         return converged, new_featuresets
     for featureset, value in featureset_scores[1:]:
         if value >= previous_value:
             lastfeature = featureset[-1]
             new_featureset = copy.copy(best_featureset)
             new_featureset.append(lastfeature)
-            print len(new_featureset)
             if not set(new_featureset) == set(best_featureset) and new_featureset not in new_featuresets:
                 new_featuresets.append(new_featureset)
+        logging.debug("New featureset has a length of %d", len(new_featureset))
     if new_featuresets == []:
         converged = True
-        print "didn't find any sufficient candidates, stopping"
+        logging.info("There are no further featureset expansions possible. The current best featureset is %s", best_featureset)
     return converged, new_featuresets
 
 
+class StreamToLogger(object):
+   """
+   Fake file-like stream object that redirects writes to a logger instance.
+   """
+   def __init__(self, logger, log_level=logging.INFO):
+      self.logger = logger
+      self.log_level = log_level
+      self.linebuf = ''
+ 
+   def write(self, buf):
+      for line in buf.rstrip().splitlines():
+         self.logger.log(self.log_level, line.rstrip())
+ 
+ 
 if __name__ == '__main__':
-    logging.basicConfig(filename='example.log',level=logging.DEBUG)
     converged = False
     
     repetition = 0
     orangedata, config, experiment, critical_score, path = load_data()
+    FORMAT = "%(asctime)-15s %(message)s"
+    logging.basicConfig(filename=os.path.join(path, experiment, 'search.log'),level=logging.DEBUG,format=FORMAT)
+    
+    stderr_logger = logging.getLogger('STDERR')
+    sl = StreamToLogger(stderr_logger, logging.INFO)
+    sys.stderr = sl
+    
+    logging.info("Working in path %s", path)
+    logging.info("Original features are %d", len(orangedata.domain.features))
+    
     allowed_features = filter_features(orangedata.domain.features)
     featuresets = [[feature.name] for feature in allowed_features][:10]
     previous_value = 0
@@ -438,7 +408,7 @@ if __name__ == '__main__':
         
         #creat a new configfile and pass it by overriding commandline params
         newconfig_filename = get_new_config(config, path, featuresets, experiment, repetition)
-        print newconfig_filename
+        logging.debug("Iteration filename %s", newconfig_filename)
         get_new_commandline_args(newconfig_filename)
         
         #run the experiment
@@ -446,11 +416,13 @@ if __name__ == '__main__':
         mysuite.start()
         
         featureset_scores = get_scores(mysuite, path, experiment, critical_score)
-        print featureset_scores
+        logging.debug("Featureset scores: \n%s\n", featureset_scores)
+        logging.info("Best score for iteration %d = %s", repetition,  featureset_scores[0][1])
+        logging.info("Featureset %s", featureset_scores[0][0])
         #the featureset to augment
         
         converged, featuresets = get_new_featuresets(featureset_scores, previous_value)
-        print featuresets[0][1]
+        
         
     
 
