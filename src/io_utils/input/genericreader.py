13bbb5ccb62f706feb6d565561e88ca200ccaead
elav01 <elav01>
2012-09-06 18:50:04 +0000

    Fix BLEU calculation from n=1 to variable n-gram order with default n=4 BLEU dataset-level calculation Dataset iterate function (same as self.parallelsentences) Merge parallelsenteces by also copying reference Script to select several predicted/human ranks and evaluate resulting set with automatic metrics
    Former-commit-id: f4ce7ecdaeba7cd77311ce54cc210b8e8ad89855

--- src/io_utils/input/genericreader.py
+++ src/io_utils/input/genericreader.py
@@ -10,18 +10,19 @@ from sentence.dataset import DataSet
 
 class GenericReader:
     '''
-    classdocs
+    Abstract base class to describe the basic functionality of a reader, i.e. a mechanism that can
+    import data from external entities (e.g. files) for use with the framework.
     '''
     
-
     
     def __init__(self, input_filename, load = True):
         """
-        Constructor. Creates an XML object that handles ranking file data
-        @param input_filename: the name of XML file
+        Constructor. Creates a memory object that handles file data
+        @param input_filename: the name of file
         @type input_filename: string
         @param load: by turning this option to false, the instance will be 
-                     initialized without loading everything into memory
+                     initialized without loading everything into memory. This can be
+                     done later by calling .load() function
         @type load: boolean 
         """
         self.input_filename = input_filename
@@ -35,11 +36,9 @@ class GenericReader:
         raise NotImplementedError( "Should have implemented this" )
 
         
-    
     def unload(self):
         raise NotImplementedError( "Should have implemented this" )
 
-    
     
     def get_parallelsentence(self, XMLEntry):   
         raise NotImplementedError( "Should have implemented this" )
@@ -48,13 +47,20 @@ class GenericReader:
         """
         Returns the contents of the parsed file into an object structure, which is represented by the DataSet object
         Note that this will cause all the data of the file to be loaded into system memory at once. 
-        For big data sets this may not be optimal, so consider sentence-by-sentence reading with SAX (e.g. saxjcml.py)
+        For big data sets this may not be optimal, so consider sentence-by-sentence reading with SAX, or CElementTree (e.g. saxjcml.py)
         @return the formed data set
         @rtype DataSet
         """
         return DataSet(self.get_parallelsentences())
         
     def get_parallelsentences(self):
+        """
+        Returns the contents of the parsed file into an a list with 
+        ParallelSentence objects. Note that this will cause all the data of the file to be loaded into system memory at once. 
+        For big data sets this may not be optimal, so consider sentence-by-sentence reading with SAX or CElementTree (e.g. saxjcml.py)
+        @return the list of parallel sentences
+        @rtype [ParallelSentence, ...]
+        """
         raise NotImplementedError( "Should have implemented this" )
 
     
