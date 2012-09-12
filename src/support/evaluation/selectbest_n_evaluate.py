a432e886c26118be8c1daffb67efc7d54f46386c
elav01 <elav01>
2012-09-12 14:16:14 +0000

    Improved meteor code Improved script to evaluate ranks with automatic metrics

--- src/support/evaluation/selectbest_n_evaluate.py
+++ src/support/evaluation/selectbest_n_evaluate.py
@@ -15,15 +15,61 @@ import sys
 from sentence.parallelsentence import ParallelSentence
 from sentence.scoring import Scoring
 
+
+def sort_sentences_per_system(dataset, predicted_rank_name, original_rank_name):
+    sentences_per_system={}
+    for parallelsentence in dataset:
+        
+        reference = parallelsentence.get_reference()
+        
+        predicted_rank_vector = [float(f) for f in parallelsentence.get_filtered_target_attribute_values(predicted_rank_name, "system", "_ref")]
+        original_rank_vector = [float(f) for f in parallelsentence.get_filtered_target_attribute_values(original_rank_name, "system", "_ref")]
+        
+        min_predicted = min(predicted_rank_vector)     
+        min_original = min(original_rank_vector)
+        max_original = max(original_rank_vector)
+        
+            
+        predicted_best = False
+        for translation in parallelsentence.get_translations():
+            
+            
+            translation_string = translation.get_string()
+            reference_string = reference.get_string()
+            
+            #first gather based on the system name
+            system = translation.get_attribute("system")
+            sentence_tuple = (translation_string, [reference_string])    
+            sentences_per_system.setdefault(system, []).append(sentence_tuple)
+
+            #then get best and worst human-ranked systems and best estimated system
+            if float(translation.get_attribute(predicted_rank_name)) == min_predicted and not predicted_best:
+                sentences_per_system.setdefault("-predicted_best", []).append(sentence_tuple)
+                predicted_best = True
+            if float(translation.get_attribute(original_rank_name)) == min_original:
+                sentences_per_system.setdefault("-human_best", []).append(sentence_tuple)
+            elif float(translation.get_attribute(original_rank_name)) == min_original+1:
+                sentences_per_system.setdefault("-human_second", []).append(sentence_tuple)
+            elif float(translation.get_attribute(original_rank_name)) == min_original+2:
+                sentences_per_system.setdefault("-human_third", []).append(sentence_tuple)
+            elif float(translation.get_attribute(original_rank_name)) == min_original+3:
+                sentences_per_system.setdefault("-human_fourth", []).append(sentence_tuple)
+            elif float(translation.get_attribute(original_rank_name)) == max_original:
+                sentences_per_system.setdefault("-human_worst", []).append(sentence_tuple)
+                
+    return sentences_per_system
+
 if __name__ == '__main__':
     datafile = sys.argv[1]
     
         
     java_classpath = ["/home/elav01/taraxu_tools/meteor-1.3/meteor-1.3.jar","/home/elav01/taraxu_tools/meteor-1.3","/usr/share/py4j/py4j0.7.jar","/home/elav01/workspace/TaraXUscripts/src/experiment/autoranking"]
     dir_path = "/home/elav01/workspace/TaraXUscripts/src/experiment/autoranking"
+    meteor = MeteorGenerator("en", java_classpath, dir_path)
+    
+    scorers = [bleu, meteor]
     
     dataset = JcmlReader(datafile).get_dataset()
-    #meteor = MeteorGenerator("en", java_classpath, dir_path)
     
     #optional 
     if len(sys.argv) == 3:
@@ -39,53 +85,12 @@ if __name__ == '__main__':
     predicted_rank_name = "rank_soft"
     original_rank_name = "rank"
     
-    sentences_per_system={}
-    for parallelsentence in dataset:
-        
-        reference = parallelsentence.get_reference()
-        
-        predicted_rank_vector = parallelsentence.get_filtered_target_attribute_values(predicted_rank_name, "system", "_ref")
-        original_rank_vector = parallelsentence.get_filtered_target_attribute_values(original_rank_name, "system", "_ref")
-        
-        min_predicted = min(predicted_rank_vector)     
-        min_original = min(original_rank_vector)
-        max_original = max(original_rank_vector)
-        
-        
-        for translation in parallelsentence.get_translations():
-            if translation.get_attribute(predicted_rank_name) == min_predicted:
-                predicted_first_sentences.append((translation.get_string(), [reference.get_string()]))
-                break
-        for translation in parallelsentence.get_translations():
-            if translation.get_attribute(original_rank_name) == min_original:
-                original_first_sentences.append((translation.get_string(), [reference.get_string()]))
-                break
-        for translation in parallelsentence.get_translations():
-            if translation.get_attribute(original_rank_name) == max_original:
-                original_last_sentences.append((translation.get_string(), [reference.get_string()]))
-                break
-            
-        for translation in parallelsentence.get_translations():
-            system = translation.get_attribute("system")
-            translation_string = translation.get_string()
-            reference_string = reference.get_string()
-            sentence_tuple = (translation_string, [reference_string])    
-            sentences_per_system.setdefault(system, []).append(sentence_tuple)
-        
+    sentences_per_system = sort_sentences_per_system(dataset, predicted_rank_name, original_rank_name)        
     
-    for system, sentence_tuples in sentences_per_system.iteritems():
-        print system, round(100.00*bleu.score_sentences(sentence_tuples),2), len(sentence_tuples)
+    for scorer in scorers:
+        print scorer.__name__
+        print "----------------"
+        for system, sentence_tuples in sorted(sentences_per_system.items()):
+            print system, "&", round(100.00*scorer.score_sentences(sentence_tuples),2), "&", len(sentence_tuples), " \\\\"
         
-    
-    print
         
-            
-    bleuscore_gold = round(100.00*bleu.score_sentences(original_first_sentences),2)
-    bleuscore_worst = round(100.00*bleu.score_sentences(original_last_sentences),2)
-    bleuscore_predicted = round(100.00*bleu.score_sentences(predicted_first_sentences),2)
-    
-    print "Best", bleuscore_gold
-    print "worst", bleuscore_worst
-    print "ours", bleuscore_predicted
-    print
-    
