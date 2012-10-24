d635d65d85373bbd7ea7d3f16251839546cc40ce
elav01 <elav01>
2012-10-24 17:37:00 +0000

    Script that will help me clean up attribute lists

--- src/support/preprocessing/select_attributes.py
+++ src/support/preprocessing/select_attributes.py
@@ -10,7 +10,8 @@ import sys
 
 if __name__ == '__main__':
     dataset = JcmlReader(sys.argv[1]).get_dataset()                    
-    expressions = sys.argv[2:] 
+    expressions = sys.argv[2:]
+    print expressions
 #                   ["bb_.*"], #baseline
 #                   #20first with gain radio with manual removal
 #                   ["tgt-1_berkeley-n","src_grammar_subject_verb_agreement_matches","src_grammar_noun_adjective_confusion_chars","src_grammar_wrong_word_matches","tgt-1_lgt_pp_v_plural_5_","tgt-1_lgt_nom_adj_fem_6_","tgt-1_lgt_double_punctuation","tgt-1_lgt_comma_parenthesis_whitespace","src_grammar_wrong_word","tgt-1_lgt_pp_v_singular_4_","src_style_do_not_use_complex_coordinations_chars","src_style_avoid_multiple_negation_chars","src_grammar_wrong_verb_form", "src_grammar_np_number_agreement_chars", "tgt-1_lgt_det_nom_masc_3", "src_style_repeat_modal_verb", "src_grammar_np_number_agreement", "src_style_disambiguate_that", "src_style_verb_close_to_subject_chars"],
@@ -22,7 +23,7 @@ if __name__ == '__main__':
 #                   ]
     for expression in expressions:
         attributes = dataset.select_attribute_names(expression)
-        print len(attributes),": " , ",".join(attributes)
-        print '["%s"]' % '","'.join(attributes)
+        print ",".join(attributes)
+#        print '["%s"]' % '","'.join(attributes)
         
     
