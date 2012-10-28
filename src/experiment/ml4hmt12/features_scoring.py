959445829e1a6b2fbc007e539153c40b1fdbad38
elav01 <elav01>
2012-10-28 17:13:17 +0000

    Print based on scores

--- src/experiment/ml4hmt12/features_scoring.py
+++ src/experiment/ml4hmt12/features_scoring.py
@@ -8,25 +8,27 @@ import Orange
 import sys
 
 data = Orange.data.Table(sys.argv[1])
-scores = [Orange.feature.scoring.Relief,
-          Orange.feature.scoring.InfoGain,
-          Orange.feature.scoring.GainRatio,
-          Orange.feature.scoring.Gini,
-          Orange.feature.scoring.Relevance,
-          Orange.feature.scoring.Cost,
-          Orange.feature.scoring.Distance,
-          Orange.feature.scoring.MDL]
+scores = [Orange.feature.scoring.Relief(),
+          Orange.feature.scoring.InfoGain(),
+          Orange.feature.scoring.GainRatio(),
+          Orange.feature.scoring.Gini(),
+          Orange.feature.scoring.Relevance(),
+          Orange.feature.scoring.Cost(),
+          Orange.feature.scoring.Distance(),
+          Orange.feature.scoring.MDL()]
 
 
 if __name__ == '__main__':
     print 'Feature scores for best three features (with score_all):'
-    for score in scores:
-        print score
-        print "---------"
-        ma = Orange.feature.scoring.score_all(data, score=score)
-        for m in ma:
-            print "%5.4f\t%s" % (m[1], m[0])
-        print 
+    
+    for feature in data.domain.features:
+        print "{}\t".format(feature.name)
+        for score in scores:
+            print "{}\t".format(score(feature, data)),
+        print
+    
+            
+            
         
     
     
