2ea4b0e68b41474573598fde69673b76dcdcfae9
elav01 <elav01>
2012-10-28 17:06:11 +0000

    Scoring function for features
    Former-commit-id: c6e16e9fdfa2674777bbd442d8082b1e073ab213

---
+++ src/experiment/ml4hmt12/features_scoring.py
@@ -0,0 +1,28 @@
+'''
+Created on Oct 28, 2012
+
+@author: Eleftherios Avramidis
+'''
+
+import Orange
+import sys
+
+data = Orange.data.Table(sys.argv[1])
+scores = [Orange.feature.scoring.Relief,
+          Orange.feature.scoring.InfoGain,
+          Orange.feature.scoring.GainRatio,
+          Orange.feature.scoring.Gini,
+          Orange.feature.scoring.Relevance,
+          Orange.feature.scoring.Cost,
+          Orange.feature.scoring.Distance,
+          Orange.feature.scoring.MDL]
+
+
+if __name__ == '__main__':
+    print 'Feature scores for best three features (with score_all):'
+    for score in scores:
+        ma = Orange.feature.scoring.score_all(data, score=score)
+        for m in ma:
+            print "%5.4f\t%s" % (m[1], m[0])
+    
+    
