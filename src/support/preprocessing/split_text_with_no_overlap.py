6162230a9aaf22858c971755b0af18fb32443c31
elav01 <elav01>
2012-11-07 18:04:20 +0000

    Do the same for source and target sentences
    Former-commit-id: b14ca8437dc7ddf04efabd4ab647a89d6380d49b

--- src/support/preprocessing/split_text_with_no_overlap.py
+++ src/support/preprocessing/split_text_with_no_overlap.py
@@ -4,50 +4,61 @@ Created on 7 Nov 2012
 @author: elav01
 '''
 
-import sys
+import sys, argparse
+
 
 if __name__ == '__main__':
     
-    filename = sys.argv[1]
-    train_output_filename = sys.argv[2]
-    test_output_filename = sys.argv[3]
-    percentage = float(sys.argv[4])    
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('inputstem')
+    parser.add_argument('sourcelang')
+    parser.add_argument('targetlang')
+    parser.add_argument('trainstem')    
+    parser.add_argument('teststem')
+    parser.add_argument('percentage')
     
-    f = open(filename, 'r')
-    lines = [l.strip() for l in f.readlines()]
+    args = parser.parse_args()
+    
+    source_filename = args.inputstem + args.sourcelang
+    target_filename = args.inputstem + args.sourcelang
+    source_training_filename = args.trainstem + args.sourcelang
+    target_training_filename = args.trainstem + args.targetlang
+    source_test_filename = args.teststem + args.sourcelang
+    target_test_filename = args.teststem + args.targetlang
+    
+    f = open(source_filename, 'r')
+    sourcelines = [l.strip() for l in f.readlines()]
     f.close()
     
-    target_test_len = int(percentage * len(lines))
-    training_len = len(lines) - target_test_len
-    valid_test_lines = []
+    f = open(target_filename, 'r')
+    targetlines = [l.strip() for l in f.readlines()]
+    f.close()
+    
+    assert(len(sourcelines)==len(targetlines))
+    
+    target_test_len = int(args.percentage * len(sourcelines))
+    training_len = len(sourcelines) - target_test_len
+    source_valid_test_lines = []
+    target_valid_test_lines = []
     
     
-    while valid_test_lines < target_test_len:
-        sys.stderr.write("I already have {} test lines, whereas the aim is {}. Trainset will contain {} sentences\n".format(len(valid_test_lines), target_test_len, training_len))
+    while len(source_valid_test_lines) < target_test_len:
+        sys.stderr.write("I already have {} test lines, whereas the aim is {}. Trainset will contain {} sentences\n".format(len(source_valid_test_lines), target_test_len, training_len))
         
-        for test_line in lines[training_len+1:]:
+        for source_test_line, target_test_line in zip(sourcelines[training_len+1:], targetlines[training_len+1:]):
             test_line_valid = True
-            for training_line in lines[:training_len]:
-                if training_line == test_line:
+            for source_training_line, target_training_line in zip(sourcelines[:training_len], targetlines[:training_len]):
+                if source_training_line == source_test_line or target_training_line == target_test_line:
                     test_line_valid = False
                     break
             if test_line_valid:
-                valid_test_lines.append(test_line)
+                source_valid_test_lines.append(source_test_line)
+                target_valid_test_lines.append(target_test_line)
         
         training_len = training_len - 10
     
     sys.stderr("Writing training sentences")
-    f = open(train_output_filename, 'w')
-    for training_line in lines[:training_len]:
-        f.write(training_line)
-        f.write('\n')
-    f.close()
-    
-    sys.stderr("Writing test sentences")
-    f = open(test_output_filename, 'w')
-    
-    for testline in valid_test_lines:
-        f.write(testline)
-        f.write('\n')
-
-    f.close()
+    open(source_training_filename, 'w').write("\n".join(sourcelines[:training_len]))
+    open(target_training_filename, 'w').write("\n".join(targetlines[:training_len]))
+    open(source_test_filename, 'w').write("\n".join(source_valid_test_lines))
+    open(target_test_filename, 'w').write("\n".join(target_valid_test_lines))
