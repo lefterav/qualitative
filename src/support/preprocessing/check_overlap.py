957eb1aeceb5598d726227e61388001d35bcc2c7
elav01 <elav01>
2012-11-09 13:44:47 +0000

    Improvements on the overlapping script

--- src/support/preprocessing/check_overlap.py
+++ src/support/preprocessing/check_overlap.py
@@ -11,11 +11,33 @@ if __name__ == '__main__':
     file2 = open(sys.argv[2], 'r')
     
     matched = []
+    threshold = 0.4
+    highmatched = []
+    k = 0
+#    print  "Length of file" ,len(file1.readlines())
     for line1 in file1:
         i=0
+        file2.seek(0)
+        line1 = line1.lower().strip()
+        set1 = set(line1.split()) 
         for line2 in file2:
-            print ".",
+            line2 = line2.lower().strip()
+#            print k, i  
+            set2 = set(line2.split())
+            intersection = set2.intersection(set1)
+            matched.append(len(intersection))
+            #if 1.00*len(intersection)/len(set2) > threshold:
+            #    highmatched.append((k, i, 1.00*len(intersection)/len(set2), line1, line2 ))
             if line1 == line2:
-                matched.append(i) 
+            
+                highmatched.append((k, i, 1.00*len(intersection)/len(set2), line1, line2 ))
             i+=1
-    print matched
+        k+=1
+  
+
+    file1.close()
+    file2.close()
+    for h in highmatched:
+        h = [str(j) for j in h]
+        print "\t".join(h)
+    print 1.00*sum(matched)/len(matched)
