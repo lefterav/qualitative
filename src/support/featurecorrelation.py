9de7d0d9e67da52ac96e9b8c7635af6c87b9e5a5
elav01 <elav01>
2013-01-23 17:42:11 +0000

    Fixed issue with missing class

--- src/support/featurecorrelation.py
+++ src/support/featurecorrelation.py
@@ -8,6 +8,9 @@ import Orange
 if __name__ == '__main__':
     voting = Orange.data.Table("trainset.tab")
     
+    new_domain = orange.Domain([a for a in voting.domain.variables if a.name != 'rank'], voting.domain['rank'])
+    new_data = orange.ExampleTable(new_domain, voting)
+    
     def print_best_100(ma):
         for m in ma[:100]:
             print "%5.3f %s" % (m[1], m[0])
@@ -16,13 +19,13 @@ if __name__ == '__main__':
     
     print 'Relief:\n'
     meas = Orange.feature.scoring.Relief(k=20, m=50)
-    mr = [ (a.name, meas(a, voting)) for a in voting.domain.attributes]
+    mr = [ (a.name, meas(a, new_data)) for a in new_data.domain.attributes]
     mr.sort(key=lambda x: -x[1]) #sort decreasingly by the score
     print_best_100(mr)
     
     print "InfoGain\n"
     
     meas = Orange.feature.scoring.InfoGain()
-    mr = [ (a.name, meas(a, voting)) for a in voting.domain.attributes]
+    mr = [ (a.name, meas(a, new_data)) for a in new_data.domain.attributes]
     mr.sort(key=lambda x: -x[1]) #sort decreasingly by the score
     print_best_100(mr)
