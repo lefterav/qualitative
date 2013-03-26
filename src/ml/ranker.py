3a059b9aacc9b147f004bf3323b47cddda713799
elav01 <elav01>
2013-03-26 15:04:55 +0000

    A little more cleanup code to sort of different types of classifiers
    Former-commit-id: f2c7e17b82a9c56fa031a1318aab4dd15aa015ad

---
+++ src/ml/ranker.py
@@ -0,0 +1,30 @@
+'''
+Created on 26 Mar 2013
+
+@author: elav01
+'''
+
+class Ranker(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, **kwargs):
+        '''
+        Constructor
+        '''
+        
+    def set_data(self, **kwargs):
+        pass
+    
+    def prepare_data(self, **kwargs):
+        pass
+    
+    def train(self, **kwargs):
+        pass
+    
+    
+        
+
+class OrargeRanker(object):
+    pass
