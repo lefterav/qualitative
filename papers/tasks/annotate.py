85569b9d8dcbd0f498931b5e81118703b5c4b20d
elav01 <elav01>
2013-04-02 22:41:24 +0000

    Last bits from commiting in SVN
    Former-commit-id: 01714402ac3853b6d20eec628879e6270c4d46ee

---
+++ papers/tasks/annotate.py
@@ -0,0 +1,21 @@
+'''
+Created on Aug 31, 2011
+
+@author: elav01
+'''
+from experiment.task import Task
+
+class Annotate(Task):
+    '''
+    classdocs
+    '''
+
+    
+    
+         
+         
+    
+        
+    
+    
+        
