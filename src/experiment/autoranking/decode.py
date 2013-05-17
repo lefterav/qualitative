bd42e625857957fa56ff1dec003a7aca2ca1ed00
elav01 <eleftherios.avramidis@dfki.de>
2013-05-17 17:54:21 +0200

    Draft script for decoding given previously trained QE models
    Former-commit-id: 62530bc552377eaa639f998421c46876c3109aed

--- src/experiment/autoranking/decode.py
+++ src/experiment/autoranking/decode.py
@@ -108,66 +108,25 @@ class AutorankingSuite(PyExperimentSuite):
         self.class_name = params["class_name"]
         self.class_type = params["class_type"]
         
-        self.training_sets = params["training_sets"].format(**params).split(',')
         self.testset = params["test_set"].format(**params)
         self.ties = params["ties"]
-    
+        
+        objectfile = open(params["trained_classifier"], 'r')
+        self.classifier = OrangeClassifier(pickle.load(objectfile))
+        objectfile.close()
     
     def iterate(self, params, rep, n):
         ret = {}
-        
-#        print "experiment", os.getcwd()
-#        print "iteration", n
-        
-#        if n == 0:
-#            import annotate_updated as annotate
-#            from ruffus import pipeline_run
-#            pipeline_run([annotate.analyze_external_features]) 
-        
-        if n == 0:
-            print "fetch training set"
-            parallelsentences = []
-            for training_set in self.training_sets:
-                parallelsentences.extend(JcmlReader(training_set).get_parallelsentences())
-            
-            self.trainset = DataSet(parallelsentences)  
-        
-        #TODO: alter training filters?
         
         if n == 10:
             print "fetch test set"
             shutil.copy(self.testset, "testset.jcml")
             self.testset = JcmlReader("testset.jcml").get_dataset() 
             
-        if n == 20:
-            print "pairwise training set"
-                
-            self.trainset = AnalyticPairwiseDataset(
-                                                    self.trainset, include_references = self.include_references, 
-                                                    replacement = self.replacement, 
-                                                    filter_unassigned = self.filter_unassigned,
-                                                    restrict_ranks = self.restrict_ranks
-                                                    )
-            
-            if not self.ties:  
-                self.trainset.remove_ties()
-            
-            #SAVE
-            Parallelsentence2Jcml(self.trainset).write_to_file("pairwise_trainset.jcml")    
-            
         if n == 30:
             print "pairwise testset"
             self.testset = AnalyticPairwiseDataset(self.testset, replacement = self.replacement)
             
- 
-        
-        if n == 40:
-            print "add difference features : trainset"
-            parallelsentences = self.trainset.get_parallelsentences()
-            parallelsentences = DiffGenerator().add_features_batch(parallelsentences)
-            Parallelsentence2Jcml(parallelsentences).write_to_file("diff_trainset.jcml")  
-
-        
         
         if n == 50:
             print "add difference feature : testset"
@@ -177,34 +136,6 @@ class AutorankingSuite(PyExperimentSuite):
             parallelsentences = DiffGenerator().add_features_batch(parallelsentences)
             Parallelsentence2Jcml(parallelsentences).write_to_file(self.pairwise_test_filename)  
             
-        
-        if n == 60:
-            print "produce orange trainset"
-            
-            input_file = "diff_trainset.jcml"
-            self.trainset_orange_filename = "trainset.tab"
-            
-            if os.path.isdir("/local"):
-                dir = "/local"
-            else:
-                dir = "."
-            
-            
-            CElementTreeJcml2Orange(input_file, 
-                 self.class_name,
-                 self.active_attributes, 
-                 self.meta_attributes, 
-                 self.trainset_orange_filename, 
-                 compact_mode = True, 
-                 discrete_attributes=self.discrete_attributes,
-                 hidden_attributes=self.hidden_attributes,
-                 get_nested_attributes=True,
-                 dir=dir,
-                 remove_infinite=self.remove_infinite
-                 #filter_attributes={"rank" : "0"},
-#                 class_type=class_type
-                ).convert()
-        
         
         if n == 70:
             print "produce orange testset"
@@ -232,28 +163,6 @@ class AutorankingSuite(PyExperimentSuite):
 #                 class_type=class_type
                 ).convert()
             
-        if n == 80:
-            print "train classifier"
-            input_file = self.trainset_orange_filename 
-            self.output_file = "classifier.clsf"
-            
-            trainset = Table(input_file)
-            
-            mylearner = self.learner(**self.classifier_params)
-            trained_classifier = mylearner(trainset)
-            self.classifier = OrangeClassifier(trained_classifier)
-            self.classifier.print_content()
-            
-        
-        #give the possibility to calculate classification accuracy in the end
-        if (n == 85 and not self.delay_accuracy) or (n == 185 and self.delay_accuracy):
-            print "evaluate classifier with cross-fold validation"
-            orangeData = Table(self.trainset_orange_filename)
-            learner = self.learner(**self.classifier_params)
-            cv = evaluation.testing.cross_validation([learner], orangeData, 10)
-            ret["CA"] = evaluation.scoring.CA(cv)[0]
-            ret["AUC"] = evaluation.scoring.AUC(cv)[0]
-            
         if n == 90:
             print "test_classifier"
             
@@ -270,51 +179,14 @@ class AutorankingSuite(PyExperimentSuite):
             self.classified_values_vector = [str(v[0]) for v in classified_set_vector]
             self.classified_probs_vector = [(v[1]["-1"], v[1]["1"]) for v in classified_set_vector]
             
-            
-#            print [str(v[1]["-1"]) for v in classified_set_vector]
-#            print classified_set_vector
-        
-#        if n == 95:
-#            print "accuracy over test set"
-#            orangedata = Table(self.testset_orange_filename)
-#            cv = evaluation.testing.default_evaluation([self.learner(**self.classifier_params)], orangedata)
-#            ret["CA_test"] = evaluation.scoring.CA(cv)
-#            ret["AUC_test"] = evaluation.scoring.AUC(cv)
-        
         
         if n == 100:
-            print "EVALUATION"
-            print "reloading coupled test set"
-            self.simple_testset = JcmlReader(self.pairwise_test_filename).get_dataset()
-            
-            print "reconstructing test set"
-            att_vector = [{"rank_predicted": v} for v in self.classified_values_vector]
-            att_prob_neg = [{"prob_-1": v[0]} for v in self.classified_probs_vector]
-            att_prob_pos = [{"prob_1": v[1]} for v in self.classified_probs_vector]
-#            print att_vector
-            
-            print "adding guessed rank"
-            self.simple_testset.add_attribute_vector(att_vector, "ps")
-            self.simple_testset.add_attribute_vector(att_prob_neg, "ps")
-            self.simple_testset.add_attribute_vector(att_prob_pos, "ps")
-            
-            Parallelsentence2Jcml(self.simple_testset).write_to_file("testset-pairwise-with-estranks.jcml")
-            
-            self.simple_testset = RawPairwiseDataset(cast=self.simple_testset) #this 
-#            self.simple_testset = CompactPairwiseDataset(self.simple_testset) #and this should have no effect
-            
-            self.reconstructed_hard_testset = self.simple_testset.get_single_set_with_hard_ranks("rank_predicted", "rank_hard")
+            print "Exporting results"
+
             self.reconstructed_soft_testset = self.simple_testset.get_single_set_with_soft_ranks("prob_-1", "prob_1", "rank_soft_predicted", "rank_soft")
             self.simple_testset = None
             
-        
-        if n == 120:
-            print "Scoring correlation"
-            ret.update(score(self.reconstructed_hard_testset, self.class_name, "hard", "rank_hard"))
-            ret.update(score(self.reconstructed_soft_testset, self.class_name, "soft", "rank_soft"))
-            ret = OrderedDict(sorted(ret.items(), key=lambda t: t[0]))
-         
-            print ret   
+       
         return ret
     
     
@@ -322,21 +194,13 @@ class AutorankingSuite(PyExperimentSuite):
     
     
     def save_state(self, params, rep, n):
-        if n == 0:
-            Parallelsentence2Jcml(self.trainset).write_to_file("trainset.jcml") 
-        if n == 20:
-            Parallelsentence2Jcml(self.trainset).write_to_file("pairwise_trainset.jcml")
+
         if n == 30:
             Parallelsentence2Jcml(self.testset).write_to_file("pairwise_testset.jcml")   
-        if n == 40:
-            pass
         if n == 50:
             Parallelsentence2Jcml(self.testset).write_to_file(self.pairwise_test_filename) 
         
-        if n == 80:
-            objectfile = open(self.output_file, 'w')
-            pickle.dump(self.classifier.classifier, objectfile)
-            objectfile.close()
+
         if n == 90:
             classified_vector_file = open("classified.hard.txt", 'w')
             for value in self.classified_values_vector:
@@ -349,7 +213,6 @@ class AutorankingSuite(PyExperimentSuite):
             classified_prob_file.close()
         if n == 100:
 #            Parallelsentence2Jcml(self.simple_testset).write_to_file("testset.classified.jcml")
-            Parallelsentence2Jcml(self.reconstructed_hard_testset).write_to_file("testset.reconstructed.hard.jcml")
             Parallelsentence2Jcml(self.reconstructed_soft_testset).write_to_file("testset.reconstructed.soft.jcml")
 #        if n == 110:
 #            Parallelsentence2Jcml(self.reconstructed_hard_testset).write_to_file("testset.reconstructed.org.hard.jcml")
@@ -357,40 +220,28 @@ class AutorankingSuite(PyExperimentSuite):
     
     def restore_state(self,params, rep, n):
         self.class_name = "rank" #TODO: hardcoded
-        if n > 0 and n <=20 :
-            self.trainset = JcmlReader("trainset.jcml").get_dataset()
+
         
         if n > 10 and n <=30 :
             self.testset = JcmlReader("testset.jcml").get_dataset()
         
-        if n > 20 and n <=40:
-            self.trainset =  JcmlReader("pairwise_trainset.jcml").get_dataset()
             
         if n > 30 and n <=50:
             self.testset = JcmlReader("pairwise_testset.jcml").get_dataset()
         
         if n > 50:
             self.pairwise_test_filename = "diff_testset.jcml"
-            self.trainset_orange_filename = "trainset.tab"
         
         if n > 70:
             self.testset_orange_filename = "testset.tab"
         
-        if n > 80 and n <= 90:
-            objectfile = open("classifier.clsf", 'r')
-            self.classifier = OrangeClassifier(pickle.load(objectfile))
-            objectfile.close()
         if n > 90:
             
-            classified_vector_file = open("classified.hard.txt", 'r') 
-            self.classified_values_vector = classified_vector_file.readlines()
-            classified_vector_file.close()
             classified_prob_file = open("classified.soft.txt", 'r') 
             self.classified_probs_vector = [tuple(line.split('\t')) for line in classified_prob_file]
             classified_prob_file.close()
         if n > 100:
 #            self.simple_testset = JcmlReader("testset.classified.jcml").get_dataset
-            self.reconstructed_hard_testset = JcmlReader("testset.reconstructed.hard.jcml").get_dataset()
             self.reconstructed_soft_testset = JcmlReader("testset.reconstructed.soft.jcml").get_dataset()
 #        if n == 10:
 #            self.reconstructed_hard_testset = JcmlReader("testset.reconstructed.org.hard.jcml").get_dataset()
@@ -451,6 +302,8 @@ class StreamToLogger(object):
          
     def flush(self):
         pass
+
+
 
 if __name__ == '__main__':
     FORMAT = "%(asctime)-15s [%(process)d:%(thread)d] %(message)s "
