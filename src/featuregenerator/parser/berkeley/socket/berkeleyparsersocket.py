5d2458c83980a650107eb0d6984e54f626c8a7da
elav01 <eleftherios.avramidis@dfki.de>
2013-05-17 16:37:20 +0200

    Fixed conflict
    Former-commit-id: d0fadec29ac406f224b703b8c1af4d3f5c220089

--- src/featuregenerator/parser/berkeley/socket/berkeleyparsersocket.py
+++ src/featuregenerator/parser/berkeley/socket/berkeleyparsersocket.py
@@ -182,7 +182,7 @@ class BerkeleyParserSocket():
         
 
 if __name__ == "__main__":
-    java_classpath = "/home/elav01/.local/share/py4j/py4j0.7.jar:/home/elav01/tools/qualitative/src/support/berkeley-server/lib/BerkeleyParser.jar:/home/elav01/tools/qualitative/src/featuregenerator/parser/berkeley/socket"
+    java_classpath = ["/home/elav01/.local/share/py4j/py4j0.7.jar:/home/elav01/tools/qualitative/src/support/berkeleyserver/lib/BerkeleyParser.jar:/home/elav01/tools/qualitative/src/featuregenerator/parser/berkeley/socket"]
     jvm = JVM(java_classpath)
     socket_no = jvm.socket_no
     gatewayclient = GatewayClient('localhost', socket_no)
