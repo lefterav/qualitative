52e2270777004e2df5954ea9ed3b44af7b92d409
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2013-05-30 19:04:17 +0200

    improved feature correlation script (still pretty drafty)

--- src/support/featurecorrelation.py
+++ src/support/featurecorrelation.py
@@ -7,8 +7,22 @@ Created on Jan 23, 2013
 import Orange
 
 import sys
+from io_utils.sax.cejcml2orange import CElementTreeJcml2Orange
 if __name__ == '__main__':
-    table = Orange.data.Table(sys.argv[1])
+    meta_attributes = "testset,judgement_id,langsrc,langtgt,ps1_judgement_id,ps2_judgement_id,id,tgt-1_score,tgt-1_system,tgt-2_score,tgt-2_system,document_id,judge_id,segment_id"
+    hidden_attributes = "tgt-1_berkeley-tree,tgt-2_berkeley-tree,src_berkeley-tree,rank_diff,tgt-1_ref-lev,tgt-1_ref-meteor_score,tgt-1_ref-meteor_fragPenalty,tgt-1_ref-meteor_recall,tgt-1_ref-meteor_precision,tgt-1_ref-bleu,tgt-2_ref-lev,tgt-2_ref-meteor_score,tgt-2_ref-meteor_fragPenalty,tgt-2_ref-meteor_recall,tgt-2_ref-meteor_precision,tgt-2_ref-bleu"
+
+    input_filename = sys.argv[1]
+    if input_filename.endswith(".jcml"):
+        tab_filename = input_filename.replace(".jcml",".tab"),
+        input_filename = CElementTreeJcml2Orange(input_filename, 
+                                                 sys.argv[2], 
+                                                 [], #all 
+                                                 meta_attributes, 
+                                                 
+                                                 hidden_attributes=hidden_attributes)
+        input_file = tab_filename
+    table = Orange.data.Table()
     classname = sys.argv[2]
     
     new_domain = Orange.data.Domain([a for a in table.domain.variables if a.name != classname], table.domain[classname])
