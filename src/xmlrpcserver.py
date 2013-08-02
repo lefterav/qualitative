56c5260bf499783894b0859381d6140d8c30ff14
elav01 <eleftherios.avramidis@dfki.de>
2013-08-02 21:16:56 +0200

    New module for application time autoranking and functionality added but not tested
    Former-commit-id: f116f6c1947449c29cd04b8a204d2c2fd7590191

--- src/xmlrpcserver.py
+++ src/xmlrpcserver.py
@@ -6,13 +6,7 @@ import codecs
 from types import *
 #import base64
 import unicodedata
-import cPickle as pickle
-from sentence.sentence import SimpleSentence
 
-from ml.lib.orange import OrangeRuntimeRanker 
-from sentence.parallelsentence import ParallelSentence
-
-from ml.lib import orange
 
 _baseclass = SimpleXMLRPCServer
 class StoppableServer(_baseclass):
@@ -39,43 +33,7 @@ class StoppableServer(_baseclass):
 
 
 
-class Autoranking:
 
-    def __init__(self, featuregenerators, attset, classifiername):
-        self.featuregenerators = featuregenerators
-        self.attset = attset
-        self.ranker = OrangeRuntimeRanker(classifiername)
-        
-    def rank(self, source, translations):
-        sourcesentence = SimpleSentence(source)
-        translationsentences = [SimpleSentence(t) for t in translations]
-        parallelsentence = ParallelSentence(sourcesentence, translationsentences)
-        
-        #annotate the parallelsentence
-        annotated_parallelsentence = self._annotate(parallelsentence)
-        ranking = self.ranker.rank_sentence(annotated_parallelsentence)
-        
-        
-        return ranking
-        
-    def _annotate(self, parallelsentence):
-        for featuregenerator in self.featuregenerators:
-            parallelsentence = featuregenerator.add_features_parallelsentence(parallelsentence)
-        return parallelsentence
-    
-
-if __name__ == "__main__":
-    
-    classifier_filename = "/share/taraxu/selection-mechanism/wmt13/sentenceranking/autoranking_wmt13_newfeatures1_de_en/class_nameranklangpairde-eninclude_references0.0ties0.0trainset_modeannotatedattattset_24classifierLogReg/classifier.clsf"
-    
-    
-    source = "Wir müssen diese Lösung diskutieren"
-    target1 = "We have to discuss this solution"
-    target2 = "This solution have we to discuss"
-    target3 = "We must this solution discuss"
-
-    autoranker = Autoranking(classifier_filename)
-    print autoranker.rank(source, [target1, target2, target3])
 
 # Get command line arguments
 # args = sys.argv[1:]
