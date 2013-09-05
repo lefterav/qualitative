648591c4d658d8d940881d32dc4db64214946d8b
Eleftherios Avramidis <elav01@lns-87004.sb.dfki.de>
2013-09-05 15:25:16 +0200

    Working version via XML-RPC

--- src/net/xmlrpctest.py
+++ src/net/xmlrpctest.py
@@ -1,6 +1,6 @@
 import xmlrpclib
 
-s = xmlrpclib.ServerProxy('http://localhost:8088')
-print s.rank("originaldoc", "mosestranslation", "lucytranslation", "googletranslation", "langsrc", "langtgt")  
+s = xmlrpclib.ServerProxy('http://localhost:8089')
+print s.qualityRank("Das ist eine Entscheidung.", "A decision is this.", "This is a decision .", "This is a very bad decision.", "de", "en")  
 
 # Print list of available methods
