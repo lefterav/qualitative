0efc3f73928af3f61370d0e37d69f4ec83800b63
Lukas Poustka <lupo01@lns-87004.sb.dfki.de>
2014-01-20 21:46:38 +0100

    New output file parameter for debugging.

--- metawp2/lucy-code/LucyGetData.py
+++ metawp2/lucy-code/LucyGetData.py
@@ -6,13 +6,18 @@ import traceback
 from LucyTrees import BaseTree
 
 
+"""
+This class converts new 'XML' lucy format into old 'bracket' format.
+After that various attributes from each sentence are extracted.
+"""
 class LucyGetData():
     def __init__(self):
-        filename = sys.argv[1]
+        filenameInput = sys.argv[1]
+        filenameOutput = sys.argv[2]
         self.sentenceSeparator = '#sentence-separator#'
 
-        data = self.convert_lucy_file(filename)
-        self.get_attributes(data)
+        data = self.convert_lucy_file(filenameInput)
+        self.get_attributes(data, filenameOutput)
 
 
     """
@@ -30,16 +35,21 @@ class LucyGetData():
     """
     This function returns for each sentence in lucy file a dictionary with parsed parameters.
     @param data: list with analysis, transfer and generation part (string)
+    @param outFile: output filename where the dictionary will be saved as strings for manual check (string)
     """
-    def get_attributes(self, data):
-
+    def get_attributes(self, data, outFilename):
+        
+        outFile = codecs.open(outFilename, 'w', 'utf-8')
         for part in self.get_parts(data):
             trees = re.split(self.sentenceSeparator, part)[1:]
 
             for i in range(len(trees)):
-                print 'Tree No.: %s, Tree char length: %s' % (str(i), len(trees[i]))
-                bt = BaseTree(trees[i], {}, '')                                      
-                print bt.dct, '\n\n'
+                treeInfo = '\n\n\n\nTree No %s, tree char length %s:\n\n' % (str(i), len(trees[i]))
+                outFile.write(treeInfo); print treeInfo
+                bt = BaseTree(trees[i], {}, '')
+                outFile.write(str(bt.dct))
+                bt.dct = {}
+        outFile.close()
 
 
     """
@@ -60,8 +70,17 @@ class LucyGetData():
         data = data.replace('<CAN>', 'CAN ')
         data = data.replace('</CAN>', ' ')
         data = data.replace(' )', ')')
+        leftBracketCAN = re.search('CAN ("[^"]*\([^"]*")', data)
+        if leftBracketCAN: data = data.replace(leftBracketCAN.group(1), leftBracketCAN.group(1).replace('(', '<left-round-bracket>'))
+        leftBracketALO = re.search('ALO ("[^"]*\([^"]*")', data)
+        if leftBracketALO: data = data.replace(leftBracketALO.group(1), leftBracketALO.group(1).replace('(', '<left-round-bracket>'))
+        rightBracketCAN = re.search('CAN ("[^"]*\)[^"]*")', data)
+        if rightBracketCAN: data = data.replace(rightBracketCAN.group(1), rightBracketCAN.group(1).replace(')', '<right-round-bracket>'))
+        rightBracketALO = re.search('ALO ("[^"]*\)[^"]*")', data)
+        if rightBracketALO: data = data.replace(rightBracketALO.group(1), rightBracketALO.group(1).replace(')', '<right-round-bracket>'))
         print '%s converted successfully' % filename
         return data
 
 
 LucyGetData()
+#print re.search('CAN ("[^"]*\([^"]*")', 'CAN "("').group(1)
