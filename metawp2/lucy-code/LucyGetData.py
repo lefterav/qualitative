9ef76f4335316c38dfb612b4de112fc4a96aa5cf
Lukas Poustka <lupo01@lns-87004.sb.dfki.de>
2014-01-23 01:31:50 +0100

    Changed interface of functions.
    Former-commit-id: c837f38f5db3500b158e45d38a0e6044e491a163

--- metawp2/lucy-code/LucyGetData.py
+++ metawp2/lucy-code/LucyGetData.py
@@ -12,18 +12,13 @@ After that various attributes from each sentence are extracted.
 """
 class LucyGetData():
     def __init__(self):
-        filenameInput = sys.argv[1]
-        filenameOutput = sys.argv[2]
         self.sentenceSeparator = '#sentence-separator#'
-
-        data = self.convert_lucy_file(filenameInput)
-        self.get_attributes(data, filenameOutput)
 
 
     """
     This function splits the lucy file to analysis, transfer and generation part.
-    @param data: string
-    @return: list with 3 lucy file parts
+    @param data: lucy tree file (string)
+    @return: analysis, transfer and generation part of lucy tree file (list)
     """
     def get_parts(self, data):
         analysis = data.split('<analysis>')[1].split('</analysis>')[0]
@@ -34,31 +29,43 @@ class LucyGetData():
 
     """
     This function returns for each sentence in lucy file a dictionary with parsed parameters.
-    @param data: list with analysis, transfer and generation part (string)
+    @param data: old bracket lucy tree format (string)
     @param outFile: output filename where the dictionary will be saved as strings for manual check (string)
     """
-    def get_attributes(self, data, outFilename):
+    def process_old_format_file(self, data, outFilename):
         
         outFile = codecs.open(outFilename, 'w', 'utf-8')
+        
+        # analysis, transfer and generation part
         for part in self.get_parts(data):
             trees = re.split(self.sentenceSeparator, part)[1:]
 
             for i in range(len(trees)):
                 treeInfo = '\n\n\n\nTree No %s, tree char length %s:\n\n' % (str(i), len(trees[i]))
                 outFile.write(treeInfo); print treeInfo
-                bt = BaseTree(trees[i], {}, '')
-                outFile.write(str(bt.dct))
-                bt.dct = {}
+                sentenceFeatures = self.process_sentence(trees[i])
+                outFile.write(str(sentenceFeatures))
         outFile.close()
 
 
     """
-    This function converts new lucy format into the old one.
-    @param data: new lucy format (XML)
-    @return: old lucy format with round brackets
+    This function calls BaseTreee class, where tree features are parsed and saved as dict.
+    @param sentence: Lucy sentence in old bracket format (string)
+    @return: dictionary with parsed features (dict)
     """
-    def convert_lucy_file(self, filename):
-        data = codecs.open(filename, 'r', 'utf-8').read()
+    def process_sentence(self, sentence):
+        bt = BaseTree(sentence, {}, '')
+        return bt.dct
+        
+
+    """
+    This function converts new lucy format into the old one.
+    @param data: new lucy XML format (string)
+    @return: old lucy format with round brackets (string)
+    """
+    def process_new_format_file(self, filename):
+        data = codecs.open(filename, 'r', 'utf-8').read() # remove from here (1 sentence per time)
+        # 1 sentence per time, but from bracket format (old one)
         data = re.sub('<NODE><SENTENCE>[^<]*</SENTENCE>', '\n%s(\n' % (self.sentenceSeparator), data)
         data = data.replace('<NODE>', '(')
         data = data.replace('</NODE>', ')')
@@ -82,5 +89,11 @@ class LucyGetData():
         return data
 
 
-LucyGetData()
-#print re.search('CAN ("[^"]*\([^"]*")', 'CAN "("').group(1)
+if __name__ == '__main__':
+    filenameInput = sys.argv[1] # path to file with XML lucy format
+    filenameOutput = sys.argv[2] # path where a dict with trees features will be saved as string
+
+    objLucy = LucyGetData()
+    data = objLucy.process_new_format_file(filenameInput)
+    objLucy.process_old_format_file(data, filenameOutput)
+
