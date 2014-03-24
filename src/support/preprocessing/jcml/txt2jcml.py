fe7f66467aba89e9157f0a3fedd9e3676fd258b9
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-03-24 17:10:31 +0100

    Server-side changes for error prediction task

--- src/support/preprocessing/jcml/txt2jcml.py
+++ src/support/preprocessing/jcml/txt2jcml.py
@@ -5,10 +5,10 @@ Created on 23 Feb 2012
 '''
 from optparse import OptionParser
 from sentence.sentence import SimpleSentence
+from collections import OrderedDict
 from sentence.parallelsentence import ParallelSentence
 from io_utils.sax.saxps2jcml import Parallelsentence2Jcml
 from featuregenerator.glassbox.moses.extractor import MosesGlassboxExtractor
-from collections import OrderedDict
 
 if __name__ == '__main__':
 
@@ -16,27 +16,31 @@ if __name__ == '__main__':
     parser.add_option("-s", "--source", dest="source_filename",
                       help="read one source sentence per line from FILE", metavar="FILE")
     
-    parser.add_option("-t", "--translation", nargs=4,  dest="target_filenames", type="str",
-                      help="read one translation output sentence per line from FILE s", metavar="FILE")
+    parser.add_option("-t", "--translation", dest="target_filename",
+                      help="read one translation output sentence per line from FILE", metavar="FILE")
     
-    parser.add_option("-m", "--systems", nargs=4,  dest="system_names",
-                      help="system names")    
+    parser.add_option("-m", "--system", dest="system_name",
+                      help="system name")
+    
   
     parser.add_option("-r", "--reference", dest="reference_filename",
                       help="read one reference sentence per line from FILE", metavar="FILE")
     
-    parser.add_option("-l", "--scores", nargs=4, dest="score_filenames",
+    parser.add_option("-l", "--score", dest="score_filename",
                       help="read one score per line from FILE", metavar="FILE")
     
-    parser.add_option("-a", "--feature-names", nargs=17,  dest="feature_names", type="str",
+    parser.add_option("-a", "--feature-names", action="append", dest="feature_names", type="str",
                       help="a list of feature names", default=[])
-                      
-    parser.add_option("-v", "--target-features-csv", nargs=4,  dest="target_feature_csv_files",
-                      help="a list of CSV files containing system features, one file per system output")
-
     
-    parser.add_option("-q", "--feature-files", nargs=4,  dest="feature_files", type="str", default=[],
+    parser.add_option("-q", "--feature-files", action="append", dest="feature_files", type="str", default=[],
                       help="a list of feature FILEs in respective order")
+
+    parser.add_option("-b", "--target-features-tab", dest="target_features_tab", type="str", 
+                      help="all target features in one file, tab-separated")
+    
+    parser.add_option("-n", "--target-features-tab-names", dest="target_features_tab_names", type="str", 
+                      help="all target feature names in one file, tab-separated")
+
     
     parser.add_option("-o", "--output", dest="output_filename",
                       help="write output to this jcml FILE", metavar="FILE")
@@ -56,32 +60,35 @@ if __name__ == '__main__':
     (opt, args) = parser.parse_args()
     
     source_file = open(opt.source_filename, 'r')
-    
-    print opt.target_filenames
-    print opt.system_names
-    
-    target_files = [open(target_filename, 'r') for target_filename in opt.target_filenames] 
+    target_file = open(opt.target_filename, 'r')
     
     feature_file_objects = [open(f, 'r') for f in opt.feature_files]
-    print opt.target_feature_csv_files, opt.feature_names
+    print opt.feature_files, opt.feature_names
     try:
         reference_file = open(opt.reference_filename, 'r')
     except:
         reference_file = None
     try:
-        score_files = [open(score_filename) for score_filename in opt.score_filenames]
+        score_file = open(opt.score_filename)
     except:
-        score_files = None
+        score_file = None
+        
+    try:
+        target_features_tabfile = open(opt.target_features_tab)
+    except:
+        target_features_tabfile = None
+    
+    try:
+        target_features_tab_names_file = open(opt.target_features_tab_names)
+        target_features_tab_names = target_features_tab_names_file.readline().strip().split("\t")
+        target_features_tab_names_file.close()
+    except:
+        target_features_tab_names = []
+#    print "Feature tab names", target_features_tab_names 
     
     if opt.moseslog:
         extractor = MosesGlassboxExtractor()
         glassbox_features_dicts = extractor.create_dicts_of_sentences_attributes(opt.moseslog)
-    
-    if opt.target_feature_csv_files:
-        target_feature_csv_files = dict([(sys_name,open(feature_filename)) for sys_name, feature_filename in zip(opt.system_names,opt.target_feature_csv_files)])
-    else:
-        target_feature_csv_files = None
-    
 
     parallelsentences = []
     i = 0
@@ -90,6 +97,8 @@ if __name__ == '__main__':
         i+=1
         atts = OrderedDict()
         source_line = source_line.strip()
+        target_line = target_file.readline().strip()
+        
         
         if reference_file:
             reference_line = reference_file.readline().strip()
@@ -97,36 +106,35 @@ if __name__ == '__main__':
         else:
             reference_sentence = None
         
-        if score_files:
-            scores = dict([(system_name, float(score_file.readline().strip())) for score_file, system_name in zip(score_files, opt.system_names)])
+        #target sentence features        
+        if score_file:
+            score = score_file.readline().strip()
+            atts["score"] = score
         
-        #TODO: won't work after number of target sentences changed. look at older version
+        atts["system"] = opt.system_name
+        
+        #process glass-box features
         if opt.moseslog:
             atts.update(glassbox_features_dicts[i-1])
         
+        #process tab-separated features file
+        if target_features_tabfile:
+            feature_values = target_features_tabfile.readline().strip().split("\t")
+            for i, feature_value in enumerate(feature_values):
+                try:
+                    feature_name = target_features_tab_names[i-1]
+                except:
+                    feature_name = i
+                atts["qb_{}".format(feature_name)] = feature_value
+                
         source_sentence = SimpleSentence(source_line)
+        target_sentences = [SimpleSentence(target_line, atts)]
         
-        target_sentences = []
-        
-        #prepare target sentences
-        for target_file, system_name in zip(target_files, opt.system_names):
-           atts = {}
-           atts["system"] = system_name
-           
-           if score_files:
-               atts["rank"] = scores[system_name]
-           if target_feature_csv_files:
-               features = zip(opt.feature_names, target_feature_csv_files[system_name].readline().strip().split(","))
-               atts.update(features)
-               
-           
-    	   target_line = target_file.readline().strip()
-           target_sentences.append(SimpleSentence(target_line, atts))
-    
         additional_atts = {}
         for feature_name, file_object in zip(opt.feature_names, feature_file_objects):
             value = file_object.readline().strip()
             additional_atts[feature_name] = value
+            
                 
         ps_atts =  {"langsrc" : opt.langsrc ,
                      "langtgt" : opt.langtgt ,
