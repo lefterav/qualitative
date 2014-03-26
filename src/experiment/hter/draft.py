b887d3974bf12efce7bffb6a1ead9d1dd89f9b6c
elav01 <eleftherios.avramidis@dfki.de>
2014-03-26 19:55:49 +0100

    First effort to wrap separate ter classifiers

--- src/experiment/hter/draft.py
+++ src/experiment/hter/draft.py
@@ -6,7 +6,7 @@ import sys
 import yaml
 from io_utils.sax.utils import join_jcml, CEJcmlReader
 from io_utils.input.jcmlreader import JcmlReader
-from ml.lib.scikit.scikit import SkRegressor
+from ml.lib.scikit.scikit import SkRegressor, TerRegressor
 import logging as log
 
 #todo: this should be read from config file (or expsuite)
@@ -14,11 +14,26 @@ cfg_path = "/home/elav01/workspace/qualitative/src/experiment/hter/config/svr.cf
 class_name = "ter_substitutions"
 desired_parallel_attributes = []
 desired_source_attributes = []
+ter_class_names = []
 desired_target_attributes = ["q_1022_1","q_1012_1","q_1015_1","q_1001_1","q_1002_1","q_1006_1","q_1036_1","q_1009_1","q_1057_1","q_1054_1","q_1053_1","q_1050_1","q_1049_1"]
 #"q_1075_1","q_1074_1",,"q_1046_1"
 #["qb_1022","qb_1012","qb_1015","qb_1001","qb_1002","qb_1006","qb_1036","qb_1009","qb_1057","qb_1054","qb_1053","qb_1050","qb_1049","qb_1075","qb_1074","qb_1046"]
 
+def train_ter_separately(config, dataset, desired_parallel_attributes, desired_source_attributes, desired_target_attributes):
+    regressors = []
+    for class_name in ter_class_names:
+        regressor = train_regressor(config, dataset, class_name, desired_parallel_attributes, desired_source_attributes, desired_target_attributes)
+        regressors.append(regressor)
+    return TerRegressor(config, regressors)
+    
 
+def train_regressor(config, dataset, class_name, desired_parallel_attributes, desired_source_attributes, desired_target_attributes):
+    regressor = SkRegressor(config)
+    log.info("Loading data")
+    regressor.load_training_dataset(dataset, class_name, desired_parallel_attributes, desired_source_attributes, desired_target_attributes)
+    regressor.set_learning_method()
+    return regressor
+    
 
 if __name__ == '__main__':
     
@@ -38,13 +53,24 @@ if __name__ == '__main__':
     config = None
     with open(cfg_path, "r") as cfg_file:
         config = yaml.load(cfg_file.read())
-         
+    
     #initialize the regressor
     regressor = SkRegressor(config)
     log.info("Loading data")
-    regressor.load_training_dataset(dataset, class_name, desired_parallel_attributes, desired_source_attributes, desired_target_attributes, meta_attributes)
+    regressor.load_training_dataset(dataset, class_name, desired_parallel_attributes, desired_source_attributes, desired_target_attributes)
     regressor.set_learning_method()
     log.info("Performing cross validation")
     scores = regressor.cross_validate_start(cv=5)
+    
+    ter_regressor = train_ter_separately(config, dataset, desired_parallel_attributes, desired_source_attributes, desired_target_attributes)
+    scores = ter_regressor.cross_validate_start(cv=5)
+    
     print scores
+
+
+
+
+from sklearn.cross_validation import check_cv, check_arrays, check_random_state, is_classifier
+from sklearn.metrics import make_scorer
+
     
