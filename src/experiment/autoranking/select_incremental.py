9d30fd41b2b87435e9cf9a3a538e91bd74519f9e
elav01 <eleftherios.avramidis@dfki.de>
2014-07-07 20:49:35 +0200

    XML simple sentence selection: write output to log

--- src/experiment/autoranking/select_incremental.py
+++ src/experiment/autoranking/select_incremental.py
@@ -15,11 +15,24 @@ if __name__ == '__main__':
 
     jcmlfilename = sys.argv[2]
     outputfile = open(sys.argv[3], 'w')
+    counts = dict()
     for parallelsentence in CEJcmlReader(jcmlfilename).get_parallelsentences():
         ranking, description = ranker.rank_sentence(parallelsentence)
-        ranking_dict = OrderedDict(ranking)
-        selected_sentence = ranking_dict[1] 
-        outputfile.write("\n".format(selected_sentence.get_string()))
+        ranking.sort(key=lambda x: int(x.get_attribute("system")))
+        print ranking
+        for rank, sentence in ranking:
+            if rank=="1":
+                selected_sentence = sentence
+                system_name = sentence.get_attribute("system")
+                counts[system_name] = counts.setdefault(system_name, 0) + 1
+                outputfile.write("\n".format(selected_sentence.get_string()))
+                break
+                
+    outputfile.close()                
+    logfile = open(sys.argv[3]+".log", 'w')
+    for system_name in counts.keys():
+        logfile.write("{}:{}\n".format(system_name, counts[system_name]))
+    logfile.close()
         
         
             
