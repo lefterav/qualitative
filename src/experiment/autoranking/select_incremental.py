7b7c8eb188a50eef5eb6cf075c99742a219896b0
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-08 20:00:19 +0200

    Modifications in order to select best system output  directly to a text file

--- src/experiment/autoranking/select_incremental.py
+++ src/experiment/autoranking/select_incremental.py
@@ -16,16 +16,21 @@ if __name__ == '__main__':
     jcmlfilename = sys.argv[2]
     outputfile = open(sys.argv[3], 'w')
     counts = dict()
-    for parallelsentence in CEJcmlReader(jcmlfilename).get_parallelsentences():
+    text = dict()
+    for parallelsentence in CEJcmlReader(jcmlfilename, all_target=True).get_parallelsentences(False):
+#        text = dict([(t.get_attribute("system"), t.get_string()) for t in parallelsentence.get_translations()])
+#        print text
         ranking, description = ranker.rank_sentence(parallelsentence)
-        ranking.sort(key=lambda x: int(x.get_attribute("system")))
-        print ranking
+        #ranking.sort(key=lambda x: x[1].get_attribute("system"))
+
         for rank, sentence in ranking:
             if rank=="1":
                 selected_sentence = sentence
                 system_name = sentence.get_attribute("system")
+                #selected_text = text[system_name]
+                selected_text = selected_sentence.get_string().replace("\r", "").replace("\n", "")
                 counts[system_name] = counts.setdefault(system_name, 0) + 1
-                outputfile.write("\n".format(selected_sentence.get_string()))
+                outputfile.write("{}\n".format(selected_text))
                 break
                 
     outputfile.close()                
