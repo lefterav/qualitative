dc3f76dd94f255203e5052d12c9fb6c66c60a578
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-13 17:04:29 +0200

    Moved old classifier wrappers from 'classifier' to 'ml/var'

--- src/ml/__init__.py
+++ src/ml/__init__.py
@@ -1,4 +1,6 @@
 """
 Unified interface for access to several ML libraries and toolkits.
-New ML libraries should be placed into /lib and implement learner.py or ranker.py 
+New ML libraries should be placed into /lib and implement learner.py or ranker.py
+/var contains old classifier wrappers that still work but need to be
+gradually migrated 
 """
