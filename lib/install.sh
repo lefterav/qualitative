47f9696a29613db31a6ffa3cf6f3838aa1f8ebea
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-14 00:31:46 +0200

    bash script to download required external jar files

---
+++ lib/install.sh
@@ -0,0 +1,4 @@
+wget http://www.dfki.de/~elav01/download/qualitative/lib/BerkeleyParser.jar
+wget http://www.dfki.de/~elav01/download/qualitative/lib/LanguageTool.jar
+wget http://www.dfki.de/~elav01/download/qualitative/lib/meteor-1.3.jar
+wget http://www.dfki.de/~elav01/download/qualitative/lib/py4j0.7.jar
