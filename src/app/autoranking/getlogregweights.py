259bb63e29730d2b0383b32768d068b24aeefac5
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-13 22:12:29 +0200

    Deleted useless stuff from src.app.autoranking
    Former-commit-id: 575d3bc9d2619939b43306f658d8d31073ee9cf5

--- src/app/autoranking/getlogregweights.py
+++
@@ -1,8 +0,0 @@
-import cPickle
-from Orange.classification import logreg
-f=open('classifier.clsf','r')
-c=cPickle.load(f)
-f.close()
-f=open('logreg.dump.log','w')
-f.write(logreg.dump(c))
-f.close()
