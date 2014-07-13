eac2ddc726418924ec575105ce640e37b80dee66
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-13 17:03:55 +0200

    Moved automatically generated documentation from 'doc' to 'doc/api'
    Former-commit-id: 84bc200017d2ea301b1baacef63e39e7008c135f

---
+++ src/ml/var/svm.py
@@ -0,0 +1,25 @@
+"""
+
+@author: Eleftherios Avramidis
+"""
+
+from orange import SVMLearner
+from classifier import OrangeClassifier
+
+
+class SVM( OrangeClassifier ):
+    """
+    classdocs
+    """
+
+
+    def __init__(self, data):
+        """
+        Constructor
+        """
+        l = SVMLearner() 
+        l.svm_type = SVMLearner.Nu_SVC 
+        l.nu = 0.3 
+        l.probability = True
+        self.classifier = l( data.get_data() )
+        
