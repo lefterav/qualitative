db4974700bb0e429585326e2913888f4d59d1c66
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-13 13:45:26 +0200

    Bootstrap for HTER experiment

--- loonybin-mt-toolpack/descriptors/wordalign/MgizaForceAlign.py
+++
@@ -1,7 +0,0 @@
-from loonybin import Tool
-from Mgiza import Mgiza
-
-# Common configuration for Qin's QMT Package including MGIZA and Chaksi
-class MgizaForceAlign(Mgiza):
-    # TODO: Determine requirements of ForceAlign
-    pass
