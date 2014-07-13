b5c568d78c3cb42a676b5f2267d88a6564f4a621
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-13 13:45:26 +0200

    Bootstrap for HTER experiment

--- metawp2/converters/annotate.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-SOURCE_LANG=$1
-TARGET_LANG=$2
-echo "Creating XLIFF parts..."
-python ./converters/joshua2xlf.py ../joshua-$SOURCE_LANG-$TARGET_LANG/6-Test.800-decode-trees/default/final/eNBestOut ../joshua-$SOURCE_LANG-$TARGET_LANG/6-Test.800-decode-trees/default/working/inputs/fSentsIn $SOURCE_LANG  $TARGET_LANG ../joshua-$SOURCE_LANG-$TARGET_LANG/6-Test.800-decode-trees/default/working/inputs/weightsFile 1776 1
-echo "Creating tar archive..."
-tar -czf t1.$SOURCE_LANG-$TARGET_LANG.tar.gz t1-$SOURCE_LANG-$TARGET_LANG
-echo "Removing temporary dir..."
-rm -rf t1-$SOURCE_LANG-$TARGET_LANG
