94d0028f785e5567d4a9a13d3e37494ca170dcdc
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-13 13:45:26 +0200

    Bootstrap for HTER experiment
    Former-commit-id: a96ca14d5fbc55ada0d024fb7e3c84c0170af381

--- loonybin-mt-toolpack/helpers/topbest-josh.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-prevId = ''
-for line in sys.stdin:
-    (id, e, scores, total) = line.split(' ||| ')
-    if id != prevId:
-        prevId = id
-        print e
