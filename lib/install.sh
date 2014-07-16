265c6b42f251785e482e386a088190d2c93bef1b
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-16 23:22:55 +0200

    Fixed sorting of responses in application.py
    Former-commit-id: d94f1857eb9461b867f2eee3dc326e4a59a2cb41

--- lib/install.sh
+++ lib/install.sh
@@ -3,3 +3,4 @@ wget http://www.dfki.de/~elav01/download/qualitative/lib/BerkeleyParser.jar
 wget http://www.dfki.de/~elav01/download/qualitative/lib/LanguageTool.jar
 wget http://www.dfki.de/~elav01/download/qualitative/lib/meteor-1.3.jar
 wget http://www.dfki.de/~elav01/download/qualitative/lib/py4j0.7.jar
+javac -cp py4j0.7.jar ../src/util/JavaServer.java -d .
