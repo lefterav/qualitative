44ed636a4976c0c9b236c34cd57ad0fac14963db
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-07-18 09:34:32 +0200

    Most probably fixed connection errors with JVM upon parsing
    Former-commit-id: 0bd264bbd3d9a1c74d8b9f30e126a33d2b9a03a5

--- src/util/jvm.py
+++ src/util/jvm.py
@@ -13,12 +13,11 @@ def get_libs():
     path = os.path.abspath(__file__)
     components = path.split(os.sep)
     rootpath = os.path.join(os.sep, *components[:-3])
-    libpath = os.path.join(rootpath, "lib")    
-    libs = [os.path.join(libpath, f) for f in os.listdir(libpath) if f.endswith('.jar') or f.endswith('.class')]
-    libs.append(libpath)
+    libpath = os.path.join(rootpath, "lib")
+    libpath_all = os.path.join(libpath, "*")    
+    #libs = [os.path.join(libpath, f) for f in os.listdir(libpath) if f.endswith('.jar') or f.endswith('.class')]
+    libs = [libpath, libpath_all]
     return libs
-    
-
 
 class JVM(object):
     '''
@@ -36,8 +35,7 @@ class JVM(object):
         #java_classpath.extend(get_libs())
         java_classpath = get_libs()
         path = os.path.abspath(__file__)
-        libdir = path
-        java_classpath.append(os.path.dirname(path))
+        #java_classpath.append(os.path.dirname(path))
         
         classpath = ":".join(java_classpath)
         print "classpath = ", classpath
@@ -49,15 +47,14 @@ class JVM(object):
         #except:
         #    pass
         
-        
         # prepare and run Java server
         #cmd = "java -cp %s:%s:%s JavaServer" % (berkeley_parser_jar, py4j_jar, dir_path)        
         cmd = ["java", "-cp", classpath, "JavaServer" ]
         print cmd
-#            cmd = " ".join(cmd)
+        cmd = " ".join(cmd)
         
-        self.jvm = subprocess.Popen(cmd, shell=False, bufsize=0, stdout=subprocess.PIPE) #shell=True,
-        time.sleep(10)
+        self.jvm = subprocess.Popen(cmd, shell=True, bufsize=0, stdout=subprocess.PIPE) #shell=True,
+        time.sleep(3)
         self.jvm.stdout.flush()
         self.socket_no = int(self.jvm.stdout.readline().strip())
         self.pid = self.jvm.pid
