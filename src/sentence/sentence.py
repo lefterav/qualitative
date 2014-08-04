a9529df9e7a9cb5a38152ed67e0f4d459519f4a5
Eleftherios Avramidis <eleftherios.avramidis@dfki.de>
2014-08-04 17:50:11 +0200

    Changes for nbest ranking app

--- src/sentence/sentence.py
+++ src/sentence/sentence.py
@@ -108,3 +108,13 @@ class SimpleSentence(object):
         
         self.attributes.update(incoming_attributes)
         self.string = ss.string
+    
+    def __get__(self, obj, cls=None): 
+        return self.get_attribute(obj)
+
+    def __set__(self, obj, val): 
+        self.attributes[obj] = val
+
+    def __delete__(self, obj): 
+        del(self.attributes[obj])
+        
