cc6c8165769b31f5b1b0e52f1618f34f5a26ac70
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-08-06 16:42:05 +0200

    Corrections on the way to the nbest reranker

--- src/app/autoranking/nbest_reranking.py
+++ src/app/autoranking/nbest_reranking.py
@@ -13,24 +13,33 @@ import re
 from itertools import izip
 from multiprocessing import Pool
 
-def get_nbest_translations(nbestfile):
-    pattern = re.compile("(\d*) \|\|\| (.*) \|\|\| ")
+import logging
+
+def get_nbest_translations(nbestfilename, n=10):
+    pattern = re.compile("(\d*) \|\|\| (.*) \|\|\| .* ")
     
-    previous_sentence_id = 0
+    previous_sentence_id = '0'
     translations = []
-    with open(nbestfilename) as nbestfile:
-        for line in nbestfile:
-            sentence_id, string =  re.findall(pattern, line)
-            if sentence_id != previous_sentence_id:
-                yield translations
-                translations = []
-            translations.append(SimpleSentence(string))
-    yield translations
+    nbestfile = open(nbestfilename)
+    for line in nbestfile:
+         sentence_id, string = re.findall(pattern, line)[0]
+         sys.stderr.write("sentence id {}\n".format(sentence_id))
+         if sentence_id != previous_sentence_id:
+             yield translations
+             previous_sentence_id = sentence_id
+             translations = []
+         if len(translations) < n:
+             attributes = {'system': 'n_{}'.format(sentence_id)}
+             translations.append(SimpleSentence(string, attributes))
+    nbestfile.close()
+    if translations:
+        yield translations
 
 def get_sources(sourcefilename):
-    with open(sourcefilename) as sourcefile:
-        for line in sourcefile:
-            yield SimpleSentence(line)
+    sourcefile = open(sourcefilename)
+    for line in sourcefile:
+        yield SimpleSentence(line)
+    sourcefile.close()
 
 
 class NbestProcessor:
@@ -40,13 +49,19 @@ class NbestProcessor:
     
     def process_files(self, sourcefilename, nbestfilename, outputfilename):
         outputfile = open(outputfilename, 'w')
-        for source, translations in izip(get_sources(sourcefilename), get_nbest_translations()):
+        for source, translations in izip(get_sources(sourcefilename), get_nbest_translations(nbestfilename)):
+            if len(translations)==1:
+                outputfile.write("{}\n".format(translations[0]))
+                continue
+
+            sys.stderr.write("translations: {}".format(translations))
             ranked_parallelsentence, metadata = self.ranker.get_ranked_sentence(source, translations)
             
             ordered_translations = sorted(ranked_parallelsentence.get_translations(), key=lambda translation: float(translation["rank"])) 
             best_translation = ordered_translations[0]
             
             outputfile.write("{}\n".format(best_translation.get_string()))
+        outputfile.close()
                 
         
 #Please decode with   -include-segmentation-in-n-best
@@ -57,9 +72,12 @@ if __name__ == '__main__':
     outputfilename = sys.argv[4]
     configfilenames = sys.argv[5:]
 
+   # FORMAT = "%(asctime)-15s %(clientip)s %(user)-8s %(message)s"
+   # logging.basicConfig(format=FORMAT, level=logging.DEBUG)  
+    
+   # logging.debug("Logging enabled")
     nbestprocessor = NbestProcessor(classifiername, configfilenames)
     nbestprocessor.process_files(sourcefilename, nbestfilename, outputfilename)
     
-        
     
     
