e5c23ee279959536540b66bc5962b57c1ffdf0a3
elav01 <eleftherios.avramidis@dfki.de>
2014-09-23 18:21:28 +0200

    Cleaned up code and epydoc for OrangeRanker and abstract Ranker. Still untested

--- src/ml/ranker.py
+++ src/ml/ranker.py
@@ -1,18 +1,22 @@
 '''
-Created on 26 Mar 2013
+Define abstract base classes that sketch the pairwise ranker behavior 
+to be implemented for wrapping ML toolkits
 
+Created on 26 Mar 2013
 @author: Eleftherios Avramidis
 '''
-from sentence.pairwisedataset import pairwise_ondisk
-import os
-import cPickle as pickle
-from dataprocessor.ce.utils import join_jcml 
+import cPickle as pickle 
 
-class PairwiseRanker():
+class PairwiseRanker:
     '''
-    classdocs
+    Abstract base class for a machine learning algorithms that learn and apply ranking
+    @ivar fit: whether the classifier has been fit/trained or not
+    @type fit: C{bool}
+    @ivar learner: the un-trained classifier class to be used for training
+    @type learner: toolkit-specific C{object}  
+    @ivar classifier: the trained classifier object
+    @type classifier: toolkit-specific C{object} 
     '''
-    
     def __init__(self, classifier=None, filename=None, learner=None):
         self.fit = True
         if classifier:
@@ -26,12 +30,13 @@ class PairwiseRanker():
             self.fit = False
     
     def train(self, dataset_filename, **kwargs):
-        pairwise_dataset_filename = dataset_filename.replace(".jcml", ".pair.jcml")
-        pairwise_ondisk(dataset_filename, pairwise_dataset_filename, **kwargs)
-        super(PairwiseRanker, self).train(pairwise_dataset_filename)
-        #self.classifier = PairwiseRanker(classifier=pairwise_classifier)
-      
+        raise NotImplementedError()
+    #------------------------------ def train(self, dataset_filename, **kwargs):
+        # pairwise_dataset_filename = dataset_filename.replace(".jcml", ".pair.jcml")
+        # pairwise_ondisk(dataset_filename, pairwise_dataset_filename, **kwargs)
+        #---------- super(PairwiseRanker, self).train(pairwise_dataset_filename)
+        #----- #self.classifier = PairwiseRanker(classifier=pairwise_classifier)
+#------------------------------------------------------------------------------ 
     
-    def rank(self, parallelsentence):
-        
-        pass
+    def get_ranked_sentence(self, parallelsentence):
+        raise NotImplementedError()
