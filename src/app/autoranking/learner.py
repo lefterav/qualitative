4be717527cd138927470d142e90d32efe3f14524
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2014-10-01 20:37:54 +0200

    Fixed issues locally on single run test set

--- src/app/autoranking/learner.py
+++ src/app/autoranking/learner.py
@@ -20,10 +20,12 @@ from sentence import scoring
 
 class RankingExperiment(PyExperimentSuite):
     
-    restore_supported = True
+    #restore_supported = True
     
     def reset(self, params, rep):
-        self.restore_supported = True
+        #self.restore_supported = True
+        logging.info("Running in {}".format(os.getcwd()))
+        logging.debug("params = {}".format(params))
         
         #=======================================================================
         # get method-specific parameters
@@ -43,37 +45,7 @@ class RankingExperiment(PyExperimentSuite):
         self.discrete_attributes = params["discrete_attributes"].split(",")
         self.attribute_set = self._read_attributeset(params)
         
-        #=======================================================================
-        # prepare training and test data
-        #=======================================================================
-        training_sets = params["training_sets"].format(**params).split(',')
-        training_path = params["training_path"].format(**params)
-        dataset_filename = "all.trainingset.jcml"
-    
-        self._join_or_link(training_path, training_sets, dataset_filename)
         
-        #if cross validation is enabled
-        if params["test"] == "crossvalidation":
-            self.trainingset_filename = "{}.trainingset.jcml".format(rep)
-            testset_filename = "{}.testset.jcml".format(rep)
-            self.testset_filenames = [testset_filename]
-            self.crossvalidation(dataset_filename, 
-                                 self.trainingset_filename, 
-                                 testset_filename, 
-                                 params, rep, 
-                                 shuffle=params.setdefault("cross_shuffle", False)
-                                 )
-            
-        #if a list of test-sets is given for testing upon
-        elif params["test"] == "list":
-            self.trainingset_filename = dataset_filename
-            testset_filenames = params["test_sets"].format(**params).split(',')
-            self.testset_filenames = [os.path.join(params["test_path"], f) for f in testset_filenames]
-        
-        #if no testing is required
-        elif params["test"] == "None":
-            self.trainingset_filename = dataset_filename
-            self.testset_filenames = []
             
         
     def _join_or_link(self, source_path, source_datasets, ready_dataset):
@@ -81,43 +53,16 @@ class RankingExperiment(PyExperimentSuite):
         Create a joined file from the given datasets if needed,
         or link them if they have already been given as one file
         """
-        source_datasets = [os.path.join(source_path, source_datasets)]
+        source_datasets = [os.path.join(source_path, f) for f in source_datasets]
         if len(source_datasets)==1:
-            os.link(source_datasets[0], ready_dataset)
+            logging.debug("Linking {} to {}".format(source_datasets[0], ready_dataset))
+            logging.debug(os.listdir(os.curdir))
+            os.symlink(source_datasets[0], ready_dataset)
+            
         else:
             logging.info("Joining training files")
             join_jcml(source_datasets, ready_dataset)
         
-          
-        
-    def crossvalidation(self, dataset_filename,
-                        trainingset_filename,
-                        testset_filename,
-                        params, rep, 
-                        shuffle=False):
-        """ This method takes a dataset in form of a numpy array of shape n x d,
-            where n is the number of data points and d is the dimensionality of 
-            the data. It further requires the current params dictionary and the
-            current repetition number. The flag 'shuffle' determines, if the
-            dataset should be shuffled before returning the training and testing
-            batches. There will be params['repetitions'] many equally sized batches, 
-            the rest of the dataset is discarded.
-        """
-
-        #We do not need shuffling and this one it's not easily applicable
-        #key = int(hashlib.sha1(dataset).hexdigest()[:7], 16)
-        #indices = range(dataset.shape[0])
-        #if shuffle:
-        #    # create permutation unique to dataset
-        #    random.seed(key)
-        #    indices = random.permutation(indices)
-        #       
-        fold_jcml(dataset_filename,
-                        trainingset_filename,
-                        testset_filename,
-                        params['repetitions'],
-                        rep)
-    
     
     def _read_attributeset(self, params):
         general_attributes = self._read_attributes(params, "general")
@@ -154,12 +99,12 @@ class RankingExperiment(PyExperimentSuite):
         
         logging.info("train: Attribute_set before training: {}".format(params["attribute_set"]))
         
-        output_filename = "trainingset.{}.tab".format(rep) 
-        ranker_filename = "ranker.{}.dump"
+        output_filename = "{}.trainingset.tab".format(rep) 
+        ranker_filename = "{}.ranker.dump".format(rep)
                                       
         logging.info("Launching ranker based on {}".format(params["learner"]))                                                
         ranker = OrangeRanker(learner=params["learner"])
-        ranker.train(dataset_filename = dataset_filename, 
+        ranker.train(dataset_filename = self.trainingset_filename, 
                      output_filename = output_filename,
                      **params)
         
@@ -175,27 +120,63 @@ class RankingExperiment(PyExperimentSuite):
         """
         Load test set and apply machine learning to assign labels
         """
-        testset_input = self.testsets[0]
-        testset_output = "testset_annotated.jcml"
-        ranker = OrangeRanker(filename="ranker.dump")
-        return ranker.test(testset_input, testset_output)
+        testset_input = self.testset_filenames[0]
+        self.testset_output = "{}.testset_annotated.jcml".format(rep)
+        ranker = OrangeRanker(filename="{}.ranker.dump".format(rep))
+        return ranker.test(testset_input, self.testset_output)
     
     
     def evaluate(self, params, rep):
         """
         Load predictions (test) and analyze performance
         """
-        testset_output = "testset_annotated.jcml"
-        testset = CEJcmlReader(testset_output, all_general=True, all_target=True)
+        
+        testset = CEJcmlReader(self.testset_output, all_general=True, all_target=True)
         
         class_name = params["class_name"]
         scores = scoring.get_metrics_scores(testset, "rank_hard", class_name , prefix="soft", invert_ranks=False)
         return scores
     
+    def prepare_data(self, params, rep):
+        #=======================================================================
+        # prepare training and test data
+        #=======================================================================
+        training_sets = params["training_sets"].format(**params).split(',')
+        training_path = params["training_path"].format(**params)
+        dataset_filename = "all.trainingset.jcml"
+    
+        self._join_or_link(training_path, training_sets, dataset_filename)
+        
+        #if cross validation is enabled
+        if params["test"] == "crossvalidation":
+            self.trainingset_filename = "{}.trainingset.jcml".format(rep)
+            testset_filename = "{}.testset.jcml".format(rep)
+            self.testset_filenames = [testset_filename]
+            fold_jcml(dataset_filename,
+                        self.trainingset_filename,
+                        testset_filename,
+                        params['repetitions'],
+                        rep)
+            
+        #if a list of test-sets is given for testing upon
+        elif params["test"] == "list":
+            logging.info("Preparing testing on a list of provided test sets")
+            self.trainingset_filename = dataset_filename
+            testset_filenames = params["test_sets"].format(**params).split(',')
+            self.testset_filenames = [os.path.join(params["test_path"], f) for f in testset_filenames]
+        
+        #if no testing is required
+        elif params["test"] == "None":
+            self.trainingset_filename = dataset_filename
+            self.testset_filenames = []
+            
     
     def iterate(self, params, rep, n):
         ret = OrderedDict()
+        logging.info("Running in {}".format(os.getcwd()))
         logging.info("Iteration {}".format(n))
+        if n==0:
+            self.prepare_data(params, rep)
         if n==1:
             ret.update(self.train(params, rep))
         if n==2:
@@ -233,3 +214,5 @@ if __name__ == '__main__':
     mysuite = RankingExperiment();
     
     mysuite.start()
+    #params = params =  {'params_svmeasylearner': "{'verbose':True}", 'test_path': '/home/dupo/taraxu_data/qualitative/', 'class_name': 'rank', 'iterations': 4, 'path': '/home/dupo/taraxu_data/qualitative/', 'langpair': 'de-en', 'discrete_attributes': 'src_reuse_status,src_terminologyAdmitted_status,src_total_status,src_spelling_status,src_style_status,src_grammar_status,src_terminology_status,src_resultStats_projectStatus,tgt-1_reuse_status,tgt-1_terminologyAdmitted_status,tgt-1_total_status,tgt-1_spelling_status,tgt-1_style_status,tgt-1_grammar_status,tgt-1_terminology_status,tgt-1_resultStats_projectStatus,tgt-2_reuse_status,tgt-2_terminologyAdmitted_status,tgt-2_total_status,tgt-2_spelling_status,tgt-2_style_status,tgt-2_grammar_status,tgt-2_terminology_status,tgt-2_resultStats_projectStatus', 'params_logreg': "{'stepwise_lr':True}", 'learner': 'LogRegLearner', 'experiment': 'grid', 'test': 'crossvalidation', 'test_sets': 'wmt2008-de-en-jcml-rank.all.analyzed.f.dev.jcml', 'attset_24_source': 'l_tokens', 'hidden_attributes': 'tgt-1_berkeley-tree,tgt-2_berkeley-tree,src_berkeley-tree,rank_diff,tgt-1_ref-lev,tgt-1_ref-meteor_score,tgt-1_ref-meteor_fragPenalty,tgt-1_ref-meteor_recall,tgt-1_ref-meteor_precision,tgt-1_ref-bleu,tgt-2_ref-lev,tgt-2_ref-meteor_score,tgt-2_ref-meteor_fragPenalty,tgt-2_ref-meteor_recall,tgt-2_ref-meteor_precision,tgt-2_ref-bleu,tgt-1_rank,tgt-2_rank', 'repetitions': 1, 'attset_24_target': 'cross-meteor_score,l_tokens,parse-VP', 'training_path': '/home/elav01/taraxu_data/qualitative/', 'tempdir': '/home/elav01/taraxu_data/qualitative/tmp', 'bidirectional_pairs': True, 'ties': False, 'training_sets': 'wmt2008-de-en-jcml-rank.all.analyzed.f.dev.jcml', 'remove_infinite': False, 'name': 'dev/learnerLogRegLearnerattattset_24', 'meta_attributes': 'testset,judgement_id,langsrc,langtgt,ps1_judgement_id,ps2_judgement_id,id,tgt-1_score,tgt-1_system,tgt-2_score,tgt-2_system,document_id,judge_id,segment_id', 'attset_24_general': None, 'att': 'attset_24'}
+    #mysuite.reset(params, 1)
