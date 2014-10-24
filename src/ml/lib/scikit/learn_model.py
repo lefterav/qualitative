edc59634a736c0fe49f30ebd6c530abd8836327c
lefterav <lefterav@sammy>
2014-10-24 02:08:59 +0200

    Improved installation instructions Tested on local pc and fixed issues with class vector

--- src/ml/lib/scikit/learn_model.py
+++ src/ml/lib/scikit/learn_model.py
@@ -40,7 +40,6 @@ import logging as log
 import numpy as np
 import os
 import sys
-import yaml
 from sklearn_utils import open_datasets_crossvalidation,\
     scale_datasets_crossvalidation
 
@@ -153,7 +152,6 @@ def set_optimization_params(opt):
             params[key] = np.linspace(item[0], item[1], num=item[2], endpoint=True)
             
         elif isinstance(item, list) and assert_string(item):
-            print key, item
             params[key] = item
     
     return params
@@ -167,7 +165,7 @@ def optimize_model(estimator, X_train, y_train, params, scores, folds, verbose, 
         log.debug(params)
         log.debug(scores)
         
-        clf = GridSearchCV(estimator, params, loss_func=score_func, 
+        clf = GridSearchCV(estimator, params, scoring='f1', 
                            cv=folds, verbose=verbose, n_jobs=n_jobs)
         
         clf.fit(X_train, y_train)
@@ -537,8 +535,8 @@ USAGE
             
         # opens the config file
         config = None
-        with open(cfg_path, "r") as cfg_file:
-            config = yaml.load(cfg_file.read())
+#        with open(cfg_path, "r") as cfg_file:
+#            config = yaml.load(cfg_file.read())
          
         run(config)
         
