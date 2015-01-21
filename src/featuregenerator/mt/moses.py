3ad33b9f27dfa82798264af359a4232f299754be
Eleftherios Avramidis <eleftherios.avramidis@dfki.de>
2015-01-21 01:25:51 +0100

    Moses worker not fully functional

--- src/featuregenerator/mt/moses.py
+++ src/featuregenerator/mt/moses.py
@@ -5,6 +5,7 @@ Created on 26 Nov 2014
 '''
 import xmlrpclib
 from engine import MtEngine
+from sentence.sentence import SimpleSentence
 
 class MtMonkeyWorker(MtEngine):
     def __init__(self, source_language, target_language, url):
@@ -13,9 +14,26 @@ class MtMonkeyWorker(MtEngine):
         self.server = xmlrpclib.ServerProxy(url)
         
     def translate_string(self, string):
-        response = self.server.translate({ 'text': string })
-        text = response['text']
-        features = response
+        response = self.server.process_task({ 'text': string,
+                                             'action': 'translate',
+                                             'sourceLang': self.source_language,
+                                             'targetLang': self.target_language,
+                                             'alignmentInfo': True,
+                                             'nBestSize': 10,
+                                             })
+        translated = response['translated']
+        source_tokenized = response['src-tokenized']
+        translations = ()
+        for translated_entry in translated:
+            translated_text = translated_entry['text']
+            translated_score = translated_entry['score']
+            translated_tokenized = translated_entry['tokenized']
+            translated_alignment = translated_entry['alignment-raw']
+            #process alignment-raw
+            #find a way to continue working with both tokenized and untokenized text
+            translation = SimpleSentence(translated_text)
+            translations.append(translation)
+            
         
 
                
