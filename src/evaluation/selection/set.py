36c35e789916db91a11b3789fcf35a570c5a4099
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2015-02-20 20:01:09 +0100

    Changes on server side for running evaluation metrics on system selection
    Former-commit-id: e68565e9ae8c407db9910d91c64484cd219ba20a

--- src/evaluation/selection/set.py
+++ src/evaluation/selection/set.py
@@ -12,6 +12,9 @@ from featuregenerator.reference.rgbf import RgbfGenerator
 from featuregenerator.reference.wer.werfeaturegenerator import WERFeatureGenerator
 from featuregenerator.reference.hjerson import Hjerson
 import logging as log
+from util.jvm import JVM
+from py4j.java_gateway import GatewayClient, JavaGateway
+
 
 def evaluate_selection(parallelsentences, 
                         metrics=[], 
@@ -19,33 +22,44 @@ def evaluate_selection(parallelsentences,
                         rank_name="rank_hard",
                         default_system=None,
                         out_filename=None,
-                        ref_filename=None):
+                        ref_filename=None,
+                        language=None):
     
     selected_sentences = []
     results = OrderedDict()
     selected_systems = defaultdict(int) #collect the winnings of each system
     
+    gateway = JavaGateway(GatewayClient('localhost', JVM(None).socket_no), auto_convert=True, auto_field=True)
+ 
+    
     
     #iterate over all parallelsentences, get the selected ones in a list along with references
-    for parallelsentence in parallelsentences:
+    for j, parallelsentence in enumerate(parallelsentences):
 
-        ranking = [tgt.get_attribute(rank_name) for tgt in parallelsentence.get_translations()]
-        log.debug("ranking: {}".format(ranking))
+        ranking = [int(tgt.get_attribute(rank_name)) for tgt in parallelsentence.get_translations()]
         #get the best sentence according to ranking
         best_rank = function(ranking)
-        log.debug("Best rank: {}".format(best_rank))
 
         reference = parallelsentence.get_reference()
+        if not reference:
+            log.warning("Sentence {} did not have a reference".format(parallelsentence.get_attribute("judgement_id")))
+            continue
         #for statistic reasons we collect statistics for all sentences that have the best rank
-        for counter, translation in enumerate(parallelsentence.get_translations()):
-            log.debug("Sentence rank: {} {}".format(translation.get_attribute("system"), translation.get_attribute(rank_name)))
+        counter = 0
+        for translation in parallelsentence.get_translations():
+
 
             if int(translation.get_attribute(rank_name)) == int(best_rank):
                 system_name = translation.get_attribute("system")
                 selected_systems[system_name] += 1
+                #if there is a tie, collect the first sentence that appears TODO:improve
                 if counter == 0:
                     selected_sentences.append((translation.get_string(), [reference.get_string()]))
+                    counter+=1
+                
     
+    if j==0:
+        raise ValueError("File reader contains no sentences")
     for system_name, counts in selected_systems.iteritems():
         results["sel-counts_{}".format(system_name)] = counts
         results["sel-perc_{}".format(system_name)] = 1.00*counts/len(selected_sentences)
@@ -57,15 +71,19 @@ def evaluate_selection(parallelsentences,
                  RgbfGenerator(),
                  WERFeatureGenerator(),
                  Hjerson()]
+        if language and gateway:
+            metrics.append(MeteorGenerator(language, gateway))
     
     for metric in metrics:
-        results.update(metric.analytic_score_sentences(selected_sentences))
+        metric_results = metric.analytic_score_sentences(selected_sentences)
+        results.update(metric_results)
         
     with open(out_filename, 'w') as f:
         f.write(os.linesep.join([t for t,_ in selected_sentences]))
-    
-    with open(ref_filename, 'w') as f:
-        f.write(os.linesep.join([r for _,r in selected_sentences]))
+
+    if ref_filename:
+        with open(ref_filename, 'w') as f:
+            f.write(os.linesep.join([r[0] for _,r in selected_sentences]))
 
     return results
 
