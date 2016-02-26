Implemented weighed and pooled variance for kendall tau on ranking sets Implemented some basic tests for Kendall tau on ranking sets Fixed issues with Kendall tau's inability to handle ties

src/evaluation/ranking/set.py
@@ -58,20 +58,42 @@ def kendall_tau_set(predicted_rank_vectors, original_rank_vectors, **kwargs):
     #count how many ranking lists exist for each length (e.g 100 lists with length 5 etc.)
     ranking_counts_per_length = defaultdict(int)   
     sum_tau_per_length = defaultdict(float)
+    #sums for the fractions of the 
+    sum_inverted_single_max_variance = 0
+    sum_weighed_variance = 0
+    sum_ranking_lengths = 0
+    skipped = 0
     
     for predicted_rank_vector, original_rank_vector in zip(predicted_rank_vectors, original_rank_vectors):
         
         segtau, segprob, concordant_count, discordant_count, all_pairs_count, original_ties, predicted_ties, pairs = segment.kendall_tau(predicted_rank_vector, original_rank_vector, **kwargs)
         ranking_length = len(predicted_rank_vector)
         
-        if segtau and segprob:                    
+        if segtau == None:
+            logging.debug("Segment tau was found to be {}".format(segtau))
+            # and segprob):
+            skipped += 1
+        else:                    
             segtaus.append(segtau)
             segprobs.append(segprob)
             
-            #gather statistics per length
+            #gather statistics per length to calculate significance for null hypothesis
             ranking_counts_per_length[ranking_length] += 1
             sum_tau_per_length[ranking_length] += segtau
             
+            #maximum variance
+            max_var = (1.00 - segtau*segtau) / (ranking_length / 2.00)
+            #sum of item variance weighed by ranking length, used for the nominator of the pooled variance
+            sum_weighed_variance = (ranking_length - 1) * max_var
+            #sum of inverted maximum variance used for the denominator weighed standard error (4.9)
+            try:
+                sum_inverted_single_max_variance += 1.00 / max_var
+            except ZeroDivisionError:
+                sum_inverted_single_max_variance += 1.00 / 0.0000000001
+            #sum of ranking length
+            sum_ranking_lengths += ranking_length - 1
+            
+        #sum overal pair identities            
         concordant += concordant_count
         discordant += discordant_count
         valid_pairs += all_pairs_count
@@ -82,19 +104,30 @@ def kendall_tau_set(predicted_rank_vectors, original_rank_vectors, **kwargs):
             sentences_with_ties += 1
         pairs_overall += pairs
     
+    if skipped:
+        logging.warn("{} items were skipped from tau calculation".format(skipped))
+    
     #this is probably when the test has only has ties
-    if concordant+discordant==0:
+    if valid_pairs == 0:
         logging.warn("I had to skip item in tau evaluation because of zero pairs")
         return {}
-    tau = 1.00 * (concordant - discordant) / (concordant + discordant)
+    tau = 1.00 * (concordant - discordant) / valid_pairs
     prob = segment.kendall_tau_prob(tau, valid_pairs)
     
     #average tau statistics
     avg_seg_tau = np.average(segtaus)               
     avg_seg_prob = np.product(segprobs)
     
+    #weighed tau and significance for null hypothesis
     weighed_tau, weighed_prob, variance_per_length = _inverse_weighted_tau(ranking_counts_per_length, sum_tau_per_length)
     
+    #maximum standard deviation from the pooled variance
+    pooled_std = np.sqrt(sum_weighed_variance / sum_ranking_lengths)
+    
+    #maximum standard deviation from the mean based on inverse weighed variance
+    inverse_weighed_std = np.sqrt(1.00 / sum_inverted_single_max_variance) 
+    
+    #averages for the ties
     predicted_ties_avg = 100.00*predicted_ties / pairs_overall
     sentence_ties_avg = 100.00*sentences_with_ties / len(predicted_rank_vector)
     
@@ -104,6 +137,8 @@ def kendall_tau_set(predicted_rank_vectors, original_rank_vectors, **kwargs):
              'tau_avg_seg_prob': avg_seg_prob,
              'tau_weighed': weighed_tau,
              'tau_weighed_prob': weighed_prob,
+             'tau_invw_std': inverse_weighed_std,
+             'tau_pooled_std': pooled_std,
              'tau_concordant': concordant,
              'tau_discordant': discordant,
              'tau_valid_pairs': valid_pairs,
@@ -120,6 +155,7 @@ def kendall_tau_set(predicted_rank_vectors, original_rank_vectors, **kwargs):
         stats["tau_weighed_var_{}".format(n)] = variance_per_length[n]
 
     return stats
+    
 
 def _inverse_weighted_tau(ranking_counts_per_length, sum_tau_per_length):
     """
@@ -132,6 +168,7 @@ def _inverse_weighted_tau(ranking_counts_per_length, sum_tau_per_length):
     inv_variance_sum = 0
     variance_per_length = {}
     #iterate for all lengths, i.e. ranking with three, four, etc
+    logging.debug("Sum tau per length: {}".format(sum_tau_per_length))
     for length in sum_tau_per_length.keys():
         if length < 2:
             #definition only holds for m >= 2