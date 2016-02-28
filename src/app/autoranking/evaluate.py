c231730f5f6740933eff11de96330104b48f33c1
Eleftherios Avramidis <elav01@dfki.de>
2016-02-28 04:54:35 +0100

    Added draft evaluation script to reprocess existing annotations
    Former-commit-id: 2460da99cc713c9ebee8748e9bf251c0ebd8a7fe

---
+++ src/app/autoranking/evaluate.py
@@ -0,0 +1,22 @@
+'''
+Created on 26 Feb 2016
+
+@author: elav01
+'''
+
+from dataprocessor.ce.cejcml import CEJcmlReader
+from sentence import scoring
+import sys
+
+def evaluate(filename, metric="rank_hard", prefix="hard", class_name = "rank"):
+        """
+        Load predictions (test) and analyze performance
+        """        
+        testset = CEJcmlReader(filename, all_general=True, all_target=True)        
+        scores = scoring.get_metrics_scores(testset, metric, class_name , prefix=prefix, invert_ranks=False)
+        return scores
+
+if __name__ == '__main__':
+    scores = evaluate(sys.argv[1], metric=sys.argv[2])
+    
+    print "\n".join(["{}\t{}".format(k, v) for k, v in scores.iteritems()])
