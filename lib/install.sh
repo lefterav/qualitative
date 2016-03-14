662dda7ae53847c7c4ee1c26d2979cd3da25031b
Eleftherios Avramidis <elav01@dfki.de>
2016-03-14 15:08:14 +0100

    Improvements and tests for JVM and Berkeley Socket

--- lib/install.sh
+++ lib/install.sh
@@ -6,5 +6,5 @@ wget http://www.dfki.de/~elav01/download/qualitative/lib/meteor-1.3.jar
 wget http://www.dfki.de/~elav01/download/qualitative/lib/py4j0.7.jar
 echo "Done. Compiling necessary java code of ours..."
 javac -cp py4j0.7.jar ../src/util/JavaServer.java -d .
-javac -cp ./BerkeleyParser.jar ../src/featuregenerator/parser/berkeley/socket/BParser.java -d .
+javac -cp ./BerkeleyParser.jar ../src/featuregenerator/blackbox/parser/berkeley/socketservice/BParser.java -d .
 echo "Done. Now is time to download the necessary resources (probably by running the script into the directory /res) ."
---
+++ src/featuregenerator/blackbox/parser/berkeley/socketservice/berkeleyparsersocket_test.py
@@ -0,0 +1,34 @@
+'''
+Created on 14 Mar 2016
+
+@author: elav01
+'''
+import unittest
+from util.jvm import LocalJavaGateway
+import os
+from berkeleyparsersocket import BerkeleyParserSocket
+from numpy.ma.testutils import assert_equal
+
+GRAMMAR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 
+                          '..', '..', '..', '..', '..', '..', 
+                          'res', 'grammars', 'eng_sm6.gr'))
+
+class TestBerkeleyParserSocket(unittest.TestCase):
+
+    def setUp(self):
+        self.gateway = LocalJavaGateway()
+        self.parser = BerkeleyParserSocket(GRAMMAR, self.gateway)
+
+    def tearDown(self):
+        self.gateway.shutdown()
+
+    def testParse(self):
+        sentence = self.parser.parse("This is a test")
+        assert(len(sentence["nbest"]) > 0, "Parser should return at list one result")
+        print sentence
+        assert_equal(sentence["nbest"][0]["tree"], '(S (NP (DT This)) (VP (VBZ is) (NP (DT a) (NN test)))) )')
+        assert_equal(sentence["nbest"][0]["confidence"], u'-26.320426032971984')
+        
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
