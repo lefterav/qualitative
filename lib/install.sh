4541b9c0872eba3da28d4cb2c6b3de215b32724d
Eleftherios Avramidis <elav01@dfki.de>
2016-03-16 17:00:10 +0100

    Test for Languagetool sentence check Installation script and instructions for Java8

--- lib/install.sh
+++ lib/install.sh
@@ -1,10 +1,13 @@
+JAVA_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/
 echo "Downloading required java libraries. Please be patient..."
 wget http://www.dfki.de/~elav01/download/qualitative/lib/BerkeleyParser.jar
 #wget https://berkeleyparser.googlecode.com/files/BerkeleyParser-1.7.jar
-wget http://www.dfki.de/~elav01/download/qualitative/lib/LanguageTool.jar
+wget https://languagetool.org/download/LanguageTool-3.2.zip
+unzip LanguageTool-3.2.zip
+rm LanguageTool-3.2.zip
 wget http://www.dfki.de/~elav01/download/qualitative/lib/meteor-1.3.jar
-wget http://www.dfki.de/~elav01/download/qualitative/lib/py4j0.7.jar
+wget http://central.maven.org/maven2/net/sf/py4j/py4j/0.9.2/py4j-0.9.2.jar
 echo "Done. Compiling necessary java code of ours..."
-javac -cp py4j0.7.jar ../src/util/JavaServer.java -d .
-javac -cp ./BerkeleyParser.jar ../src/featuregenerator/blackbox/parser/berkeley/socketservice/BParser.java -d .
+$JAVA_PATH/javac -cp py4j-0.9.2.jar ../src/util/JavaServer.java -d .
+$JAVA_PATH/javac -cp ./BerkeleyParser.jar ../src/featuregenerator/blackbox/parser/berkeley/socketservice/BParser.java -d .
 echo "Done. Now is time to download the necessary resources (probably by running the script into the directory /res) ."
