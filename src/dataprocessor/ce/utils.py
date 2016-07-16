5db9db51e7883063ceb53620e3e34d903c8c4f1f
lefterav <lefterav@pindi.me>
2016-07-16 23:17:48 +0200

    Removed sorting while processing pairwise sentence combinations and filtering. Wrote some testing
    Former-commit-id: 25d85626a17989247e599f11fc29e4a9c914768d

--- src/dataprocessor/ce/utils.py
+++ src/dataprocessor/ce/utils.py
@@ -11,6 +11,7 @@ from sentence.dataset import DataSet
 import os
 from time import sleep
 from random import randint
+from copy import deepcopy
 
 
 def get_length_jcml(filename):
@@ -164,8 +165,17 @@ def get_clean_testset(input_file, output_file):
     reconstructed_dataset = filtered_dataset.get_multiclass_set()
     reconstructed_dataset.remove_ties()
     IncrementalJcml(output_file).add_parallelsentences(reconstructed_dataset.get_parallelsentences())
+
+def fold_jcml_respect_ids(filename, training_filename, test_filename, 
+                     repetitions, fold, length=None, clean_testset=True):
     
-def fold_jcml_respect_ids(filename, training_filename, test_filename, repetitions, fold, length=None, clean_testset=True):
+                     
+    fold_respect_ids(CEJcmlReader(filename, all_general=True, all_target=True), 
+                     IncrementalJcml(training_filename), IncrementalJcml(test_filename), 
+                     repetitions, fold, length, clean_testset)
+    
+
+def fold_respect_ids(reader, training_writer, test_writer, repetitions, fold, length=None, clean_testset=True):
     """
     An incremental function for cross validation. The function is designed to be executed once for every fold. 
     The current fold is passed as a parameter, so the given xml file is read in a memory efficient way (entry by entry)
@@ -175,22 +185,14 @@ def fold_jcml_respect_ids(filename, training_filename, test_filename, repetition
         raise SystemExit("{}-fold cross validation does not make sense. Use at least 2 repetitions.".format(repetitions))
     
     if not length:
-        #quickest way of getting the length of the file, without parsing its xml
-        length = int(subprocess.check_output(["grep", "-c", "<judgedsentence", filename]).strip())
-        logging.info("Dataset has {} entries".format(length))
+        length = len(reader)
         
-    
     #get how big each batch should be
     batch_size = length // repetitions
     
     if batch_size == 0:
-            raise SystemExit('Too many repetitions for cross-validation with this dataset. Max. number of repetitions is {}.'.format(length))
-    
-    #create one reader and two writers (for training and test set respectively)    
-    reader = CEJcmlReader(filename, all_general=True, all_target=True)
-    training_writer = IncrementalJcml(training_filename)
-    test_writer = IncrementalJcml(test_filename)
-    
+        raise SystemExit('Too many repetitions for cross-validation with this dataset. Max. number of repetitions is {}.'.format(length))
+        
     #define where is the beginning and the end of the test set
     test_start = length - (batch_size * (fold+1))
     test_end = length - (batch_size * fold)
@@ -199,7 +201,8 @@ def fold_jcml_respect_ids(filename, training_filename, test_filename, repetition
     if fold == repetitions-1:
         test_start = 0 
     
-    logging.info("Test set for fold {} will be between HITs {} and {} (size: {})".format(fold, test_start, test_end-1, test_end-test_start))
+    logging.info("Test set for fold {} will be between HITs {} and {} (size: {})".format(
+        fold, test_start, test_end-1, test_end-test_start))
     
     counter = 0
     
@@ -211,56 +214,71 @@ def fold_jcml_respect_ids(filename, training_filename, test_filename, repetition
     test_size = 0
     merged_test_size = 0 
     train_size = 0
+    
+    logging.debug("Data length before loop of fold {}: {}".format(fold, len(reader)))
 
     for parallelsentence in reader.get_parallelsentences():
         sentence_id = parallelsentence.get_fileid_tuple()
-        
-        (parallelsentence.attributes.setdefault("testset", None), parallelsentence.get_id())
+        logging.debug("Data length before loop of sentence_id : {}, {}".format(sentence_id, len(reader)))
 
         # collect judgments of the same sentence until a new sentence appears
         # (we suppose that the original corpus has been ordered by sentence id)
         
         # if a new sentence appears, flush
+        logging.debug("{}, {}".format(counter, sentence_id))
         if previous_sentence_id != None and sentence_id != previous_sentence_id:
-            train_count, test_count, merged_test_count = _flush_per_id(parallelsentences_per_id, training_writer, test_writer, 
-                                                    counter, test_start, test_end, clean_testset)
+            logging.debug("Data length before flushing of sentence_id : {}, {}".format(sentence_id, len(reader)))
+
+            train_count, test_count, merged_test_count = _flush_per_id(parallelsentences_per_id, 
+                                                         training_writer, test_writer, counter, 
+                                                         test_start, test_end, clean_testset)
+            logging.debug("Data length after flushing of sentence_id : {}, {}".format(sentence_id, len(reader)))
+
             totalsentences += len(parallelsentences_per_id)
             train_size += train_count
             test_size += test_count
             merged_test_size += merged_test_count
             parallelsentences_per_id = []
         
-        parallelsentences_per_id.append(parallelsentence)
+        parallelsentences_per_id.append(deepcopy(parallelsentence))
         previous_sentence_id = sentence_id
         counter+=1
         
-    train_count, test_count, merged_test_count = _flush_per_id(parallelsentences_per_id, training_writer, test_writer, 
-                                            counter, test_start, test_end, clean_testset)
+        
+    train_count, test_count, merged_test_count = _flush_per_id(parallelsentences_per_id, 
+                                              training_writer, test_writer, counter, 
+                                              test_start, test_end, clean_testset)
     totalsentences += len(parallelsentences_per_id)
     train_size += train_count
     test_size += test_count
     merged_test_size += merged_test_count
     
     if test_size != batch_size:
-        logging.info("Fold {} will have an actual number of {} test sentences  instead of {}".format(fold, test_size, batch_size))
+        logging.info("Fold {} will have an actual number of {} test sentences  instead of {}".format(
+            fold, test_size, batch_size))
     if merged_test_count != test_size:
         logging.info("Fold {} aggregated {} test sentences to {}".format(fold, test_size, merged_test_count))
     training_writer.close()
     test_writer.close()
     
         
-def _flush_per_id(parallelsentences_per_id, training_writer, test_writer, counter, test_start, test_end, clean_testset):
-    if counter < test_start or counter >= test_end:
+def _flush_per_id(parallelsentences_per_id, training_writer, test_writer, counter, test_start, 
+                  test_end, clean_testset):
+    if counter <= test_start or counter > test_end:
+        logging.debug("Flushing in training set")
         training_writer.add_parallelsentences(parallelsentences_per_id)
         return len(parallelsentences_per_id), 0, 0
     else:
         if not clean_testset:
+            logging.debug("Flushing in normal testset")
             test_writer.add_parallelsentences(parallelsentences_per_id)
             count = len(parallelsentences_per_id)
             return 0, count, count
         else:
-            dataset = DataSet(parallelsentences_per_id)
-            analytic_dataset = AnalyticPairwiseDataset(dataset) 
+            logging.debug("Flushing in clean testset")
+            tmpdataset = DataSet(parallelsentences_per_id)
+            analytic_dataset = AnalyticPairwiseDataset(tmpdataset)
+            tmpdataset = None 
             filtered_dataset = FilteredPairwiseDataset(analytic_dataset, 1.00)
             filtered_dataset.remove_ties()
             reconstructed_dataset = filtered_dataset.get_multiclass_set()
