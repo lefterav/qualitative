a8230a28343e7b8cdff86337ee65e4cf09f27936
Eleftherios Avramidis <elav01@dfki.de>
2016-07-27 17:17:05 +0200

    First steps for the re-development of the selector

--- src/featuregenerator/__init__.py
+++ src/featuregenerator/__init__.py
@@ -4,3 +4,11 @@ generation of features over the parallel objects. Any new featuregenerator
 should implement languagefeaturegenerator.py (if it is language-specific)
 or featuregenerator.py  (it is language-generic).
 """
+
+
+def get_feature_generators(attribute_set):
+    """
+    Return the feature generators that are required to provide the features 
+    in the given attribute set 
+    """
+    
