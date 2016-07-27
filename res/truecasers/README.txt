ec694c67316710a1c9a5bb194e5d9ce1abaa8c3c
lefterav <lefterav@pindi.me>
2016-07-27 19:10:42 +0200

    Uncommited tree changes

--- res/truecasers/README.txt
+++
@@ -1,2 +0,0 @@
-You can put the truecaser models here.
-Location needs anyway to be specified in annotation.cfg
--- src/featuregenerator/__init__.py
+++ src/featuregenerator/__init__.py
@@ -3,4 +3,20 @@ Interface for feature generators, i.e. classes which handle the
 generation of features over the parallel objects. Any new featuregenerator
 should implement languagefeaturegenerator.py (if it is language-specific)
 or featuregenerator.py  (it is language-generic).
+<<<<<<< HEAD
 """
+=======
+<<<<<<< HEAD
+"""
+=======
+"""
+
+
+def get_feature_generators(attribute_set):
+    """
+    Return the feature generators that are required to provide the features 
+    in the given attribute set 
+    """
+    
+>>>>>>> 1e0079519c7081adadf5cc9536496604773fcc03
+>>>>>>> master
