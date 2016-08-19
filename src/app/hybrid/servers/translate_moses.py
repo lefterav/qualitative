5daca00cc83ed3bfe3606658785bfef41560147b
lefterav <lefterav@pindi.me>
2016-08-19 21:15:35 +0300

    NeuralMonkey worker tested and fixed

--- src/app/hybrid/servers/translate_moses.py
+++ src/app/hybrid/servers/translate_moses.py
@@ -4,15 +4,16 @@ Created on Aug 18, 2016
 @author: lefterav
 '''
 from mt.moses import ProcessedWorker
+from mt.neuralmonkey import NeuralMonkeyWorker
 
-uri="http://lns-87247.dfki.uni-sb.de:9302"
+uri="http://blade-1.dfki.uni-sb.de:9502"
 source_language="de"
 target_language="en"
 truecaser_model="/home/lefterav/workspace/qualitative/res/truecasers/truecase-model.3.de"
 splitter_model="/home/lefterav/workspace/qualitative/res/splitter/split-model.de"
 
 if __name__ == '__main__':
-    translator = ProcessedWorker(uri,
+    translator = NeuralMonkeyWorker(uri,
                                       source_language,
                                       target_language,
                                       truecaser_model,
