cfb797b25ccd8218ba2eb9dc2f6ce2f6573d9338
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2016-09-06 17:40:36 +0200

    Fixed the code of translate selection to process whole files. Now still have to solve multiple sentences in a line and URL tokenization.

--- src/app/hybrid/translate_selection.py
+++ src/app/hybrid/translate_selection.py
@@ -1,9 +1,10 @@
 '''
+Process a text file with 3 translation engines and the selection mechanism
+
 Created on Aug 19, 2016
 
 @author: lefterav
 '''
-
 
 from mt.moses import ProcessedWorker
 from mt.neuralmonkey import NeuralMonkeyWorker
@@ -13,52 +14,73 @@ import logging as log
 import sys
 from dataprocessor.sax.saxps2jcml import IncrementalJcml
 
-#uri="http://blade-1.dfki.uni-sb.de:9502"
-source_language="de"
-target_language="en"
-truecaser_model="/home/lefterav/workspace/qualitative/res/truecasers/truecase-model.3.de"
-splitter_model="/home/lefterav/workspace/qualitative/res/splitter/split-model.de"
+def parse_args():
 
+    parser = argparse.ArgumentParser(description="Run translate engines and selection mechanism on a file")
+    parser.add_argument('--engines', nargs='*', action='append', default=['Lucy','Moses','NeuralMonkey'],
+                        help="A list of the engines to be used for translating, in the prefered order")
+    parser.add_argument('--source_language', default='en', help="The language code of the source language")
+    parser.add_argument('--target_language', default='de', help="The language code of the target language")
+    parser.add_argument('--config', nargs='*', 
+                        help="A list of configuration files for the engines and the feature generators")
+    parser.add_argument('--ranking_model',
+                        help="The path of the file containing a ranker, as a result of the training process")
+    parser.add_argument('--input', help="The location of a text file to be translated")
+    parser.add_argument('--text_output', help="The location of the text file where translations will be written")
+    parser.add_argument('--description_output', default=None,
+                        help="The location of the text file where the description of the selection process will be written")
+    parser.add_argument('--parallelsentence_output', default=None,
+                        help="The location of the JCML file where the full annotated and ranked parallel sentences will be written")
+    parser.add_argument('--debug', type=bool, default=False, help="Run in full verbose mode")
 
-parser = argparse.ArgumentParser(description="Run translate engine on a file")
-parser.add_argument('--engines', nargs='*', action='append', default=['Lucy','Moses','NeuralMonkey'])
-parser.add_argument('--source_language', default='en')
-parser.add_argument('--target_language', default='de')
-parser.add_argument('--config', nargs='*', default=["/home/lefterav/workspace/qualitative/config/autoranking/features.dev.cfg",
-                                              "/home/lefterav/workspace/qualitative/config/autoranking/mt.cfg"
-                                              ])
-parser.add_argument('--ranking_model', default="/home/lefterav/workspace/qualitative/res/qe/0.model.dump")
-parser.add_argument('--text_output')
-parser.add_argument('--description_output')
-parser.add_argument('--parallelsentence_output')
-parser.add_argument('--debug', type=bool, default=False)
-args = parser.parse_args()
+    args = parser.parse_args()
+    log.debug("Read config filenames from commandline {}".format(args.config))
+    return args
 
-#source = "as opposed to the US , UK , and Canadian central banks , the European Central Bank ( ECB ) did not cut interest rates , arguing that a rate drop combined with rising raw material prices and declining unemployment would trigger an inflationary spiral ."
-source = "Drag and drop the application icon from the menu to the desktop."
-
-if __name__ == '__main__':
+def set_loglevel(args):
     loglevel = log.INFO
-    if "--debug" in sys.argv:
+    if args.debug:
         loglevel = log.DEBUG
     log.basicConfig(level=loglevel,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M')  
-    log.debug("Read config filenames from commandline {}".format(args.config))
+
+def translate_file(args):
     translator = Pilot3Translator(args.engines, args.config, 
                                   args.source_language, 
                                   args.target_language, 
                                   args.ranking_model)
-    best_translation_string, parallelsentence, description = translator.translate_with_selection(source)
     
-    with open(args.text_output, 'w') as f:
-        f.write(best_translation_string)
+    text_input = open(args.input)
+    text_output = open(args.text_output, 'w')
+
+    if args.description_output:
+        description_output = open(args.description_output, 'w')
+
+    if args.parallelsentence_output:
+        parallelsentence_output = IncrementalJcml(args.parallelsentence_output)
+
+    counter = 0
+    for source in text_input:
+        counter+=1
+        log.info("Translating sentence {}".format(counter))
+        best_translation_string, parallelsentence, description = translator.translate_with_selection(source)
+        text_output.write(best_translation_string)
+        if args.parallelsentence_output:
+            parallelsentence_output.add_parallelsentence(parallelsentence)
+        if args.description_output:
+            description_output.write(str(description))
+
+    text_output.close()
+    if args.parallelsentence_output:
+        parallelsentence_output.close()
+    if args.description_output:
+        description_output.close()
     
-    parallelsentence_output = IncrementalJcml(args.parallelsentence_output)
-    parallelsentence_output.add_parallelsentence(parallelsentence)
-    parallelsentence_output.close()
-    
-    with open(args.description_output, 'w') as f:
-        f.write(str(description))
-    
+if __name__ == '__main__':
+    args = parse_args()
+    set_loglevel(args)
+    translate_file(args)
+
+   
     
