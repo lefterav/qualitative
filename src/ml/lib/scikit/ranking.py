bc4ad88bf0dd1cd1c0b935945525cbdcd3bf3c64
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2016-09-22 18:43:25 +0200

    Changed again the setting for NeuralMonkey, modified the ranking script to have some backwards compatibility with old models
    Former-commit-id: 43d005e95506dcf61176b4bd9070f73ef7ac2764

--- src/ml/lib/scikit/ranking.py
+++ src/ml/lib/scikit/ranking.py
@@ -36,12 +36,8 @@ from sklearn.ensemble.forest import ExtraTreesClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-<<<<<<< HEAD
 from sklearn.lda import LDA
 #from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-=======
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
->>>>>>> 5e87434df556e592aaf57b087fb45207963f939e
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomTreesEmbedding
