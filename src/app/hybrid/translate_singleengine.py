b6e33605318d0b18345b54010eb4d49199f41670
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2016-09-26 20:01:21 +0200

    Fixed and tested single-engine translation script

--- src/app/hybrid/translate_singleengine.py
+++ src/app/hybrid/translate_singleengine.py
@@ -7,7 +7,6 @@ Created on Sep 26, 2016
 from mt.moses import MosesWorker, ProcessedMosesWorker
 from mt.neuralmonkey import NeuralMonkeyWorker
 from mt.lucy import LucyWorker, AdvancedLucyWorker
-from mt.hybrid import Pilot3Translator
 import argparse
 import logging
 import logging as log
@@ -15,15 +14,13 @@ import sys
 import os
 import codecs
 from dataprocessor.sax.saxps2jcml import IncrementalJcml
-from mt.selection import Autoranking
 from sentence.sentence import SimpleSentence
 from sentence.parallelsentence import ParallelSentence
-from app.hybrid.servers.xmlrpcserver_worker import worker_class
 from ConfigParser import SafeConfigParser
 
 def parse_args():
 
-    parser = argparse.ArgumentParser(description="Run translate engines and selection mechanism on a file")
+    parser = argparse.ArgumentParser(description="Run translate a single translation engine on a file")
     parser.add_argument('--engine', default='ProcessedMoses',
                         help= "the name of the engine for translating")
     parser.add_argument('--source_language', default='en', help="The language code of the source language")
@@ -47,16 +44,48 @@ def set_loglevel(debug=False):
 
     log.debug("Setting verbose output ON")
 
+def load_worker(config, args):
+    worker_kwargs = {'source_language': args.source_language, 
+                     'target_language': args.target_language}
 
-def translate_and_select_sourcefile(args):
-    log.info("Engines: {}".format(args.engines)) 
+    if args.engine=="ProcessedMoses":
+        engine_name = "Moses"
+        truecaser_section = "Truecaser:{}".format(args.source_language)
+        worker_kwargs['truecaser_model'] = config.get(truecaser_section, 'model')
+    elif args.engine=="AdvancedLucy":
+        engine_name = "Lucy"
+        moses_section = "Moses:{}-{}".format(args.source_language, args.target_language)
+        worker_kwargs['moses'] = config.get(moses_section, 'uri')
+    else:
+        engine_name = args.engine
+
+    if engine_name.endswith("Lucy"):
+        worker_kwargs.update(config.items(engine_name))
+    else:
+        worker_section = "{}:{}-{}".format(engine_name, args.source_language, args.target_language)
+        log.info("Loading {}".format(worker_section))
+        worker_kwargs.update(config.items(worker_section))
+    
+    worker_class = eval("{}Worker".format(args.engine))
+    worker = worker_class(**worker_kwargs)
+    return worker
+
+def translate_file(worker, source_file, target_file):
+    source = open(source_file)
+    target = open(target_file, 'w')
+    for line in source:
+        translation, _ = worker.translate(line)
+        print >>target, translation
+    target.close()
+    source.close()
 
 if __name__ == '__main__':
     args = parse_args()
+    set_loglevel(args.debug)
     config = SafeConfigParser()
     config.read(args.config)
-    worker_kwargs = config.items(args.engine)
 
-    worker_class = eval("{}Worker".format(args.engine))
-    worker = worker_class(worker_kwargs)
-    
+    worker = load_worker(config, args)
+    translate_file(worker, args.input, args.text_output)
+
+   
