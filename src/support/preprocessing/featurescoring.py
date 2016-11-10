38a09d007d5235918cae3f1a418143c7926ba7d6
lefterav <lefterav@pindi.me>
2016-11-10 17:44:46 +0200

    Script for scoring attributes based on orange libraries

---
+++ src/support/preprocessing/featurescoring.py
@@ -0,0 +1,30 @@
+'''
+Utility script that measures the relevance of features against the gold labels. 
+
+Created on Nov 9, 2016
+
+@author: lefterav
+'''
+
+from ml.lib.orange.ranking import dataset_to_instances
+import sys
+from Orange.feature.scoring import score_all, InfoGain, GainRatio, Relief, Relevance, Cost, Gini, Distance, MDL
+
+
+def print_feature_scores(instances, methods):
+    for method in methods:
+        print method
+        scores = score_all(instances, method)
+        i = 0
+        for score, attribute_name in scores:
+            i += 1
+            print "%5.3f\t%s" % (score, attribute_name)
+            
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    instances = dataset_to_instances(filename)
+    methods = [InfoGain, GainRatio, Relief, Relevance, Cost, Gini, Distance, MDL]
+    print_feature_scores(instances, methods)
+
+    
