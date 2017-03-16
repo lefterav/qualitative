eb929120e6fdb63e55932650414fa8986ceaba0e
Eleftherios Avramidis <eleftherios.avramidis@gmail.com>
2017-03-16 10:53:37 +0100

    Reverted WMT conversion scripts

--- src/support/preprocessing/jcml/jcml2ref.py
+++ src/support/preprocessing/jcml/jcml2ref.py
@@ -8,6 +8,11 @@ from xml.etree.cElementTree import iterparse
 import sys
 from dataprocessor.input.jcmlreader import JcmlReader
 
+try:
+    TAG = sys.argv[2]
+except:
+    TAG = 'ref'
+
 if __name__ == '__main__':
     source_file = open(sys.argv[1], "r")
     # get an iterable
@@ -21,6 +26,9 @@ if __name__ == '__main__':
     target_id = 0
     for event, elem in context:
         #new sentence: get attributes
-        if event == "start" and elem.tag == "ref":
-            print elem.text
+        if event == "start" and elem.tag == TAG:
+            try:
+                print elem.text
+            except:
+                print elem.text.encode('utf-8').decode('ascii','ignore') 
         
