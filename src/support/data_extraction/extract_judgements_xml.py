c6227a426b99324fe9d295085408f60e0147dc08
lefterav <lefterav@pindi.me>
2017-04-01 01:34:53 +0200

    Tested extraction for WMT15 judgments

--- src/support/data_extraction/extract_judgements_xml.py
+++ src/support/data_extraction/extract_judgements_xml.py
@@ -5,12 +5,16 @@ Created on Mar 30, 2017
 '''
 
 from xml.etree.ElementTree import iterparse
-from sentence.sentence import SimpleSentence
-from sentence.parallelsentence import ParallelSentence
 import ConfigParser
 import sys
-from dataprocessor.sax.saxps2jcml import IncrementalJcml
+import logging as log
 
+from dataprocessor.sax.saxps2jcml import IncrementalJcml,\
+    MultiLangpairIncrementalWriter
+from sentence.sentence import SimpleSentence
+from sentence.parallelsentence import ParallelSentence
+
+# global dictionary mapping 3-letter to 2-letter language codes 
 LANGCODE = {'eng': 'en',
             'deu': 'de',
             'ces': 'cs',
@@ -19,26 +23,40 @@ LANGCODE = {'eng': 'en',
             'rus': 'ru'             
             }
 
+
 class WMTEvalXmlReader:
     """
     Read Appraise XML output in combination with user ratings as per WMT15
+    @ivar config: the loaded configuration file
+    @type config: ConfigParser
+    @ivar task: the label of the task in the configuration file
+    @type task: string
+    @ivar testset: the name of the test set at hand
+    @type testset: string
+    @ivar xmlfile: the xmlfile to be processed.
+    @type xmlfile: file
+    @ivar judges: a dictionary mapping the judge names to their anonymized ids
+    @type judges: dict(string, string)
+    @ivar langpairs: the langpairs whose annotations should be extracted from the given file
+    @type langpairs: list of strings
     """
     
-    def __init__(self, config):
+    def __init__(self, config, task):
         """
         Initialize function, by providing a config file with the parameters for data and format
         @param config File object containing a config file of the required format
         @type config: string
+        @param task: The identifier of the particular task in the config file
+        @typte tas: string
         """
         self.config = config
-        csvfilename = "%s/%s" % (config.get("data", "path"), config.get("data", "filename"))
-        self.xmlfile = open(csvfilename)
+        self.task = task
+        path = config.get(self.task, "path")
+        self.testset = config.get(self.task, "testset")
+        filename = config.get(self.task, "filename").format(path)
+        self.xmlfile = open(filename)
         self.judges = {}
-        
-        
-    def get_testset_per_langpair(self, langpair):
-        return self.config.get("testsets", langpair)
-    
+        self.langpairs = config.get(self.task, "langpairs").split(",")
     
     def anonymize_judge(self, judgeID):
         """
@@ -52,7 +70,6 @@ class WMTEvalXmlReader:
             self.judges[judgeID] = 'judge%d' % (len(self.judges)+1)
         return self.judges[judgeID]
     
-        
     def parse(self):
         """
         Process the XML file and return parallel sentence objects 
@@ -65,6 +82,8 @@ class WMTEvalXmlReader:
         # turn it into an iterator
         context = iter(context)
         translations = []
+        testset = self.testset
+        ignore_hit = False
         
         for event, elem in context:
             #new sentence: get attributes
@@ -72,38 +91,42 @@ class WMTEvalXmlReader:
                 hit_id = elem.attrib['hit-id']
                 source_language = LANGCODE[elem.attrib['source-language']]
                 target_language = LANGCODE[elem.attrib['target-language']]
-                testset = self.get_testset_per_langpair("{}-{}".format(source_language, target_language))
+                if "{}-{}".format(source_language, target_language) not in self.langpairs:
+                    ignore_hit = True
+                else:
+                    ignore_hit = False
             
+            # Don't parse HITS that don't belong to the desired langpairs
+            if ignore_hit:
+                continue
+            
+            # new ranking tasks defines sentence id, chance to get the text of source and reference
             if event == "start" and elem.tag == "ranking-task":
                 sentence_id = elem.attrib['id']
-                source_string = self.extract_source(testset, 
-                                                    source_language,
-                                                    target_language,
-                                                    sentence_id)
+                source_string = self.extract_source(testset, source_language, target_language, sentence_id)
                 source_sentence = SimpleSentence(source_string, {})
                 
                 reference_string = self.extract_reference(testset, source_language, target_language, sentence_id)
                 reference_sentence = SimpleSentence(reference_string, {})
             
+            # result entry provides the duration and the user id
             if event == "start" and elem.tag == "ranking-result":
                 user_id = self.anonymize_judge(elem.attrib['user'])
                 duration = elem.attrib['duration']
-                
+            
             if event == "start" and elem.tag == "translation":
                 system_id = elem.attrib['system'].split(",").pop()
                 system_id = system_id.replace(".txt", "")
                 rank = elem.attrib['rank']
                 attributes = {'system_id' : system_id, 'rank' : rank}
-                try:
-                    translation_string = self.extract_translation(testset,
-                                                              source_language,
-                                                              target_language,
-                                                              sentence_id,
-                                                              system_id)
-                except Exception as e:
-                    print e
-                    continue
                 
+                try:
+                    translation_string = self.extract_translation(testset, source_language, target_language, sentence_id, system_id)
+                except Exception as e:
+                    if ".tuning-" in str(e):
+                        continue
+                    else:
+                        raise(e)
                 translation = SimpleSentence(translation_string, attributes)
                 translations.append(translation)
             
@@ -118,35 +141,38 @@ class WMTEvalXmlReader:
                               'duration' : duration,
                               'id': sentence_id, 
                               }
-                parallelsentence = ParallelSentence(source_sentence, 
-                                                    translations, 
-                                                    reference_sentence, 
-                                                    attributes)
+                parallelsentence = ParallelSentence(source_sentence, translations, reference_sentence, attributes)
                 source_sentence = None
                 translations = []
                 reference_sentence = []
                 attributes = {}
                 yield parallelsentence
                 
-    
-    def extract_translation(self, testset, source_language, 
-                             target_language, sentence_id, system_id):
+    def extract_translation(self, testset, source_language, target_language, 
+                            sentence_id, system_id):
         """
         Retrieve the text of the system outputs from the respective text files
         @param testset: the name of the test set (e.g. newstest2015)
+        @type testset: string
+        @param source_language: a 2-letter language code for the source language
+        @type source_language: string
+        @param target_language: a 2-letter language code for the target language
+        @type target_language: string
+        @param sentence_id: the id of the sentence to be extracted
+        @type sentence_id: string
+        @param system_id: the id of the system that produced the sentence
+        @type system_id: string
+        @return: the text of the translation
+        @rtype: string
         """
         langpair = "{}-{}".format(source_language, target_language)
         sentence_index = int(sentence_id)
-        sentence_indexing_base = self.config.getint("format","sentence_indexing_base")
-        path = self.config.get("data","path")
+        sentence_indexing_base = self.config.getint(self.task,"sentence_indexing_base")
         result = ''
 
         if system_id != '_ref':
-            pattern_submissions = self.config.get('data', 'pattern_submissions')
-            filename = pattern_submissions.format(path=path,
-                                                  langpair=langpair,
-                                                  testset=testset,
-                                                  system=system_id)    
+            pattern_submissions = self.config.get(self.task, 'pattern_submissions')
+            filename = pattern_submissions.format(langpair=langpair, testset=testset, system=system_id)    
             textfile = open(filename)
             
             index = 0
@@ -160,16 +186,28 @@ class WMTEvalXmlReader:
             result = self.extract_references(target_language, None, sentence_index)
         return result
     
-    
     def extract_source(self, testset, source_language, target_language, sentence_id):
+        """
+        Retrieve the text of the source sentence from the external text files 
+        for the given sentence id.
+        @param testset: the name of the test set (e.g. newstest2015)
+        @type testset: string
+        @param source_language: a 2-letter language code for the source language
+        @type source_language: string
+        @param target_language: a 2-letter language code for the target language
+        @type target_language: string
+        @param sentence_id: the id of the sentence to be extracted
+        @type sentence_id: string
+        @return: the text of the source sentence
+        @rtype: string        
+        """        
         langpair = "{}-{}".format(source_language, target_language)
         sentence_id = int(sentence_id)
-        sentence_indexing_base = self.config.getint("format","sentence_indexing_base")
-        path = self.config.get("data","path")    
+        sentence_indexing_base = self.config.getint(self.task,"sentence_indexing_base")
+        path = self.config.get(self.task,"path")    
         
-        pattern_source = self.config.get("data","pattern_source")
-        filename = pattern_source.format(path=path,
-                                         langpair=langpair, 
+        pattern_source = self.config.get(self.task,"pattern_source")
+        filename = pattern_source.format(langpair=langpair, 
                                          srclang=source_language, 
                                          tgtlang=target_language,
                                          testset=testset)
@@ -187,16 +225,28 @@ class WMTEvalXmlReader:
         source_file.close()
         return result
     
-
     def extract_reference(self, testset, source_language, target_language, sentence_id):
+        """
+        Retrieve the text of the reference translation from the external text files 
+        for the given sentence id.
+        @param testset: the name of the test set (e.g. newstest2015)
+        @type testset: string
+        @param source_language: a 2-letter language code for the source language
+        @type source_language: string
+        @param target_language: a 2-letter language code for the target language
+        @type target_language: string
+        @param sentence_id: the id of the sentence to be extracted
+        @type sentence_id: string
+        @return: the text of the reference translation
+        @rtype: string        
+        """             
         langpair = "{}-{}".format(source_language, target_language)
         sentence_id = int(sentence_id)
-        sentence_indexing_base = self.config.getint("format","sentence_indexing_base")
-        path = self.config.get("data","path")    
+        sentence_indexing_base = self.config.getint(self.task,"sentence_indexing_base")
+        path = self.config.get(self.task,"path")    
         
-        pattern_source = self.config.get("data","pattern_reference")
-        filename = pattern_source.format(path=path,
-                                         langpair=langpair, 
+        pattern_source = self.config.get(self.task,"pattern_reference")
+        filename = pattern_source.format(langpair=langpair, 
                                          srclang=source_language, 
                                          tgtlang=target_language,
                                          testset=testset)
@@ -212,22 +262,44 @@ class WMTEvalXmlReader:
         if result =='':
             print "Cannot resolve source sentence {} in file {}".format(sentence_id, filename)
         reference_file.close()
-        return result
-    
+        return result      
+
+
+def _read_configuration():
+    config = ConfigParser.ConfigParser()
+    sys.stderr.write("Opening config file: {}\n".format(sys.argv[1]))
+    config.read([sys.argv[1]])
+    return config
+
+def process_tasks(config):
+    tasks = [section for section in config.sections() if section.startswith("task:")]
+    print tasks
+    for task in tasks:
+        print "Starting", task
+        
+        if config.get(task, "reader_mode") == "xml":
+            reader = WMTEvalXmlReader(config, task)
+            
+        else: 
+            reader = None
+        parallelsentences = reader.parse()
+        
+        langpairs = config.get(task, "langpairs").split(",")
+        output_pattern = config.get(task, "pattern_output")
+        writer = MultiLangpairIncrementalWriter(langpairs, output_pattern)
+        writer.add_parallelsentences(parallelsentences)
+        print "Finished ", task
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 1:
-        print 'USAGE: {} configuration_file.cfg'.format(sys.argv[0])
+        print 'USAGE: {} CONFIGURATION_FILES'.format(sys.argv[0])
         #print 'USAGE: %s SORTEDJUDGMENTS.CSV PATH' % sys.argv[0]
         #print '\tpath = path to folder with evaluation raw data'
     else:
-        config = ConfigParser.RawConfigParser()
-        sys.stderr.write("Opening config file: {}\n".format(sys.argv[1]))
-        config.read([sys.argv[1]])
-        reader = WMTEvalXmlReader(config)
-        parallelsentences = reader.parse()
-        writer = IncrementalJcml(config.get("output", "filename"))
-        writer.add_parallelsentences(parallelsentences)
+        config = _read_configuration()
+        process_tasks(config)
+        
         
         
     
